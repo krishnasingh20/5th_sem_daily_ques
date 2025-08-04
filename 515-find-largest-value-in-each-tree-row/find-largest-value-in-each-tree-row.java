fa90606a03deed6b14aaa8c605a706d5764be1c8
krishnasingh20 <krishnasinghks1225@gmail.com>
2025-08-04 21:56:54 +0530

    Time: 4 ms (7.99%) | Memory: 45.5 MB (52.27%) - LeetSync

--- 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.java
+++ 515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.java
@@ -15,21 +15,31 @@
  */
 class Solution {
     public List<Integer> largestValues(TreeNode root) {
-        List<Integer> ll = new ArrayList<>();
-        largest(root, 0, ll);
-        return ll;
+        List<Integer> ans = new ArrayList<>();
+        levelorder(root, ans);
+        return ans;
     }
-    public void largest(TreeNode root, int cl, List<Integer> ll) {
+    public void levelorder(TreeNode root, List<Integer> ans) {
         if(root == null) {
             return;
         }
-        if(cl == ll.size()) {
-            ll.add(root.val);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> ll = new ArrayList<>();
+            for(int i = 0; i < size; i++) {
+                TreeNode rv = q.remove();//remove first
+                ll.add(rv.val);
+                if(rv.left != null) {
+                    q.add(rv.left);
+                }
+                if(rv.right != null) {
+                    q.add(rv.right);
+                }
+            }
+            Collections.sort(ll);
+            ans.add(ll.get(size - 1));
         }
-        if(cl < ll.size()) {
-            ll.set(cl, Math.max(ll.get(cl), root.val));
-        }
-        largest(root.left, cl + 1, ll);
-        largest(root.right, cl + 1, ll);
     }
 }
