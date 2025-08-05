3b4ca50e224c2a48dab7cf4ef6cc99961123a3b5
krishnasingh20 <krishnasinghks1225@gmail.com>
2025-08-05 15:02:52 +0530

    Time: 47 ms (66.86%) | Memory: 64.1 MB (80.13%) - LeetSync

--- 2689-rearranging-fruits/rearranging-fruits.java
+++ 2689-rearranging-fruits/rearranging-fruits.java
@@ -1,34 +1,29 @@
 class Solution {
     public long minCost(int[] basket1, int[] basket2) {
-        HashMap<Integer, Integer> map = new HashMap<>();
         int minval = Integer.MAX_VALUE;
+        HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < basket1.length; i++) {
             map.put(basket1[i], map.getOrDefault(basket1[i], 0) + 1);
-            minval = Math.min(minval, basket1[i]);
-        }
-        
-        for(int i = 0; i < basket2.length; i++) {
             map.put(basket2[i], map.getOrDefault(basket2[i], 0) - 1);
+            minval = Math.min(minval, basket1[i]);
             minval = Math.min(minval, basket2[i]);
         }
-
         List<Integer> ll = new ArrayList<>();
         for(int key: map.keySet()) {
-            int diff = map.get(key);
-            if(diff % 2 != 0) {
+            if(map.get(key) % 2 != 0) {
                 return -1;
             }
+            int diff = map.get(key);
             for(int i = 0; i < Math.abs(diff) / 2; i++) {
                 ll.add(key);
             }
         }
         Collections.sort(ll);
-        long mincost = 0;
+        long ans = 0;
         int m = ll.size();
         for(int i = 0; i < m / 2; i++) {
-            int a = ll.get(i);
-            mincost += Math.min(a, 2 * minval);
+            ans += Math.min(ll.get(i), 2 * minval);
         }
-        return mincost;
+        return ans;
     }
 }
