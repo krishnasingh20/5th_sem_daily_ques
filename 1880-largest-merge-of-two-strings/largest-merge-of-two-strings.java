962fac547e8c2a70f6600f769fc29ee7ed1e67d3
krishnasingh20 <krishnasinghks1225@gmail.com>
2025-08-08 19:30:43 +0530

    Time: 51 ms (19.68%) | Memory: 45.2 MB (54.92%) - LeetSync

---
+++ 1880-largest-merge-of-two-strings/largest-merge-of-two-strings.java
@@ -0,0 +1,57 @@
+class Solution {
+    public String largestMerge(String word1, String word2) {
+        int i = 0;
+        int j = 0;
+        int n = word1.length();
+        int m = word2.length();
+        StringBuilder ans = new StringBuilder();
+        while(i < n && j < m) {
+            if(word1.charAt(i) > word2.charAt(j)) {
+                ans.append(word1.charAt(i));
+                i++;
+            }
+            else if(word1.charAt(i) < word2.charAt(j)) {
+                ans.append(word2.charAt(j));
+                j++;
+            }
+            else {
+                int a = i;
+                int b = j;
+                boolean flag1 = false;
+                boolean flag2 = false;
+                while(a < n && b < m) {
+                    if(word1.charAt(a) == word2.charAt(b)) {
+                        a++;
+                        b++;
+                    }
+                    else if(word1.charAt(a) > word2.charAt(b)) {
+                        flag1 = true;
+                        break;
+                    }
+                    else {
+                        flag2 = true;
+                        break;
+                    }
+                }
+                if(flag1 || (a == n && b == m) || (a < n && b == m)) {
+                    ans.append(word1.charAt(i));
+                    i++;
+                }
+                else {
+                    ans.append(word2.charAt(j));
+                    j++;
+                }
+            }
+        }
+
+        while(i < n) {
+            ans.append(word1.charAt(i));
+            i++;
+        }
+        while(j < m) {
+            ans.append(word2.charAt(j));
+            j++;
+        }
+        return ans.toString();
+    }
+}
