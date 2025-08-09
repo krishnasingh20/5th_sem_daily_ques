f7331c2f591eb97e327e7317589ebe1691cf8da4
krishnasingh20 <krishnasinghks1225@gmail.com>
2025-08-09 15:15:55 +0530

    Time: 47 ms (74.15%) | Memory: 59.3 MB (13.16%) - LeetSync

--- 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.java
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.java
@@ -1,12 +1,14 @@
 class Solution {
     public long minimumTime(int[] time, int totalTrips) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int t: time) {
+            min = Math.min(min, t);
+            max = Math.max(max, t);
+        }
+        long low = min;
+        long high = (long)max * totalTrips;
         long ans = 0;
-        Arrays.sort(time);
-        long low = time[0];
-        long high = 0;
-        for(int i = 0; i < time.length; i++) {
-            high += time[i];
-        } high *= totalTrips;
         while(low <= high) {
             long mid = low + (high - low) / 2;
             if(ispossible(time, mid, totalTrips)) {
@@ -17,16 +19,16 @@ class Solution {
                 low = mid + 1;
             }
         }
-        return ans;   
+        return ans;
     }
     public boolean ispossible(int[] time, long mid, int totalTrips) {
-            long possibleTrip = 0;
-            for(int i = 0; i < time.length; i++) {
-                possibleTrip += (mid / time[i]);
-                if(possibleTrip >= totalTrips) {
-                    return true;
-                }
+        long possibletrip = 0;
+        for(int t: time) {
+            possibletrip += (mid / t);
+            if(possibletrip >= totalTrips) {
+                return true;
             }
-            return false;
         }
+        return false;
+    }
 }
