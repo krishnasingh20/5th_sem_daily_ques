77bc0ac927e1c3d1e23c5b85fe92ab1b49646e79
krishnasingh20 <krishnasinghks1225@gmail.com>
2025-08-15 00:51:49 +0530

    Time: 13 ms (62.41%) | Memory: 55.5 MB (21.33%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,49 +1,28 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
         int n = nums.length;
-        long totalSubarray = (long)n * (n + 1) / 2;
         // subarray with less then k odd numbers
-        long subarray1 = lessThenK(nums, k);
-        // subarrray with more then k odd numbers
-        long subarray2 = moreThenK(nums, k);
-        return (int)(totalSubarray - (subarray1 + subarray2));
+        int subarray1 = count(nums, k - 1);
+        // subarrray with less then equal to k odd numbers
+        int subarray2 = count(nums, k);
+        return subarray2 - subarray1;
     }
-    public long lessThenK(int[] nums, int k) {
+    public int count(int[] nums, int k) {
         int si = 0;
         int ei = 0;
-        long ans = 0;
-        int crt_odd = 0;
-        while(ei < nums.length) {
-            if(nums[ei] % 2 != 0) {
-                crt_odd++;
-            }
-            while(crt_odd >= k) {
-                if(nums[si] % 2 != 0) {
-                    crt_odd--;
-                }
-                si++;
-            }
-            ans += (ei - si + 1);
-            ei++;
-        }
-        return ans;
-    }
-    public long moreThenK(int[] nums, int k) {
-        int si = 0;
-        int ei = 0;
-        long ans = 0;
+        int ans = 0;
         int crt_odd = 0;
         while(ei < nums.length) {
             if(nums[ei] % 2 != 0) {
                 crt_odd++;
             }
             while(crt_odd > k) {
-                ans += (nums.length - ei);
                 if(nums[si] % 2 != 0) {
                     crt_odd--;
                 }
                 si++;
             }
+            ans += (ei - si + 1);
             ei++;
         }
         return ans;
