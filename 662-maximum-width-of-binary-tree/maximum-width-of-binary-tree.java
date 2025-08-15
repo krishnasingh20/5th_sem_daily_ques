27ee0e3960dacb8930b82f6382c2ea46cd776009
krishnasingh20 <krishnasinghks1225@gmail.com>
2025-08-15 11:54:06 +0530

    Time: 1 ms (99.05%) | Memory: 43.3 MB (64.61%) - LeetSync

---
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int widthOfBinaryTree(TreeNode root) {
+        Queue<Pair> q = new LinkedList<>();
+        Pair p = new Pair();
+        p.node = root;
+        p.idx = 0;
+        q.add(p);
+        int ans = 0;
+        while(!q.isEmpty()) {
+            Pair start = q.peek();
+            Pair end = null;
+            int size = q.size();
+            for(int i = 0; i < size; i++) {
+                end = q.poll();
+                if(end.node.left != null) {
+                    Pair nn = new Pair();
+                    nn.node = end.node.left;
+                    nn.idx = 2 * end.idx + 1;//formula for finding index of ith parent left child
+                    q.add(nn);
+                }
+                if(end.node.right != null) {
+                    Pair nn = new Pair();
+                    nn.node = end.node.right;
+                    nn.idx = 2 * end.idx + 2;//formula for finding index of ith parent right child 
+                    q.add(nn);
+                }
+            }
+            ans = Math.max(ans, (end.idx - start.idx + 1));
+        }
+        return ans;
+    }
+    class Pair {
+        int idx;
+        TreeNode node;
+    }
+}
