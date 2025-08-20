04d2cede55ba7b83d708724574c7b8453a3e784a
krishnasingh20 <krishnasinghks1225@gmail.com>
2025-08-20 15:47:51 +0530

    Time: 15 ms (49.09%) | Memory: 55.4 MB (85.17%) - LeetSync

---
+++ 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maximumBags(int[] capacity, int[] rocks, int additionalRocks) {
+        int n = capacity.length;
+        for(int i = 0; i < n; i++) {
+            capacity[i] -= rocks[i];
+        }
+        Arrays.sort(capacity);
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            if(additionalRocks - capacity[i] < 0) {
+                return ans;
+            }
+            ans++;
+            additionalRocks -= capacity[i];
+        }
+        return ans;
+    }
+}
