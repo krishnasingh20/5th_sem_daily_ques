ce37b88325fea8b873ae25bc782b3bbdb3f057b6
krishnasingh20 <krishnasinghks1225@gmail.com>
2025-08-25 23:50:35 +0530

    Time: 2 ms (99.41%) | Memory: 47 MB (82.43%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.java
+++ 498-diagonal-traverse/diagonal-traverse.java
@@ -4,55 +4,44 @@ class Solution {
         int m = mat[0].length;
         int[] ans=  new int[n*m];
         int idx = 0;
-        ArrayList<Integer> ll = new ArrayList<>();
-        // add upper half
         boolean flag = true;
         for(int i = 0; i < n; i++) {
             int j = i;
             int k = 0;
+            int idx1 = idx;
             while(j >= 0 && k <= m-1) {
-                ll.add(mat[j][k]);
+                ans[idx++] = mat[j][k];
                 j--;
                 k++;
             }
-            if(flag) {
-                for(int l = 0; l < ll.size(); l++) {
-                    ans[idx++] = ll.get(l);
-                }
-                flag = false;
+            if(!flag){
+                reverse(ans, idx1, idx - 1);
             }
-            else {
-                Collections.reverse(ll);
-                for(int l = 0; l < ll.size(); l++) {
-                    ans[idx++] = ll.get(l);
-                }
-                flag = true;
-            }
-            ll.clear();
+            flag = !flag;
         }
         for(int i = 1; i < m; i++) {
             int j = i;
             int k = n-1;
+            int idx1 = idx;
             while(j < m && k >= 0) {
-                ll.add(mat[k][j]);
+                ans[idx++] = mat[k][j];
                 j++;
                 k--;
             }
-            if(flag) {
-                for(int l = 0; l < ll.size(); l++) {
-                    ans[idx++] = ll.get(l);
-                }
-                flag = false;
+            if(!flag){
+                reverse(ans, idx1, idx - 1);
             }
-            else {
-                Collections.reverse(ll);
-                for(int l = 0; l < ll.size(); l++) {
-                    ans[idx++] = ll.get(l);
-                }
-                flag = true;
-            }
-            ll.clear();
+            flag = !flag;
         }
         return ans;
+    }
+    public void reverse(int[] arr, int i, int j) {
+        while(i <= j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
     }
 }
