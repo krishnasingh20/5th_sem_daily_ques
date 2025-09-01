0a5578268623b416051899a5129ca69e029805cd
krishnasingh20 <krishnasinghks1225@gmail.com>
2025-09-02 02:06:45 +0530

    Time: 9 ms (93.99%) | Memory: 45 MB (71.02%) - LeetSync

--- 2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.java
+++ 2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.java
@@ -15,30 +15,6 @@ class Solution {
                 set.add(num);
             }
         }
-        int ans = 0;
-        for(int num: set) {
-            if(isPrime(num)) {
-                ans++;
-            }
-        }
-        return ans;
-    }
-    public boolean isPrime(int num) {
-        if(num <= 1) {
-            return false;
-        }
-        if(num == 2 || num == 3) {
-            return true;
-        }
-        if(num % 2 == 0 || num % 3 == 0) {
-            return false;
-        }
-        int limit = (int)Math.sqrt(num);
-        for(int i = 5; i <= limit; i += 6) {
-            if(num % i == 0 || num % (i+2) == 0) {
-                return false;
-            }
-        }
-        return true;
+        return set.size();
     }
 }
