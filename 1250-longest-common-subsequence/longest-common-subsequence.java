801f47445df5322c95faf84ee7811372e39d8a55
krishnasingh20 <krishnasinghks1225@gmail.com>
2025-09-05 23:35:44 +0530

    Time: 21 ms (51.90%) | Memory: 51 MB (56.55%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,12 +1,14 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         // since two parameter is changing so we need 2D DP
-        int[][] dp = new int[text1.length()][text2.length()];
-        for(int i = 0; i < dp.length; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-        return lcs(text1, text2, 0, 0, dp);
+        // int[][] dp = new int[text1.length()][text2.length()];
+        // for(int i = 0; i < dp.length; i++) {
+        //     Arrays.fill(dp[i], -1);
+        // }
+        // return lcs(text1, text2, 0, 0, dp);
+        return LCSBU(text1, text2);
     }
+    // topdown approach
     public int lcs(String s1, String s2, int i, int j, int[][] dp) {
         if(i == s1.length() || j == s2.length()) {
             return 0;
@@ -24,5 +26,24 @@ class Solution {
             ans = Math.max(f, s);
         }
         return dp[i][j] = ans;
+    }
+    // bottom up approach 
+    public int LCSBU(String s1, String s2) {
+        int[][] dp = new int[s1.length()+1][s2.length()+1];
+        for(int i = 1; i < dp.length; i++) {
+            for(int j = 1; j < dp[0].length; j++) {
+                int ans = 0;
+                if(s1.charAt(i-1) == s2.charAt(j-1)) {
+                    ans = 1 + dp[i-1][j-1];
+                }
+                else {
+                    int f = dp[i-1][j];
+                    int s = dp[i][j-1];
+                    ans = Math.max(f, s);
+                }
+                dp[i][j] = ans;
+            }
+        }
+        return dp[dp.length - 1][dp[0].length - 1];
     }
 }
