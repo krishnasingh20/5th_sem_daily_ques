de9578bcffd42015378fb3f5d57dfa9c4d83c4d5
krishnasingh20 <krishnasinghks1225@gmail.com>
2025-09-05 10:56:31 +0530

    Time: 99 ms (65.16%) | Memory: 64.4 MB (29.85%) - LeetSync

---
+++ 502-ipo/ipo.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
+        int[][] arr = new int[profits.length][2];
+        for(int i = 0; i < profits.length; i++) {
+            arr[i][0] = capital[i];
+            arr[i][1] = profits[i];
+        }
+        Arrays.sort(arr, (a, b)->a[0]-b[0]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->b[1]-a[1]);
+        for(int i = 0; i < arr.length;) {
+            while(i < arr.length && w >= arr[i][0]) {
+                pq.add(arr[i]);
+                i++;
+            }
+            if(pq.isEmpty() || k == 0) {
+                return w;
+            }
+            w += pq.poll()[1];
+            k--;
+        }
+        while(!pq.isEmpty() && k > 0) {
+            w += pq.poll()[1];
+            k--;
+        }
+        return w;
+    }
+}
