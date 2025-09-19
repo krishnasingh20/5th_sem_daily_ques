7f3c59aea6ea15217febcf14dfc5b198325cfc49
krishnasingh20 <krishnasinghks1225@gmail.com>
2025-09-19 18:22:37 +0530

    Time: 15 ms (80.50%) | Memory: 46.1 MB (76.12%) - LeetSync

---
+++ 475-heaters/heaters.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(houses);
+        Arrays.sort(heaters);
+        int low = 0;
+        int high = 1000000000;
+        int ans = 0;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if(isPossible(houses, heaters, mid)) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+    public boolean isPossible(int[] houses, int[] heaters, int mid) {
+        int i = 0;
+        int count = 0;
+        for(int j = 0; j < houses.length;) {
+            if(i >= heaters.length) {
+                break;
+            }
+            else if(houses[j] >= heaters[i]-mid && houses[j] <= heaters[i]+mid) {
+                count++;
+                j++;
+            }
+            else {
+                i++;
+            }
+        }
+        return count == houses.length;
+    }
+}
