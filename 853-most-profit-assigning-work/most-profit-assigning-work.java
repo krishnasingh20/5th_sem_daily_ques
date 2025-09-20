2d7a70589074c7ac550a2e4b5072cc7f19afc4df
krishnasingh20 <krishnasinghks1225@gmail.com>
2025-09-20 16:20:11 +0530

    Time: 26 ms (37.38%) | Memory: 46.2 MB (22.88%) - LeetSync

--- 853-most-profit-assigning-work/most-profit-assigning-work.java
+++ 853-most-profit-assigning-work/most-profit-assigning-work.java
@@ -1,5 +1,6 @@
 class Solution {
     public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        // these question is based on logic of IPO and i used same concept to solve it, the only difference is that in ipo we have to check with aur  current capital we have in these we will check that how much max profit ith worker can get with max difficulty worker[i]
         int[][] arr = new int[profit.length][2];
         for(int i = 0; i < profit.length; i++) {
             arr[i][0] = difficulty[i];
