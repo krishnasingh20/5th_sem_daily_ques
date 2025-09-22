d2071edf31161c46a78e6423ec41a8f99a25039b
krishnasingh20 <krishnasinghks1225@gmail.com>
2025-09-22 11:33:24 +0530

    Time: 8 ms (86.33%) | Memory: 45.3 MB (43.51%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -1,6 +1,6 @@
 class Solution {
     public int longestIncreasingPath(int[][] matrix) {
-        // in these babiaclly i try to find for evry index that maximum length of decreasing path (longest increasing can be equivalent to it) and i used dp to store it so that in future when i go to that index agin the recomputed value get reassign
+        // in these babiaclly i try to find for evry index that maximum length of increasing  path and i used dp to store it so that in future when i go to that index agin the recomputed value get return , which reduces recomputation time 
         int[][] dp = new int[matrix.length][matrix[0].length];
         for(int[] d: dp) {
             Arrays.fill(d, -1);
@@ -8,13 +8,13 @@ class Solution {
         int ans = 0;
         for(int i = 0; i < matrix.length; i++) {
             for(int j = 0; j < matrix[0].length; j++) {
-                ans = Math.max(ans, longestIncPath(matrix, Long.MAX_VALUE, i, j, dp));
+                ans = Math.max(ans, longestIncPath(matrix, Integer.MIN_VALUE, i, j, dp));
             }
         }
         return ans;
     }
-    public int longestIncPath(int[][] matrix, long val, int r, int c, int[][] dp) {
-        if(r < 0 || c < 0 || r >= matrix.length || c >= matrix[0].length || matrix[r][c] == -1 || val <= (long)matrix[r][c]) {
+    public int longestIncPath(int[][] matrix, int val, int r, int c, int[][] dp) {
+        if(r < 0 || c < 0 || r >= matrix.length || c >= matrix[0].length || matrix[r][c] == -1 || val >= matrix[r][c]) {
             return 0;
         }
         if(dp[r][c] != -1) {
@@ -23,13 +23,13 @@ class Solution {
         int temp = matrix[r][c];
         matrix[r][c] = -1;
         // up
-        int up = 1 + longestIncPath(matrix, (long)temp, r-1, c, dp);
+        int up = 1 + longestIncPath(matrix, temp, r-1, c, dp);
         //down
-        int down = 1 + longestIncPath(matrix, (long)temp, r+1, c, dp);
+        int down = 1 + longestIncPath(matrix, temp, r+1, c, dp);
         //left
-        int left = 1 + longestIncPath(matrix, (long)temp, r, c-1, dp);
+        int left = 1 + longestIncPath(matrix, temp, r, c-1, dp);
         //right
-        int right = 1 + longestIncPath(matrix, (long)temp, r, c+1, dp);
+        int right = 1 + longestIncPath(matrix, temp, r, c+1, dp);
         matrix[r][c] = temp;
         return dp[r][c] = Math.max(Math.max(left, right), Math.max(up, down));
     }
