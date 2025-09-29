7af33bb91df1902e2c2569a6c7606a0305681067
Krishna Singh <krishnasinghks1225@gmail.com>
2025-09-29 13:32:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (15.02%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = i+1; j < matrix.length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i = 0; i < matrix.length; i++) {
+            reverse(i, matrix);
+        }
+    }
+    public void reverse(int row, int[][] matrix) {
+        int i = 0;
+        int j = matrix[0].length-1;
+        while(i < j) {
+            int temp = matrix[row][i];
+            matrix[row][i] = matrix[row][j];
+            matrix[row][j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
