db75e608156697d7aa8c580d702123ef4901db78
Krishna Singh <krishnasinghks1225@gmail.com>
2025-09-29 14:24:35 +0530

    Time: 1 ms (69.95%) | Memory: 45.6 MB (69.04%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -11,16 +11,8 @@ class Solution {
             }
         }
         for(int i = 0; i < matrix.length; i++) {
-            if(row[i]) {
-                for(int j = 0; j < matrix[0].length; j++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-        for(int j = 0; j < matrix[0].length; j++) {
-            if(col[j]) {
-                for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                if(row[i] || col[j]) {
                     matrix[i][j] = 0;
                 }
             }
