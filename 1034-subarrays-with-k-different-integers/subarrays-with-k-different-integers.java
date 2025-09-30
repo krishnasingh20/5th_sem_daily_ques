c0e2e5526b48f0318184288634252718ac98b69c
Krishna Singh <krishnasinghks1225@gmail.com>
2025-09-30 10:34:34 +0530

    Time: 60 ms (5.18%) | Memory: 46 MB (92.70%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,21 +1,20 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        // finding suubarray with exactly k different integer is difficult so what we can do that we can find subarray with less then equal to k diffrent integer and subarrays with less then equal to (k-1) diffrent integer yhen we can do subarray_k - subarray_(k-1) =  subarray with k different integer
-
-        return count(nums, k) - count(nums, k - 1);
+        // optimize approach
+        return count(nums, k) - count(nums, k-1);
     }
     public int count(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        int dist = 0;
         int ei = 0;
         int si = 0;
+        int dist = 0;
         int ans = 0;
         int n = nums.length;
         while(ei < n) {
-            if(map.getOrDefault(nums[ei], 0) == 0) {
+            map.put(nums[ei], map.getOrDefault(nums[ei], 0)+1);
+            if(map.get(nums[ei]) == 1) {
                 dist++;
             }
-            map.put(nums[ei], map.getOrDefault(nums[ei], 0) + 1);
             while(dist > k) {
                 map.put(nums[si], map.get(nums[si]) - 1);
                 if(map.get(nums[si]) == 0) {
