dd83417d6b80131429dbc4c2d16d9d9cfd8dc5fd
Krishna Singh <krishnasinghks1225@gmail.com>
2025-09-30 22:02:35 +0530

    Time: 9 ms (91.26%) | Memory: 45 MB (59.13%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -8,32 +8,39 @@ class Solution {
         for(char c: p.toCharArray()) {
             freq[c-'a']++;
         }
-        String pKey = keyGenerate(freq);
-        Arrays.fill(freq, 0);
+        // String pKey = keyGenerate(freq);
+        // Arrays.fill(freq, 0);
+        int[] freq1 = new int[26];
         int k = p.length();
         int n = s.length();
         for(int i = 0; i < k; i++) {
-            freq[s.charAt(i)-'a']++;
+            freq1[s.charAt(i)-'a']++;
         }
-        String sKey = keyGenerate(freq);
-        if(sKey.equals(pKey)) {
+        // String sKey = keyGenerate(freq);
+        // if(sKey.equals(pKey)) {
+        //     ll.add(0);
+        // }
+        if(Arrays.equals(freq, freq1)) {
             ll.add(0);
         }
         for(int i = k; i < n; i++) {
-            freq[s.charAt(i)-'a']++;
-            freq[s.charAt(i-k)-'a']--;
-            sKey = keyGenerate(freq);
-            if(sKey.equals(pKey)) {
+            freq1[s.charAt(i)-'a']++;
+            freq1[s.charAt(i-k)-'a']--;
+            // sKey = keyGenerate(freq);
+            // if(sKey.equals(pKey)) {
+            //     ll.add(i-k+1);
+            // }
+            if(Arrays.equals(freq, freq1)) {
                 ll.add(i-k+1);
             }
         }
         return ll;
     }
-    public String keyGenerate(int[] freq) {
-        StringBuilder str = new StringBuilder();
-        for(int i = 0; i < 26; i++) {
-            str.append(freq[i]).append('/');
-        }
-        return str.toString();
-    }
+    // public String keyGenerate(int[] freq) {
+    //     StringBuilder str = new StringBuilder();
+    //     for(int i = 0; i < 26; i++) {
+    //         str.append(freq[i]).append('/');
+    //     }
+    //     return str.toString();
+    // }
 }
