8411a54e078f7bf1b6ab85b7177a7bb2cdb9c971
Krishna Singh <krishnasinghks1225@gmail.com>
2025-09-30 22:19:01 +0530

    Time: 9 ms (91.26%) | Memory: 44.9 MB (70.56%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -3,44 +3,41 @@ class Solution {
         if(p.length() > s.length()) {
             return new ArrayList<>();
         }
+
         List<Integer> ll = new ArrayList<>();
         int[] freq = new int[26];
+
         for(char c: p.toCharArray()) {
             freq[c-'a']++;
         }
-        // String pKey = keyGenerate(freq);
-        // Arrays.fill(freq, 0);
+
         int[] freq1 = new int[26];
         int k = p.length();
         int n = s.length();
+
         for(int i = 0; i < k; i++) {
             freq1[s.charAt(i)-'a']++;
         }
-        // String sKey = keyGenerate(freq);
-        // if(sKey.equals(pKey)) {
-        //     ll.add(0);
-        // }
-        if(Arrays.equals(freq, freq1)) {
+
+        if(compare(freq, freq1)) {
             ll.add(0);
         }
+        
         for(int i = k; i < n; i++) {
             freq1[s.charAt(i)-'a']++;
             freq1[s.charAt(i-k)-'a']--;
-            // sKey = keyGenerate(freq);
-            // if(sKey.equals(pKey)) {
-            //     ll.add(i-k+1);
-            // }
-            if(Arrays.equals(freq, freq1)) {
+            if(compare(freq, freq1)) {
                 ll.add(i-k+1);
             }
         }
         return ll;
     }
-    // public String keyGenerate(int[] freq) {
-    //     StringBuilder str = new StringBuilder();
-    //     for(int i = 0; i < 26; i++) {
-    //         str.append(freq[i]).append('/');
-    //     }
-    //     return str.toString();
-    // }
+    public boolean compare(int[] a, int[] b) {
+        for(int i = 0; i < 26; i++) {
+            if(a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
