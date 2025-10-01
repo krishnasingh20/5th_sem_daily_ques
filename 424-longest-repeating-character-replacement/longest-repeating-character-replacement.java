2163d1dd3f84445b79a1d69f8d580fa7b5bf29ba
Krishna Singh <krishnasinghks1225@gmail.com>
2025-10-01 15:03:52 +0530

    Time: 7 ms (87.67%) | Memory: 44.6 MB (92.74%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -5,9 +5,11 @@ class Solution {
         int si = 0;
         int n = s.length();
         int ans = 0;
+        int max = 0;
         while(ei < n) {
             freq[s.charAt(ei)-'A']++;
-            while((ei-si+1)-max(freq) > k) {
+            max = Math.max(max, freq[s.charAt(ei)-'A']);
+            while((ei-si+1)-max > k) {
                 freq[s.charAt(si)-'A']--;
                 si++;
             }
@@ -16,43 +18,5 @@ class Solution {
         }
         return ans;
     }
-    // i used these because every time in my window i will store one with max frequency and other will include in k operation to maximize my answer
-    public int max(int[] freq) {
-        int ans = 0;
-        for(int i = 0; i < 26; i++) {
-            ans = Math.max(ans, freq[i]);
-        }
-        return ans;
-    }
-
-    // or
-
-    // public int characterReplacement(String s, int k) {
-    //     int ans=0;
-    //     for(char ch='A';ch<='Z';ch++){
-    //         ans=Math.max(ans,maxlen(s,ch,k));
-    //     }
-    //     return ans;
-    // }
-    // public static int maxlen(String s,char ch,int k){
-    //     int start=0;
-    //     int end=0;
-    //     int flip=0;
-    //     int ans=0;
-    //     while(end<s.length()){
-    //         if(s.charAt(end)!=ch){
-    //             flip++;
-    //         }
-    //         while(flip>k){
-    //             if(s.charAt(start)!=ch){
-    //                 flip--;
-    //             }
-    //             start++;
-    //         }
-    //         ans=Math.max(ans,end-start+1);
-    //         end++;
-    //     }
-    //     return ans;
-    // }
 }
 
