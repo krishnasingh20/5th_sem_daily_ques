485516e78e47ff1289e329fcdfd118aaddf1ffbf
Krishna Singh <krishnasinghks1225@gmail.com>
2025-10-01 15:02:36 +0530

    Time: 16 ms (39.33%) | Memory: 45.1 MB (32.95%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -16,6 +16,7 @@ class Solution {
         }
         return ans;
     }
+    // i used these because every time in my window i will store one with max frequency and other will include in k operation to maximize my answer
     public int max(int[] freq) {
         int ans = 0;
         for(int i = 0; i < 26; i++) {
@@ -23,4 +24,35 @@ class Solution {
         }
         return ans;
     }
+
+    // or
+
+    // public int characterReplacement(String s, int k) {
+    //     int ans=0;
+    //     for(char ch='A';ch<='Z';ch++){
+    //         ans=Math.max(ans,maxlen(s,ch,k));
+    //     }
+    //     return ans;
+    // }
+    // public static int maxlen(String s,char ch,int k){
+    //     int start=0;
+    //     int end=0;
+    //     int flip=0;
+    //     int ans=0;
+    //     while(end<s.length()){
+    //         if(s.charAt(end)!=ch){
+    //             flip++;
+    //         }
+    //         while(flip>k){
+    //             if(s.charAt(start)!=ch){
+    //                 flip--;
+    //             }
+    //             start++;
+    //         }
+    //         ans=Math.max(ans,end-start+1);
+    //         end++;
+    //     }
+    //     return ans;
+    // }
 }
+
