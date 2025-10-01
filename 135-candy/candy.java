1962bf838b5f72f63e91f78c4e1c3679f97ac39b
Krishna Singh <krishnasinghks1225@gmail.com>
2025-10-01 14:21:51 +0530

    Time: 3 ms (80.30%) | Memory: 46 MB (45.65%) - LeetSync

---
+++ 135-candy/candy.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int[] candy = new int[ratings.length];
+        Arrays.fill(candy, 1);
+        for(int i = 1; i < ratings.length; i++) {
+            if(ratings[i] > ratings[i-1]) {
+                candy[i] = Math.max(candy[i], candy[i-1]+1);
+            }
+        }
+        for(int i = ratings.length - 2; i >= 0; i--) {
+            if(ratings[i] > ratings[i+1]) {
+                candy[i] = Math.max(candy[i], candy[i+1]+1);
+            }
+        }
+        int ans = 0;
+        for(int a: candy) {
+            ans += a;
+        }
+        return ans;
+    }
+}
