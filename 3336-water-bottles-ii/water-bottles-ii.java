9248e1780dc5b67b38476dcbc87f5957354f5a40
Krishna Singh <krishnasinghks1225@gmail.com>
2025-10-02 10:00:35 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (96.77%) - LeetSync

--- 3336-water-bottles-ii/water-bottles-ii.java
+++ 3336-water-bottles-ii/water-bottles-ii.java
@@ -2,9 +2,10 @@ class Solution {
     public int maxBottlesDrunk(int numBottles, int numExchange) {
         int ans = numBottles;
         while(numBottles >= numExchange) {
-            ans += 1;
-            numBottles -= (numExchange-1);
-            numExchange++;
+            numBottles -= numExchange;
+            ans++;
+            numBottles++;//numExchange bottle ke bad ek full bottle mila jise mai pike empty bottle me sum kr diya aur jo ek bottle aur usko upper answer me add krdiya
+            numExchange++;//say in question that after every numExchange empty bottle it value increase by 1
         }
         return ans;
     }
