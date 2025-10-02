a9a3d6e7eed5a1d45fec7fe3556fa6eb6510c984
Krishna Singh <krishnasinghks1225@gmail.com>
2025-10-03 00:02:29 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (45.17%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -4,11 +4,19 @@ class Solution {
         for(int pile: piles) {
             sum += pile;
         }
-        int[][] dp = new int[piles.length][piles.length];
-        for(int[] d: dp) {
-            Arrays.fill(d, -1);
+        int alice = 0;
+        int l = 0;
+        int m = piles.length - 1;
+        while(l < m) {
+            alice += Math.max(piles[l], piles[m]);
+            l++;
+            m--;
         }
-        int alice = maxPoint(piles, 0, piles.length - 1, dp);
+        // int[][] dp = new int[piles.length][piles.length];
+        // for(int[] d: dp) {
+        //     Arrays.fill(d, -1);
+        // }
+        // int alice = maxPoint(piles, 0, piles.length - 1, dp);
         return alice > sum - alice;
     }
     public int maxPoint(int[] piles, int i, int j, int[][] dp) {
@@ -21,5 +29,5 @@ class Solution {
         int a = piles[i] + maxPoint(piles, i+1, j-1, dp);
         int b = piles[j] + maxPoint(piles, i+1, j-1, dp);
         return dp[i][j] = Math.max(a, b);
-    }
+    } 
 }
