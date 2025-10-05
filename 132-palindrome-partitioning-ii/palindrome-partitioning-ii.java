00ef2406fc53f2b1af515f374cf11e0048176ec1
Krishna Singh <krishnasinghks1225@gmail.com>
2025-10-05 14:28:29 +0530

    Time: 2477 ms (34.19%) | Memory: 41.2 MB (96.27%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -1,38 +1,25 @@
 class Solution {
     public int minCut(String s) {
-        int n = s.length();
-        if(n == 2000 && s.charAt(0) == 'a' && s.charAt(n-1) == 'b') {
-            return 1;
-        }
-        System.out.println(n);
-        long[][] dp = new long[s.length()][s.length()];
-        for(long[] d: dp) {
-            Arrays.fill(d, 1000000000);
-        }
-        return (int)partition(s, 0, 0, dp)-1;
+        int[] dp = new int[s.length()];
+        Arrays.fill(dp, -1);
+        return partition(s, 0, dp)-1;
     }
-    public long partition(String s, int i, int j, long[][] dp) {
-        if(i == s.length() && j == s.length()) {
+    public int partition(String s, int i, int[] dp) {
+        if(i >= s.length()) {
             return 0;
         }
-        if(j >= s.length()) {
-            return 1000000000;
+        if(dp[i] != -1) {
+            return dp[i];
         }
-        if(dp[i][j] != 1000000000) {
-            return dp[i][j];
+        int ans = Integer.MAX_VALUE;
+        for(int j = i; j < s.length(); j++) {
+            if(isPalindrome(s, i, j)) {
+                ans = Math.min(ans, 1+partition(s, j+1, dp));
+            }
         }
-        long ans = Long.MAX_VALUE;
-        if(isPalindrome(s.substring(i, j+1))) {
-            long a = 1 + partition(s, j+1, j+1, dp);
-            ans = Math.min(ans, a);
-        }
-        long b = partition(s, i, j+1, dp);
-        ans = Math.min(ans, b);
-        return dp[i][j] = ans;
+        return dp[i] = ans;
     }
-    public boolean isPalindrome(String s) {
-        int i = 0;
-        int j = s.length()-1;
+    public boolean isPalindrome(String s, int i, int j) {
         while(i < j) {
             if(s.charAt(i) != s.charAt(j)) {
                 return false;
