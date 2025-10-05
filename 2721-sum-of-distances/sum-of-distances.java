79b6cd9f715cc7774c7fdd47b412acb59bf3b33e
Krishna Singh <krishnasinghks1225@gmail.com>
2025-10-06 00:40:27 +0530

    Time: 28 ms (42.98%) | Memory: 76.4 MB (31.92%) - LeetSync

--- 2721-sum-of-distances/sum-of-distances.java
+++ 2721-sum-of-distances/sum-of-distances.java
@@ -1,62 +1,32 @@
 class Solution {
-    public long[] distance(int[] nums) {
-        HashMap<Integer, List<Long>> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++) {
-            if(!map.containsKey(nums[i])) {
-                List<Long> ll = new ArrayList<>();
-                ll.add((long)i);
-                map.put(nums[i], ll);
+    public long[] distance(int[] arr) {
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        for(int i = 0; i < arr.length; i++) {
+            if(!map.containsKey(arr[i])) {
+                List<Integer> ll = new ArrayList<>();
+                ll.add(i);
+                map.put(arr[i], ll);
             }
             else {
-                List<Long> ll = map.get(nums[i]);
-                ll.add((long)i+ll.get(ll.size()-1));
+                map.get(arr[i]).add(i);
             }
         }
-        long[] ans = new long[nums.length];
-        for(int i = 0; i < nums.length; i++) {
-            if(map.get(nums[i]).size() == 1) {
-                ans[i] = 0;
+        long[] ans = new long[arr.length];
+        for(List<Integer> ll: map.values()) {
+            int n = ll.size();
+            long leftSum = 0;
+            long rightSum = 0;
+            for(int l: ll) {
+                rightSum += l;
             }
-            else {
-                List<Long> ll = map.get(nums[i]);
-                int idx = binarySearch(ll, 0, ll.size()-1, i);
-                long val = 0;
-                if(idx == 0) {
-                    val += (ll.get(ll.size()-1) - ll.get(idx)) - (i*(ll.size()-1));
-                }
-                else {
-                    val += ((long)i*idx - ll.get(idx - 1));
-                    val += (ll.get(ll.size() - 1) - ll.get(idx)) - ((long)i*(ll.size() - (idx+1)));
-                }
-                ans[i] = val;
+            for(int i = 0; i < n; i++) {
+                long val = (i*(long)ll.get(i)) - leftSum;
+                val += (rightSum - (ll.get(i)*(long)(n-i)));
+                leftSum += ll.get(i);
+                rightSum -= ll.get(i);
+                ans[ll.get(i)] = val;
             }
         }
         return ans;
-    }
-    public int binarySearch(List<Long> ll, int low, int high, int target) {
-        while(low <= high) {
-            int mid = low + (high - low)/2;
-            if(mid == 0) {
-                if(ll.get(mid) == target) {
-                    return mid;
-                }
-                else if(ll.get(mid) < target) {
-                    low = mid +  1;
-                }
-                else {
-                    high = mid - 1;
-                }
-            }
-            else if(ll.get(mid)-ll.get(mid - 1) == target) {
-                return mid;
-            }
-            else if(ll.get(mid)-ll.get(mid-1) > target) {
-                high = mid - 1;
-            }
-            else {
-                low = mid + 1;
-            }
-        }
-        return -1;
     }
 }
