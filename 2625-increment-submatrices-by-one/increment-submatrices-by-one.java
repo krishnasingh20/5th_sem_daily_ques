a4cd612a37a9a99296f30925f9919e86f53c688e
Krishna Singh <krishnasinghks1225@gmail.com>
2025-10-07 19:21:48 +0530

    Time: 19 ms (64.16%) | Memory: 56.8 MB (7.52%) - LeetSync

--- 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[][] rangeAddQueries(int n, int[][] queries) {
-        int[][] prefix = new int[n][n+1];
+        int[][] prefix = new int[n][n];
         for(int[] query: queries) {
             int r1 = query[0];
             int r2 = query[2];
@@ -8,20 +8,16 @@ class Solution {
             int c2 = query[3];
             for(int i = r1; i <= r2; i++) {
                 prefix[i][c1] += 1;
-                prefix[i][c2+1] -= 1;
+                if(c2+1 < n) {
+                    prefix[i][c2+1] -= 1;;
+                }
             }
         }
         for(int i = 0; i < n; i++) {
-            for(int j = 1; j < n+1; j++) {
+            for(int j = 1; j < n; j++) {
                 prefix[i][j] += prefix[i][j-1];
             }
         }
-        int[][] ans = new int[n][n];
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                ans[i][j] = prefix[i][j];
-            }
-        }
-        return ans;
+        return prefix;
     }
 }
