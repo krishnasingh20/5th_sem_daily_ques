a78859cc9bb72c8bec20b528b44698da3a589fae
Krishna Singh <krishnasinghks1225@gmail.com>
2025-10-11 15:31:06 +0530

    Time: 13 ms (33.58%) | Memory: 46.1 MB (32.09%) - LeetSync

--- 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.java
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.java
@@ -1,10 +1,13 @@
 class Solution {
     static int mod = 1000000007;
     public int numWays(int steps, int arrLen) {
-        HashMap<String, Long> map = new HashMap<>();
-        return (int)(numWay(steps, 0, 0, arrLen, map) % mod);
+        long[][] dp = new long[501][steps+1];
+        for(long[] d: dp) {
+            Arrays.fill(d, -1);
+        }
+        return (int)(numWay(steps, 0, 0, arrLen, dp) % mod);
     }
-    public long numWay(int steps, int currStep, int i, int arrLen, HashMap<String, Long> map) {
+    public long numWay(int steps, int currStep, int i, int arrLen, long[][] dp) {
         if(currStep == steps) {
             if(i == 0) {
                 return 1;
@@ -14,14 +17,12 @@ class Solution {
         if(i < 0 || i >= arrLen) {
             return 0;
         }
-        String key = i+"/"+currStep;
-        if(map.containsKey(key)) {
-            return map.get(key);
+        if(dp[i][currStep] != -1) {
+            return dp[i][currStep];
         }
-        long stay = numWay(steps, currStep+1, i, arrLen, map);
-        long left = numWay(steps, currStep+1, i-1, arrLen, map);
-        long right = numWay(steps, currStep+1, i+1, arrLen, map);
-        map.put(key, (stay+left+right) % mod);
-        return (stay+left+right) % mod;
+        long stay = numWay(steps, currStep+1, i, arrLen, dp);
+        long left = numWay(steps, currStep+1, i-1, arrLen, dp);
+        long right = numWay(steps, currStep+1, i+1, arrLen, dp);
+        return dp[i][currStep] = (stay+left+right) % mod;
     }
 }
