1663e8c35dc0ef9819e2451bb73316a491f9e70a
Krishna Singh <krishnasinghks1225@gmail.com>
2025-10-11 15:22:05 +0530

    Time: 499 ms (11.28%) | Memory: 284.5 MB (5.13%) - LeetSync

--- 2477-number-of-ways-to-reach-a-position-after-exactly-k-steps/number-of-ways-to-reach-a-position-after-exactly-k-steps.java
+++ 2477-number-of-ways-to-reach-a-position-after-exactly-k-steps/number-of-ways-to-reach-a-position-after-exactly-k-steps.java
@@ -1,23 +1,25 @@
 class Solution {
     int mod = 1000000007;
     public int numberOfWays(int startPos, int endPos, int k) {
-        HashMap<String, Long> map = new HashMap<>();
-        return (int)(numWays(startPos, startPos, endPos, k, 0, map) % mod);
+        long[][] dp = new long[30001][k+1];
+        for(long[] d: dp) {
+            Arrays.fill(d, -1);
+        }
+        return (int)(numWays(startPos, startPos, endPos, k, 0, dp) % mod);
     }
-    public long numWays(int currPos, int startPos, int endPos, int k, int currStep, HashMap<String, Long> map) {
+    public long numWays(int currPos, int startPos, int endPos, int k, int currStep, long[][] dp) {
         if(currStep == k) {
             if(currPos == endPos) {
                 return 1;
             }
             return 0;
         }
-        String key = currPos+"/"+currStep;
-        if(map.containsKey(key)) {
-            return map.get(key);
+        // adding 1000 to avoid index out of bound in case of negative position because it is a infinite number line
+        if(dp[currPos+1000][currStep] != -1) {
+            return dp[currPos+1000][currStep];
         }
-        long left = numWays(currPos - 1, startPos, endPos, k, currStep + 1, map) % mod;
-        long right = numWays(currPos + 1, startPos, endPos, k, currStep + 1, map) % mod;
-        map.put(key, (left+right) % mod);
-        return (left + right) % mod;
+        long left = numWays(currPos - 1, startPos, endPos, k, currStep + 1, dp) % mod;
+        long right = numWays(currPos + 1, startPos, endPos, k, currStep + 1, dp) % mod;
+        return dp[currPos+1000][currStep] = (left + right) % mod;
     }
 }
