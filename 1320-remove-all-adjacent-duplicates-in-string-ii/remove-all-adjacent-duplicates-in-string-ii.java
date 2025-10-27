a091764b38f21f3ffd32603c207cba8fe128e334
Krishna Singh <krishnasinghks1225@gmail.com>
2025-10-27 23:58:16 +0530

    Time: 31 ms (48.56%) | Memory: 45.2 MB (90.69%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,38 +1,38 @@
 class Solution {
     public String removeDuplicates(String s, int k) {
-        Stack<String> st = new Stack<>();
+        Stack<Pair> st = new Stack<>();
         for(int i = 0; i < s.length(); i++) {
-            if(st.size() == 0) {
-                st.push(s.charAt(i)+"");
-                st.push("1");
-                continue;
-            }
-            String s1 = st.pop();
-            if(st.peek().charAt(0) == s.charAt(i)) {
-                int x = Integer.parseInt(s1) + 1;
+            if(!st.isEmpty() && st.peek().ch == s.charAt(i)) {
+                int x = st.peek().c + 1;
                 if(x % k == 0) {
                     st.pop();
                 }
                 else {
-                    st.push((x%k)+"");
+                    st.peek().c = x % k;
                 }
             }
             else {
-                st.push(s1);
-                st.push(s.charAt(i)+"");
-                st.push("1");
+                st.push(new Pair(s.charAt(i), 1));
             }
         }
         StringBuilder str = new StringBuilder();
         while(!st.isEmpty()) {
-            String s1 = st.pop();
-            int x = Integer.parseInt(s1);
-            char c = st.pop().charAt(0);
+            Pair p = st.pop();
+            int x = p.c;
+            char c = p.ch;
             for(int i = 0; i < x; i++) {
                 str.append(c);
             }
         }
         str.reverse();
         return str.toString();
+    }
+    class Pair {
+        char ch;
+        int c;
+        Pair(char ch, int c) {
+            this.ch = ch;
+            this.c = c;
+        }
     }
 }
