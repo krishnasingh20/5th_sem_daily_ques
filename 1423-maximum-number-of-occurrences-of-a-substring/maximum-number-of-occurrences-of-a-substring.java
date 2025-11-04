4c1b294145d53ccffee775e23456de822058ad74
Krishna Singh <krishnasinghks1225@gmail.com>
2025-11-04 21:18:01 +0530

    Time: 34 ms (82.73%) | Memory: 48.1 MB (19.48%) - LeetSync

--- 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.java
+++ 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.java
@@ -1,24 +1,34 @@
 class Solution {
     public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
-        HashMap<String, Integer> map = new HashMap<>();
         int[] freq = new int[26];
-        int max = 0;
-        for(int i = minSize; i <= maxSize; i++) {
-            for(int j = 0; j <= s.length()-i; j++) {
-                String s1 = s.substring(j, j+i);
-                int dist = 0;
-                for(int k = 0; k < s1.length(); k++) {
-                    if(freq[s1.charAt(k)-'a']++ == 0) {
-                        dist++;
-                    }
-                }
-                if(dist <= maxLetters) {
-                    map.put(s1, map.getOrDefault(s1, 0)+1);
-                    max = Math.max(max, map.get(s1));
-                }
-                Arrays.fill(freq, 0);
+        HashMap<String, Integer> map = new HashMap<>();
+        int dist = 0;
+        for(int i = 0;i < minSize; i++) {
+            if(freq[s.charAt(i)-'a']++ == 0) {
+                dist++;
             }
         }
-        return max;
+        if(dist <= maxLetters) {
+            String s1 = s.substring(0, minSize);
+            map.put(s1, map.getOrDefault(s1, 0)+1);
+        }
+        for(int i = minSize; i < s.length(); i++) {
+            if(freq[s.charAt(i)-'a']++ == 0) {
+                dist++;
+            }
+            freq[s.charAt(i - minSize)-'a']--;
+            if(freq[s.charAt(i - minSize)-'a'] == 0) {
+                dist--;
+            }
+            if(dist <= maxLetters) {
+                String s2 = s.substring(i - minSize+1, i+1);
+                map.put(s2, map.getOrDefault(s2, 0)+1);
+            }
+        }
+        int ans = 0;
+        for(String key: map.keySet()) {
+            ans = Math.max(ans, map.get(key));
+        }
+        return ans;
     }
 }
