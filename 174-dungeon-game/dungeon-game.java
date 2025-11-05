340c92b7e17aafd8ba9b029b9c27a136babdb289
Krishna Singh <krishnasinghks1225@gmail.com>
2025-11-06 00:38:57 +0530

    Time: 3 ms (24.07%) | Memory: 45.3 MB (48.39%) - LeetSync

--- 174-dungeon-game/dungeon-game.java
+++ 174-dungeon-game/dungeon-game.java
@@ -1,12 +1,13 @@
 class Solution {
     public int calculateMinimumHP(int[][] dungeon) {
-        int m = dungeon.length;
-        int n = dungeon[0].length;
-        int[][] dp = new int[m][n];
-        for(int[] d: dp) {
-            Arrays.fill(d, -1);
-        }
-        return calculateMinHP(dungeon, 0, 0, dp);
+        // int m = dungeon.length;
+        // int n = dungeon[0].length;
+        // int[][] dp = new int[m][n];
+        // for(int[] d: dp) {
+        //     Arrays.fill(d, -1);
+        // }
+        // return calculateMinHP(dungeon, 0, 0, dp);
+        return bottomUp(dungeon);
     }
     public int calculateMinHP(int[][] dungeon, int r, int c, int[][] dp) {
         if(r == dungeon.length - 1 && c == dungeon[0].length - 1) {
@@ -21,5 +22,24 @@ class Solution {
         int down = calculateMinHP(dungeon, r+1, c, dp);
         int right = calculateMinHP(dungeon, r, c+1, dp);
         return dp[r][c] = Math.max(Math.min(down, right) - dungeon[r][c], 1);
+    }
+    public int bottomUp(int[][] dungeon) {
+        int m = dungeon.length;
+        int n = dungeon[0].length;
+        int[][] dp = new int[m][n];
+        for(int r = m - 1; r >= 0; r--) {
+            for(int c = n - 1; c >= 0; c--) {
+                if(r == m - 1 && c == n - 1) {
+                    dp[r][c] = Math.max(1, 1 - dungeon[r][c]);
+                }
+                else {
+                    int down = r+1 >= m?Integer.MAX_VALUE:dp[r+1][c];
+                    int right = c+1 >= n?Integer.MAX_VALUE:dp[r][c+1];
+                    int ans = Math.max(Math.min(down, right) - dungeon[r][c],  1);
+                    dp[r][c] = ans;
+                }
+            }
+        }
+        return dp[0][0];
     }
 }
