411afcdfb9599bd91b72ac2dd7fcf3a60980d010
Krishna Singh <krishnasinghks1225@gmail.com>
2025-11-08 15:42:02 +0530

    Time: 1040 ms (5.69%) | Memory: 278.4 MB (6.50%) - LeetSync

--- 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.java
+++ 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.java
@@ -1,58 +1,64 @@
 class Solution {
     public int[] pathsWithMaxScore(List<String> board) {
-        int[][] dp1 = new int[board.size()][board.get(0).length()];
+        int n = board.size();
+        char[][] grid = new char[n][n];
+        for(int i = 0; i < n; i++) {
+            String s = board.get(i);
+            for(int j = 0; j < n; j++) {
+                grid[i][j] = s.charAt(j);
+            }
+        }
+        grid[0][0] = grid[n - 1][n - 1] = '0';
+        int[][] dp1 = new int[n][n];
         for(int[] d: dp1) {
             Arrays.fill(d, -1);
         }
-        int max = maxPath(board, 0, 0, dp1);
+        int max = maxPath(grid, n - 1, n - 1, dp1);
         if(max < 0) {
             return new int[]{0, 0};
         }
-        int[][][] dp2 = new int[board.size()][board.get(0).length()][max+1];
+        int[][][] dp2 = new int[n][n][max+1];
         for(int[][] d: dp2) {
             for(int[] a: d) {
                 Arrays.fill(a, -1);
             }
         }
-        int count = countPath(board, 0, 0, max, dp2);
+        int count = countPath(grid, n - 1, n - 1, max, dp2);
         return new int[]{max, count};
     }
-    public int maxPath(List<String> board, int r, int c, int[][] dp) {
-        if(r == board.size() - 1 && c == board.get(0).length() - 1) {
+    public int maxPath(char[][] grid, int r, int c, int[][] dp) {
+        if(r == 0 && c == 0) {
             return 0;
         }
-        if(r >= board.size() || c >= board.get(0).length() || board.get(r).charAt(c) == 'X') {
+        if(r < 0 || c < 0 || grid[r][c] == 'X') {
             return Integer.MIN_VALUE;
         }
         if(dp[r][c] != -1) {
             return dp[r][c];
         }
-        int ans = 0;
-        if(r != 0 || c != 0) {
-            ans = (board.get(r).charAt(c)-'0');
-        }
-        int down = maxPath(board, r+1, c, dp);
-        int right = maxPath(board, r, c+1, dp);
-        int diagonal = maxPath(board, r+1, c+1, dp);
-        return dp[r][c] = ans + (Math.max(down, Math.max(right, diagonal)));
+        int val = grid[r][c] - '0';
+        int up = maxPath(grid, r-1, c, dp);
+        int left = maxPath(grid, r, c-1, dp);
+        int up_left = maxPath(grid, r-1, c-1, dp);
+        return dp[r][c] = val + Math.max(up, Math.max(left, up_left));
     }
-    public int countPath(List<String> board, int r, int c, int max, int[][][] dp) {
-        if(r == board.size() - 1 && c == board.get(0).length() - 1) {
-            return max == 0?1:0;
+    public int countPath(char[][] grid, int r, int c, int max, int[][][] dp) {
+        if(r == 0 && c == 0) {
+            if(max == 0) {
+                return 1;
+            }
+            return 0;
         }
-        if(r >= board.size() || c >= board.get(0).length() || board.get(r).charAt(c) == 'X') {
+        if(r < 0 || c < 0 || grid[r][c] == 'X') {
             return 0;
         }
         if(dp[r][c][max] != -1) {
             return dp[r][c][max];
         }
-        int val = 0;
-        if(r != 0 || c != 0) {
-            val = board.get(r).charAt(c)-'0';
-        }
-        int down = countPath(board, r+1, c, max-val, dp);
-        int right = countPath(board, r, c+1, max-val, dp);
-        int diagonal = countPath(board, r+1, c+1, max-val, dp);
-        return dp[r][c][max] = (down + right + diagonal) % 1000000007;
+        int val = grid[r][c] - '0';
+        int up = countPath(grid, r-1, c, max - val, dp);
+        int left = countPath(grid, r, c-1, max - val, dp);
+        int up_left = countPath(grid, r-1, c-1, max - val, dp);
+        return dp[r][c][max] = (up + left + up_left) % 1000000007;
     }
 }
