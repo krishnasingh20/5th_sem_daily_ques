6c82fcaf9ec69efc682a7e1e0e4f742541e97f63
Krishna Singh <krishnasinghks1225@gmail.com>
2025-11-14 19:07:42 +0530

    Time: 9 ms (89.47%) | Memory: 77.4 MB (10.09%) - LeetSync

--- 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.java
@@ -4,18 +4,27 @@ class Solution {
         for(int i = 0; i < queries.length; i++) {
             int r1 = queries[i][0];
             int c1 = queries[i][1];
-            int r2 = queries[i][2];
-            int c2 = queries[i][3];
-            for(int r = r1; r <= r2; r++) {
-                ans[r][c1] += 1;
-                if(c2+1 != n) {
-                    ans[r][c2+1] -= 1;
+            int r2 = queries[i][2]+1;
+            int c2 = queries[i][3]+1;
+            ans[r1][c1]++;
+            if(c2 < n) {
+                ans[r1][c2]--;
+            }
+            if(r2 < n) {
+                ans[r2][c1]--;
+                if(c2 < n) {
+                    ans[r2][c2]++;
                 }
             }
         }
         for(int i = 0; i < n; i++) {
             for(int j = 1; j < n; j++) {
                 ans[i][j] += ans[i][j-1];
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            for(int j = 1; j < n; j++) {
+                ans[j][i] += ans[j-1][i];
             }
         }
         return ans;
