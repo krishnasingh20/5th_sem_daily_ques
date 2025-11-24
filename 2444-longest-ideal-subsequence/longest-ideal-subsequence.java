d5eb25f68dec0da7b5a88beed681ed6040f0d73d
Krishna Singh <krishnasinghks1225@gmail.com>
2025-11-24 22:04:34 +0530

    Time: 17 ms (91.06%) | Memory: 46.3 MB (57.54%) - LeetSync

--- 2444-longest-ideal-subsequence/longest-ideal-subsequence.java
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.java
@@ -1,11 +1,12 @@
 class Solution {
     public int longestIdealString(String s, int k) {
         char[] ch = s.toCharArray();
-        int[][] dp = new int[ch.length][27];
-        for(int[] d: dp) {
-            Arrays.fill(d, -1);
-        }
-        return longestString(ch, 0, 26, k, dp);
+        // int[][] dp = new int[ch.length][27];
+        // for(int[] d: dp) {
+        //     Arrays.fill(d, -1);
+        // }
+        // return longestString(ch, 0, 26, k, dp);
+        return bottomUp(ch, k);
     }
     public int longestString(char[] ch, int i, int prev, int k, int[][] dp) {
         if(i == ch.length) {
@@ -20,5 +21,21 @@ class Solution {
         }
         int exc = longestString(ch, i+1, prev, k, dp);
         return dp[i][prev] =  Math.max(inc, exc);
+    }
+    public int bottomUp(char[] ch, int k) {
+        int[] dp = new int[27];
+        int ans = 0;
+        for(int i = ch.length - 1; i >= 0; i--) {
+            int x = ch[i]-'a';
+            int s = Math.max(0, x-k);
+            int e = Math.min(26, x+k);
+            int val = 0;
+            for(int j = s; j <= e; j++) {
+                val = Math.max(val, dp[j]+1);
+            }
+            dp[x] = Math.max(val, dp[x]);
+            ans = Math.max(ans, val);
+        }
+        return ans;
     }
 }
