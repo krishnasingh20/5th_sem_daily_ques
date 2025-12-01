53a4b0ff3ecd8d5bdedef24a8ede16864aa4434c
Krishna Singh <krishnasinghks1225@gmail.com>
2025-12-01 14:41:51 +0530

    Time: 3 ms (70.33%) | Memory: 53.6 MB (65.13%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,26 +1,14 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
-        for(int i = 0; i < trust.length; i++) {
-            int a = trust[i][0];
-            int b = trust[i][1];
-            if(!map.containsKey(a)) {
-                map.put(a, new ArrayList<>());
-            }
-            map.get(a).add(b);
+        int[] inDegree = new int[n+1];
+        int[] outDegree = new int[n+1];
+        for(int[] t: trust) {
+            inDegree[t[1]]++;
+            outDegree[t[0]]++;
         }
         for(int i = 1; i <= n; i++) {
-            if(!map.containsKey(i)) {
-                boolean flag = false;
-                for(int key: map.keySet()) {
-                    if(!map.get(key).contains(i)) {
-                        flag = true;
-                        break;
-                    }
-                }
-                if(!flag && map.size() == n-1) {
-                    return i;
-                }
+            if(inDegree[i] == n - 1 && outDegree[i] == 0) {
+                return i;
             }
         }
         return -1;
