f6e4a59271ed1ddd837f676cc549633ac2790763
Krishna Singh <krishnasinghks1225@gmail.com>
2025-12-01 21:12:04 +0530

    Time: 24 ms (19.33%) | Memory: 50.6 MB (7.59%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -9,37 +9,24 @@ class Solution {
         }
         int totalTime = 0;
         int count = 0;
-        PriorityQueue<DijkstraPair> pq = new PriorityQueue<>(new Comparator<DijkstraPair>(){
-            @Override
-            public int compare(DijkstraPair a, DijkstraPair b) {
-                return a.time - b.time;
-            }
-        });
-        pq.add(new DijkstraPair(k, 0));
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b)->a[1]-b[1]);
+        pq.add(new int[]{k, 0});
         HashSet<Integer> visited = new HashSet<>();
         while(!pq.isEmpty()) {
-            DijkstraPair rp = pq.poll();//remove
-            if(visited.contains(rp.vtx)) {//ignore if already visited
+            int[] rp = pq.poll();//remove
+            if(visited.contains(rp[0])) {//ignore if already visited
                 continue;
             }
-            visited.add(rp.vtx);//marked visited
-            totalTime = Math.max(totalTime, rp.time);//self work
+            visited.add(rp[0]);//marked visited
+            totalTime = Math.max(totalTime, rp[1]);//self work
             count++;//to know how many node we have visited
-            for(int nbrs: map.get(rp.vtx).keySet()) {//add all unvisited neighbour
+            for(int nbrs: map.get(rp[0]).keySet()) {//add all unvisited neighbour
                 if(!visited.contains(nbrs)) {
-                    int time = rp.time + map.get(rp.vtx).get(nbrs);
-                    pq.add(new DijkstraPair(nbrs, time));
+                    int time = rp[1] + map.get(rp[0]).get(nbrs);
+                    pq.add(new int[]{nbrs, time});
                 }
             }
         }
         return count==n?totalTime:-1;
-    }
-    class DijkstraPair {
-        int vtx;
-        int time;
-        public DijkstraPair(int vtx, int time) {
-            this.vtx = vtx;
-            this.time = time;
-        }
     }
 }
