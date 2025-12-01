6263cceb45d07a5d0e82cec5f9658f5580f4bdaf
Krishna Singh <krishnasinghks1225@gmail.com>
2025-12-01 16:49:51 +0530

    Time: 5 ms (11.47%) | Memory: 50.2 MB (5.47%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,17 +1,6 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
         int n = isConnected.length;
-        for(int i = 0; i < n; i++) {
-            map.put(i, new ArrayList<>());
-        }
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(isConnected[i][j] == 1 && i != j) {
-                    map.get(i).add(j);
-                }
-            }
-        }
         int provinces = 0;
         HashSet<Integer> visited = new HashSet<>();
         Queue<Integer> q = new LinkedList<>();
@@ -28,9 +17,9 @@ class Solution {
                 }
                 visited.add(rv);//marked visited
                 // self work - nothing to do these time
-                for(int nbrs: map.get(rv)) {//add all unvisited neighbour
-                    if(!visited.contains(nbrs)) {
-                        q.add(nbrs);
+                for(int j = 0; j < n; j++) {// add all unvisited neighbour
+                    if(isConnected[rv][j] == 1 && !visited.contains(j)) {
+                        q.add(j);
                     }
                 }
             }
