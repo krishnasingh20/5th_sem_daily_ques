2ac3fb0208ecce10af868cc8ddee9a0db71dd834
Krishna Singh <krishnasinghks1225@gmail.com>
2025-12-10 17:59:27 +0530

    Time: 2 ms (4.64%) | Memory: 46.1 MB (5.17%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,36 +1,36 @@
 class Solution {
+    Boolean[][] dp = new Boolean[101][101];
     public boolean checkValidString(String s) {
-        Boolean[][] dp = new Boolean[s.length()][201];
-        return validate(s.toCharArray(), 0, 0, dp);
+        return validate(s.toCharArray(), 0, 0);
     }
-    public boolean validate(char[] ch, int i, int curr, Boolean[][] dp) {
+    public boolean validate(char[] ch, int i, int curr) {
         if(i == ch.length) {
             if(curr == 0) {
                 return true;
             }
             return false;
         }
-        if(dp[i][curr+100] != null) {
-            return dp[i][curr+100];
+        if(dp[i][curr] != null) {
+            return dp[i][curr];
         }
         if(ch[i] == '(') {
-            dp[i][curr+100] = validate(ch, i+1, curr+1, dp);
+            dp[i][curr] = validate(ch, i+1, curr+1);
         }
         else if(ch[i] == ')' && curr > 0) {
-            dp[i][curr+100] = validate(ch, i+1, curr-1, dp);
+            dp[i][curr] = validate(ch, i+1, curr-1);
         }
         else if(ch[i] == '*') {
-            boolean a = validate(ch, i+1, curr, dp);
-            boolean b = validate(ch, i+1, curr+1, dp);
-            dp[i][curr+100] = (a || b);
+            boolean a = validate(ch, i+1, curr);
+            boolean b = validate(ch, i+1, curr+1);
+            dp[i][curr] = (a || b);
             if(curr > 0) {
-                boolean c = validate(ch, i+1, curr-1, dp);
-                dp[i][curr+100] = (dp[i][curr+100] || c);
+                boolean c = validate(ch, i+1, curr-1);
+                dp[i][curr] = (dp[i][curr] || c);
             }
         }
         else {
-            dp[i][curr+100] = false;
+            dp[i][curr] = false;
         }
-        return dp[i][curr+100];
+        return dp[i][curr];
     }
 }
