3345ad38b9dc70d5d46cd84c861efd72757b4b7a
Krishna Singh <krishnasinghks1225@gmail.com>
2026-01-02 02:33:10 +0530

    Time: 1 ms (35.33%) | Memory: 42.8 MB (11.67%) - LeetSync

--- 365-water-and-jug-problem/water-and-jug-problem.java
+++ 365-water-and-jug-problem/water-and-jug-problem.java
@@ -1,41 +1,42 @@
 class Solution {
-    HashSet<String> visit;
     public boolean canMeasureWater(int x, int y, int target) {
-        visit = new HashSet<>();
-        return dfs(0, 0, x, y, target);
-    }
-    public boolean dfs(int currX, int currY, int x, int y, int target) {
-        if(currX+currY == target) {
-            return true;
-        }
-        String key = currX+"/"+currY;
-        if(visit.contains(key)) {
-            return false;
-        }
-        visit.add(key);
-        // case-a
-        boolean a1 = dfs(x, currY, x, y, target);
-        boolean a2 = dfs(currX, y, x, y, target);
-        if(a1 || a2) {
-            return true;
-        }
-        // case-b
-        boolean b1 = dfs(0, currY, x, y, target);
-        boolean b2 = dfs(currX, 0, x, y, target);
-        if(b1 || b2) {
-            return true;
-        }
-        // case-c
-        //(i)- pour from currX to currY
-        int req1 = y - currY;
-        int temp1 = Math.min(req1, currX);
-        boolean c1 = dfs(currX-temp1, currY+temp1, x, y, target);
-        // (ii)-pour form currY to currX
-        int req2 = x - currX;
-        int temp2 = Math.min(req2, currY);
-        boolean c2 = dfs(currX+temp2, currY, x, y, target);
-        if(c1 || c2) {
-            return true;
+        boolean[][] visited = new boolean[x+1][y+1];
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 0});
+        while(!q.isEmpty()) {
+            int[] rv = q.poll();
+            if(visited[rv[0]][rv[1]]) {
+                continue;
+            }
+            visited[rv[0]][rv[1]] = true;
+            if(rv[0]+rv[1] == target) {
+                return true;
+            }
+            // case-1
+            if(!visited[x][rv[1]]) {
+                q.add(new int[]{x, rv[1]});
+            }
+            if(!visited[rv[0]][y]) {
+                q.add(new int[]{rv[0], y});
+            }
+            // case-2
+            if(!visited[0][rv[1]]) {
+                q.add(new int[]{0, rv[1]});
+            }
+            if(!visited[rv[0]][0]) {
+                q.add(new int[]{rv[0], 0});
+            }
+            // case-3
+            int req1 = y - rv[1];
+            int temp1 = Math.min(req1, rv[0]);
+            if(!visited[rv[0]-temp1][rv[1]+temp1]) {
+                q.add(new int[]{rv[0]-temp1, rv[1]+temp1});
+            }
+            int req2 = x - rv[0];
+            int temp2 = Math.min(req2, rv[1]);
+            if(!visited[rv[0]+temp2][rv[1]-temp2]) {
+                q.add(new int[]{rv[0]+temp2, rv[1]-temp2});
+            }
         }
         return false;
     }
