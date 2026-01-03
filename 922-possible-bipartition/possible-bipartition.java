9a3047cb45b7be51c0f1892cb0cfe3a77a93a506
Krishna Singh <krishnasinghks1225@gmail.com>
2026-01-03 22:11:17 +0530

    Time: 122 ms (5.01%) | Memory: 106.7 MB (5.01%) - LeetSync

--- 922-possible-bipartition/possible-bipartition.java
+++ 922-possible-bipartition/possible-bipartition.java
@@ -8,23 +8,24 @@ class Solution {
             graph[dis[0]].add(dis[1]);
             graph[dis[1]].add(dis[0]);
         }
-        HashMap<Integer, Integer> visited = new HashMap<>();
+        int[] visited = new int[n+1];
+        Arrays.fill(visited, -1);
         Queue<Pair> q = new LinkedList<>();
         for(int i = 1; i <= n; i++) {
-            if(visited.containsKey(i)) {
+            if(visited[i] != -1) {
                 continue;
             }
             q.add(new Pair(i, 0));
             while(!q.isEmpty()) {
                 Pair rv = q.poll();
-                if(visited.containsKey(rv.vtx)) {
-                    if(visited.get(rv.vtx) != rv.dis) {
+                if(visited[rv.vtx] != -1) {
+                    if(visited[rv.vtx] != rv.dis) {
                         return false;//odd length cycle
                     }
                 }
-                visited.put(rv.vtx, rv.dis);
+                visited[rv.vtx] = rv.dis;
                 for(int nbrs: graph[rv.vtx]) {
-                    if(!visited.containsKey(nbrs)) {
+                    if(visited[nbrs] == -1) {
                         q.add(new Pair(nbrs, rv.dis+1));
                     }
                 }
