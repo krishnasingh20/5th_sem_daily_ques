f67ea1e8c91bc5c4f637a603fe49c1ec40857a5b
Krishna Singh <krishnasinghks1225@gmail.com>
2026-01-07 19:14:11 +0530

    Time: 9 ms (47.88%) | Memory: 94.3 MB (86.67%) - LeetSync

--- 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.java
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.java
@@ -2,27 +2,28 @@ class Solution {
     public boolean validPartition(int[] nums) {
         int n = nums.length;
         boolean[] dp = new boolean[n+1];
-        dp[0] = true;
-        dp[1] = false;
-        for(int i = 1; i < n; i++) {
-            //case-1->for 2 equal element
+        dp[0] = true;//before 0th index we can assume that we have valid splits
+        dp[1] = false;//1 length subarray is not valid split
+        if(nums[1] == nums[0]) {
+            dp[2] = true;
+        }
+        for(int i = 2; i < n; i++) {
+            // case-a->when we take two length subarray of all element equal
             boolean a = false;
             if(nums[i] == nums[i-1] && dp[i-1]) {
                 a = true;
             }
+            // case-b-> when we take split of 3 length subarray with all element are equal
             boolean b = false;
-            boolean c = false;
-            if(i >= 2) {
-                //case-2->for 3 equal element
-                if(nums[i] == nums[i-1] && nums[i-1] == nums[i-2] && dp[i-2]) {
-                    b = true;
-                }
-                // case-3->for 3 consecutive increasing element
-                if(nums[i]-nums[i-1] == 1 && nums[i-1] - nums[i-2] == 1 && dp[i-2]) {
-                    c = true;
-                } 
+            if(nums[i] == nums[i-1] && nums[i-1] == nums[i-2] && dp[i-2]) {
+                b = true;
             }
-            dp[i+1] = (a || b || c);
+            // case-c-> when we take 3 length subarray with consecutive increasing element
+            boolean c = false;
+            if(nums[i]-nums[i-1] == 1 && nums[i-1]-nums[i-2] == 1 && dp[i-2]) {
+                c = true;
+            }
+            dp[i+1] = (a || b || c);//take OR of all three case till ith index for valid split
         }
         return dp[n];
     }
