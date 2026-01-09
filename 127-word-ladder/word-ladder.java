e158f6a778131f7b62e331994501f20aa5af1cdb
Krishna Singh <krishnasinghks1225@gmail.com>
2026-01-09 13:34:51 +0530

    Time: 202 ms (19.61%) | Memory: 48.5 MB (48.34%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,61 +1,36 @@
 class Solution {
-    HashMap<String, Integer> map;
     public int ladderLength(String start, String end, List<String> wordList) {
-        map = new HashMap<>();
-        int n = wordList.size();
-        for(int i = 0; i < n; i++) {
-            map.put(wordList.get(i), i);
-        }
-        int len = n;
-        if(!map.containsKey(start)) {
-            map.put(start, n);
-            len++;
-        }
-        int[] steps = new int[len];
-        Arrays.fill(steps, Integer.MAX_VALUE);
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(start, 1));
-        Integer idx2 = map.get(end);
-        if(idx2 == null) {
+        HashSet<String> words = new HashSet<>(wordList);
+        if(!words.contains(end)) {
             return 0;
         }
-        int idx1 = map.get(start);
-        steps[idx1] = 1;
+        int l = start.length();
+        Queue<String> q = new LinkedList<>();
+        q.add(start);
+        int step = 1;
         while(!q.isEmpty()) {
-            Pair rv = q.poll();
-            idx1 = map.get(rv.s);
-            if(idx1 == idx2) {
-                return rv.step;
-            }
-            for(String s: wordList) {
-                idx1 = map.get(s);
-                if(steps[idx1] <= rv.step+1 || diff(rv.s, s) > 1 ) {
-                    continue;
+            int size = q.size();
+            while(size-- > 0) {
+                String rv = q.poll();
+                if(rv.equals(end)) {
+                    return step;
                 }
-                steps[idx1] = rv.step+1;
-                q.add(new Pair(s, rv.step+1));
+                for(int i = 0; i < l; i++) {
+                    for(int j = 0; j < 26; j++) {
+                        char c = (char)(j+'a');
+                        if(c == rv.charAt(i)) {
+                            continue;
+                        }
+                        String newStr = rv.substring(0, i)+c+rv.substring(i+1);
+                        if(words.contains(newStr)) {
+                            q.add(newStr);
+                            words.remove(newStr);
+                        }
+                    }
+                }
             }
+            step++;
         }
         return 0;
-    }
-    public int diff(String s1, String s2) {
-        int i = 0;
-        int n = s1.length();
-        int c = 0;
-        while(i < n) {
-            if(s1.charAt(i) != s2.charAt(i)) {
-                c++;
-            }
-            i++;
-        }
-        return c;
-    }
-    class Pair{
-        String s;
-        int step;
-        Pair(String s, int step) {
-            this.s = s;
-            this.step = step;
-        }
     }
 }
