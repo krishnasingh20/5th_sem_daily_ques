56feefe2182d7d0f1036eed8037d125edaab590e
Krishna Singh <krishnasinghks1225@gmail.com>
2026-01-10 23:44:11 +0530

    Time: 15 ms (13.90%) | Memory: 51.2 MB (5.02%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.java
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.java
@@ -20,4 +20,27 @@ class Solution {
         long skip2 = maxDot(nums1, nums2, i, j+1);
         return dp[i][j] = Math.max(take, Math.max(skip1, skip2));
     }
+    public int bottomUp(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        long[][] dp = new long[n+1][m+1];
+        for(int i = 0; i <= n; i++) {
+            dp[i][m] = Long.MIN_VALUE;
+        }
+        for(int j = 0; j <= m; j++) {
+            dp[n][j] = Long.MIN_VALUE;
+        }
+        for(int i = n-1; i >= 0; i--) {
+            for(int j = m-1; j >= 0; j--) {
+                long take = (long)nums1[i]*nums2[j];
+                if(dp[i+1][j+1] > 0) {
+                    take += dp[i+1][j+1];
+                }
+                long skip1 = dp[i+1][j];
+                long skip2 = dp[i][j+1];
+                dp[i][j] = Math.max(take, Math.max(skip1, skip2));
+            }
+        }
+        return (int)dp[0][0];
+    }
 }
