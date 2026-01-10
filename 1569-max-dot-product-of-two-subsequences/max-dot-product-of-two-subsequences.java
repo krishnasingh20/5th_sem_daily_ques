e1cc6919c784ebff2de5a9402cfbe0487c82012d
Krishna Singh <krishnasinghks1225@gmail.com>
2026-01-10 23:44:56 +0530

    Time: 12 ms (27.27%) | Memory: 46.7 MB (28.22%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.java
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.java
@@ -1,26 +1,8 @@
 class Solution {
-    Long[][] dp;
     public int maxDotProduct(int[] nums1, int[] nums2) {
-        dp = new Long[nums1.length][nums2.length];
-        return (int)maxDot(nums1, nums2, 0, 0);
+        return bottomUP(nums1, nums2);
     }
-    public long maxDot(int[] nums1, int[] nums2, int i, int j) {
-        if(i == nums1.length || j == nums2.length) {
-            return Long.MIN_VALUE;
-        }
-        if(dp[i][j] != null) {
-            return dp[i][j];
-        }
-        long take = (long)nums1[i]*nums2[j];
-        long next = maxDot(nums1, nums2, i+1, j+1);
-        if(next > 0) {
-            take += next;
-        }
-        long skip1 = maxDot(nums1, nums2, i+1, j);
-        long skip2 = maxDot(nums1, nums2, i, j+1);
-        return dp[i][j] = Math.max(take, Math.max(skip1, skip2));
-    }
-    public int bottomUp(int[] nums1, int[] nums2) {
+    public int bottomUP(int[] nums1, int[] nums2) {
         int n = nums1.length;
         int m = nums2.length;
         long[][] dp = new long[n+1][m+1];
