8a6374d46a30138bee83f715dbbe1f5f3f677233
Krishna Singh <krishnasinghks1225@gmail.com>
2026-01-14 18:16:12 +0530

    Time: 4 ms (57.06%) | Memory: 163 MB (75.29%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.java
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.java
@@ -1,22 +1,32 @@
 class Solution {
     public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
-        TreeSet<Integer> set = new TreeSet<>();
+        // below array will tell where the connection is breaking
+        int[] connection = new int[n];
+        Arrays.fill(connection, -1);
         for(int i = 1; i < n; i++) {
-            if(nums[i]-nums[i-1] > maxDiff) {
-                set.add(i-1);
+            if(nums[i] - nums[i-1] > maxDiff) {
+                connection[i-1] = i-1;
+            }
+        }
+        for(int i = n-2; i >= 0; i--) {
+            if(connection[i] != -1) {
+                continue;
+            }
+            if(connection[i+1] != -1) {
+                connection[i] = connection[i+1];
             }
         }
         int m = queries.length;
         boolean[] ans = new boolean[m];
         for(int i = 0; i < m; i++) {
-            int u = Math.min(queries[i][0], queries[i][1]);
-            int v = Math.max(queries[i][0], queries[i][1]);
-            if(u == v) {
+            if(queries[i][0] == queries[i][1]) {
                 ans[i] = true;
                 continue;
             }
-            Integer idx = set.ceiling(u);
-            if(idx != null && idx < v) {
+            //--> take path from u to v (smallest node to largest because if path form one end possible then it will also possible from another end)
+            int u = Math.min(queries[i][0], queries[i][1]);
+            int v = Math.max(queries[i][0], queries[i][1]);
+            if(connection[u] != -1 && connection[u] < v) {
                 continue;
             }
             ans[i] = true;
