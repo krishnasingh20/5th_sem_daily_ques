be45c22dfcb10283457f2bef8d315bef9f9b191e
Krishna Singh <krishnasinghks1225@gmail.com>
2026-01-16 21:45:24 +0530

    Time: 104 ms (40.43%) | Memory: 135.3 MB (81.21%) - LeetSync

--- 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.java
+++ 2117-find-original-array-from-doubled-array/find-original-array-from-doubled-array.java
@@ -1,30 +1,26 @@
 class Solution {
     public int[] findOriginalArray(int[] changed) {
         int n = changed.length;
-        if(n % 2 != 0) {
+        if((n & 1) == 1) {
             return new int[]{};
         }
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < n; i++) {
-            map.put(changed[i], map.getOrDefault(changed[i], 0) + 1);
+            map.put(changed[i], map.getOrDefault(changed[i], 0)+1);
         }
-        // int i = 0;
-        int idx = 0;
-        int[] ans = new int[n/2];
         Arrays.sort(changed);
+        int[] ans = new int[n/2];
+        int idx = 0;
         for(int i = 0; i < n; i++) {
-            int x = changed[i];
-            if(map.get(x) > 0) {
-                map.put(x, map.get(x) - 1);
-                if(map.getOrDefault(2*x, 0) > 0) {
-                    ans[idx++] = x;
-                    map.put(2*x, map.get(2*x) - 1);
+            if(map.get(changed[i]) > 0) {
+                if(map.getOrDefault(changed[i]*2, 0) == 0) {
+                    return new int[]{};
                 }
+                map.put(changed[i]*2, map.get(changed[i]*2)-1);
+                map.put(changed[i], map.get(changed[i])-1);
+                ans[idx++] = changed[i];
             }
         }
-        if(idx == n/2) {
-            return ans;
-        }
-        return new int[]{};
+        return ans;
     }
 }
