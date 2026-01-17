97d15fd259c972f03436c1a4f75008f70af2f835
Krishna Singh <krishnasinghks1225@gmail.com>
2026-01-17 15:33:41 +0530

    Time: 3 ms (98.27%) | Memory: 179.7 MB (69.70%) - LeetSync

--- 3523-find-the-power-of-k-size-subarrays-ii/find-the-power-of-k-size-subarrays-ii.java
+++ 3523-find-the-power-of-k-size-subarrays-ii/find-the-power-of-k-size-subarrays-ii.java
@@ -2,22 +2,26 @@ class Solution {
     public int[] resultsArray(int[] nums, int k) {
         int n = nums.length;
         int[] ans = new int[n-k+1];
-        int[] consecutive = new int[n];
-        consecutive[0] = 1;
-        for(int i = 1; i < n; i++) {
+        Arrays.fill(ans, -1);
+        int consecutive = 1;
+        for(int i = 1; i < k; i++) {
             if(nums[i] == nums[i-1]+1) {
-                consecutive[i] = consecutive[i-1]+1;
+                consecutive++;
+                continue;
             }
-            else {
-                consecutive[i] = 1;
-            }
+            consecutive = 1;
         }
         for(int i = k-1; i < n; i++) {
-            if(consecutive[i] >= k) {
-                ans[i-k+1] = nums[i];
+            if(i >= k) {
+                if(nums[i] == nums[i-1]+1) {
+                    consecutive++;
+                }
+                else {
+                    consecutive = 1;
+                }
             }
-            else {
-                ans[i-k+1] = -1;
+            if(consecutive >= k) {
+                ans[i-k+1] = nums[i];
             }
         }
         return ans;
