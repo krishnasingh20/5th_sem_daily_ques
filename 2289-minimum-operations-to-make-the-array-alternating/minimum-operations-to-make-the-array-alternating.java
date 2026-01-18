5a0070d87486b522b67637e3cb0f19c8f74f3ce4
Krishna Singh <krishnasinghks1225@gmail.com>
2026-01-18 19:56:31 +0530

    Time: 60 ms (74.41%) | Memory: 105.1 MB (64.88%) - LeetSync

--- 2289-minimum-operations-to-make-the-array-alternating/minimum-operations-to-make-the-array-alternating.java
+++ 2289-minimum-operations-to-make-the-array-alternating/minimum-operations-to-make-the-array-alternating.java
@@ -13,15 +13,16 @@ class Solution {
         int a20 = -1;
         int a21 = -1;
         for(int key: map.keySet()) {
-            if(map.get(key) >= a11) {
+            int val = map.get(key);
+            if(val > a11) {
                 a20 = a10;
                 a21 = a11;
                 a10 = key;
-                a11 = map.get(key);
+                a11 = val;
             }
-            else if(map.get(key) >= a21) {
+            else if(val > a21) {
                 a20 = key;
-                a21 = map.get(key);
+                a21 = val;
             }
         }
         map.clear();
@@ -33,36 +34,40 @@ class Solution {
         int b20 = -1;
         int b21 = -1;
         for(int key: map.keySet()) {
-            if(map.get(key) >= b11) {
+            int val = map.get(key);
+            if(val > b11) {
                 b20 = b10;
                 b21 = b11;
                 b10 = key;
-                b11 = map.get(key);
+                b11 = val;
             }
-            else if(map.get(key) >= b21) {
+            else if(val > b21) {
                 b20 = key;
-                b21 = map.get(key);
+                b21 = val;
             }
         }
-        int len1 = n/2 + (n % 2);
+        int len1 = n/2;
+        if((n & 1) == 1) {
+            len1++;
+        }
         int len2 = n/2;
         if(a10 != b10) {
             return (len1-a11) + (len2-b11);
         }
-        else{
+        else {
             if(a20 != -1 && b20 != -1) {
-                int opr1 = (len1-a11) + (len2-b21);
-                int opr2 = (len1-a21) + (len2-b11);
+                int opr1 = (len1-a11)+(len2-b21);
+                int opr2 = (len1-a21)+(len2-b11);
                 return Math.min(opr1, opr2);
             }
-            else if(a20 == -1 && b20 != -1) {
-                int opr1 = (len1-a11) + (len2-b21);
-                int opr2 = len1 + (len2-b11);
+            else if(a20 != -1) {
+                int opr1 = (len1-a11)+len2;
+                int opr2 = (len1-a21)+(len2-b11);
                 return Math.min(opr1, opr2);
             }
-            else if(a20 != -1 && b20 == -1) {
-                int opr1 = (len1 - a11) + len2;
-                int opr2 = (len1-a21) + (len2-b11);
+            else if(b20 != -1) {
+                int opr1 = (len1-a11)+(len2-b21);
+                int opr2 = len1+(len2-b11);
                 return Math.min(opr1, opr2);
             }
             else {
