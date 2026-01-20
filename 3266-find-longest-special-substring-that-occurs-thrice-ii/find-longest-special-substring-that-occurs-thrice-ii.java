60804fc008bf1bb8599ea465e9a1274acfa4175c
Krishna Singh <krishnasinghks1225@gmail.com>
2026-01-20 23:56:46 +0530

    Time: 20 ms (97.46%) | Memory: 50.4 MB (62.71%) - LeetSync

--- 3266-find-longest-special-substring-that-occurs-thrice-ii/find-longest-special-substring-that-occurs-thrice-ii.java
+++ 3266-find-longest-special-substring-that-occurs-thrice-ii/find-longest-special-substring-that-occurs-thrice-ii.java
@@ -1,46 +1,45 @@
 class Solution {
     public int maximumLength(String s) {
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        int[][] count = new int[26][3];
         int c1 = 1;
         int n = s.length();
         for(int i = 1; i < n; i++) {
             if(s.charAt(i) != s.charAt(i-1)) {
                 int c = s.charAt(i-1)-'a';
-                if(map.containsKey(c)) {
-                    map.get(c).add(c1);
+                if(c1 > count[c][2]) {
+                    count[c][0] = count[c][1];
+                    count[c][1] = count[c][2];
+                    count[c][2] = c1;
                 }
-                else {
-                    List<Integer> ll = new ArrayList<>();
-                    ll.add(c1);
-                    map.put(c, ll);
+                else if(c1 > count[c][1]) {
+                    count[c][0] = count[c][1];
+                    count[c][1] = c1;
                 }
-                c1 = 1;
+                else if(c1 > count[c][0]) {
+                    count[c][0] = c1;
+                }
+                c1 = 0;
             }
-            else {
-                c1++;
-            }
+            c1++;
         }
         int c = s.charAt(n-1)-'a';
-        if(map.containsKey(c)) {
-            map.get(c).add(c1);
+        if(c1 > count[c][2]) {
+            count[c][0] = count[c][1];
+            count[c][1] = count[c][2];
+            count[c][2] = c1;
         }
-        else {
-            List<Integer> ll = new ArrayList<>();
-            ll.add(c1);
-            map.put(c, ll);
+        else if(c1 > count[c][1]) {
+            count[c][0] = count[c][1];
+            count[c][1] = c1;
+        }
+        else if(c1 > count[c][0]) {
+            count[c][0] = c1;
         }
         int ans = Integer.MIN_VALUE;
-        for(int key: map.keySet()) {
-            List<Integer> ll = map.get(key);
-            Collections.sort(ll);
-            int n1 = ll.size();
-            if(n1 >= 3) {
-                ans = Math.max(ans, Math.min(Math.min(ll.get(n1-1), ll.get(n1-2)), ll.get(n1-3)));
-            }
-            if(n1 >= 2) {
-                ans = Math.max(ans, Math.min(ll.get(n1-1)-1, ll.get(n1-2)));
-            }
-            ans = Math.max(ans, ll.get(n1-1)-2);
+        for(int i = 0; i < 26; i++) {
+            ans = Math.max(ans, Math.min(Math.min(count[i][2], count[i][1]), count[i][0]));
+            ans = Math.max(ans, Math.min(count[i][2]-1, count[i][1]));
+            ans = Math.max(ans, count[i][2]-2);
         }
         return ans <= 0?-1:ans;
     }
