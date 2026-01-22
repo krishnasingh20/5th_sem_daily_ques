83cee786f57099bd6a961228b8b00a5da187d6a8
Krishna Singh <krishnasinghks1225@gmail.com>
2026-01-22 21:52:37 +0530

    Time: 4 ms (100.00%) | Memory: 60.5 MB (89.34%) - LeetSync

--- 2673-maximize-win-from-two-segments/maximize-win-from-two-segments.java
+++ 2673-maximize-win-from-two-segments/maximize-win-from-two-segments.java
@@ -1,39 +1,30 @@
 class Solution {
-    public int maximizeWin(int[] prizePositions, int k) {
-        int n = prizePositions.length;
+    public int maximizeWin(int[] positions, int k) {
+        int n = positions.length;
         int[] prefix = new int[n+1];
-        for(int i = 0; i < n; i++) {
-            int idx = upperBound(prizePositions, i, k);
-            if(idx - i + 1 == n) {
-                return n;
+        int ei = 0;
+        int si = 0;
+        while(ei < n) {
+            while(positions[ei] - positions[si] > k) {
+                positions[si] = ei;
+                prefix[si] = ei - si;
+                si++;
             }
-            prefix[i] = idx - i + 1;
-            prizePositions[i] = idx;
+            ei++;
+        }
+        while(si < ei) {
+            prefix[si] = ei - si;
+            positions[si] = ei;
+            si++;
         }
         for(int i = n - 1; i >= 0; i--) {
-            prefix[i] = Math.max(prefix[i+1], prefix[i]);
+            prefix[i] = Math.max(prefix[i], prefix[i+1]);
         }
         int ans = Integer.MIN_VALUE;
         for(int i = 0; i < n; i++) {
-            int curr = (prizePositions[i] - i + 1) + prefix[prizePositions[i]+1];
+            int curr = (positions[i] - i) + prefix[positions[i]];
             ans = Math.max(ans, curr);
         }
         return ans;
-    }
-    public int upperBound(int[] positions, int i, int k) {
-        int low = i;
-        int high = positions.length-1;
-        int idx = -1;
-        while(low <= high) {
-            int mid = low + (high - low)/2;
-            if(positions[mid] - positions[i] <= k) {
-                idx = mid;
-                low = mid + 1;
-            }
-            else {
-                high = mid - 1;
-            }
-        }
-        return idx;
     }
 }
