64439578584d9f05065d832f7a3a50fe2bbf2a9f
Krishna Singh <krishnasinghks1225@gmail.com>
2026-01-24 11:25:04 +0530

    Time: 2 ms (24.11%) | Memory: 44.6 MB (12.70%) - LeetSync

--- 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.java
+++ 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.java
@@ -5,16 +5,17 @@ class Solution {
             dsu.create((char)(i+'a'));
         }
         for(String eqn: equations) {
-            dsu.union(eqn);
+            if(eqn.charAt(1) != '!') {
+                dsu.union(eqn.charAt(0), eqn.charAt(3));
+            }
         }
-        for(String s: equations) {
-            char c1 = s.charAt(0);
-            char c2 = s.charAt(3);
-            int val = s.charAt(1)=='='?1:0;
-            char r1 = dsu.find(c1);
-            char r2 = dsu.find(c2);
-            if(val == 0 && r1 == r2) {
-                return false;
+        for(String eqn: equations) {
+            if(eqn.charAt(1) == '!') {
+                char r1 = dsu.find(eqn.charAt(0));
+                char r2 = dsu.find(eqn.charAt(3));
+                if(r1 == r2) {
+                    return false;
+                }
             }
         }
         return true;
@@ -43,18 +44,13 @@ class Solution {
             node.parent = nn;
             return nn;
         }
-        public void union(String s) {
-            char c1 = s.charAt(0);
-            char c2 = s.charAt(3);
-            int val = s.charAt(1)=='='?1:0;
+        public void union(char c1, char c2) {
             Node n1 = map.get(c1);
             Node n2 = map.get(c2);
             Node r1 = find(n1);
             Node r2 = find(n2);
-            if(val == 1) {
-                if(r1 != r2) {
-                    r1.parent = r2;
-                }
+            if(r1 != r2) {
+                r1.parent = r2;
             }
         }
     }
