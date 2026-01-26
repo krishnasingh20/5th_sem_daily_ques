71be69ca619d60a39cfb5e638abdc5d285992b49
Krishna Singh <krishnasinghks1225@gmail.com>
2026-01-26 16:40:14 +0530

    Time: 7 ms (8.48%) | Memory: 47.5 MB (6.48%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.java
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -1,57 +1,42 @@
 class Solution {
     public int longestValidParentheses(String s) {
-        // 0--> '('
-        // 1--> ')'
+        int ans = 0;
         Stack<Integer> st = new Stack<>();
         char[] ch = s.toCharArray();
-        for(int i = 0; i < ch.length; i++) {
+        int n = ch.length;
+        for(int i = 0; i < n; i++) {
             if(ch[i] == '(') {
-                st.push(0);
+                st.push(1);
             }
             else {
                 if(!st.isEmpty()) {
-                    if(st.peek() != 0 && st.peek() != 1) {
-                        int x = st.pop();
-                        if(!st.isEmpty()) {
-                            if(st.peek() == 1) {
-                                st.push(x);
-                                st.push(1);
-                            }
-                            else {
-                                st.pop();
-                                int ans = x + 2;
-                                if(!st.isEmpty() && st.peek() != 0 && st.peek() != 1) {
-                                    ans += st.pop();
-                                }
-                                st.push(ans);
-                            }
-                        }
-                        else {
-                            st.push(x);
-                            st.push(1);
-                        }
-                    }
-                    else if(st.peek() == 0) {
+                    if(st.peek() == 1) {
                         st.pop();
-                        if(!st.isEmpty() && st.peek() != 0 && st.peek() != 1) {
-                            int ans = 2 + st.pop();
-                            st.push(ans);
+                        if(!st.isEmpty() && st.peek() < 0) {
+                            int x = st.pop();
+                            ans = Math.max(ans, Math.abs(x)+2);
+                            st.push((x-2));
+                            continue;
                         }
-                        else {
-                            st.push(2);
+                        st.push(-2);
+                        ans = Math.max(ans, 2);
+                        continue;
+                    }
+                    else if(st.peek() < 0) {
+                        int x = st.pop();
+                        if(!st.isEmpty() && st.peek() == 1) {
+                            st.pop();
+                            x -= 2;
+                            if(!st.isEmpty() && st.peek() < 0) {
+                                x += st.pop();
+                            }
+                            ans = Math.max(ans, Math.abs(x));
+                            st.push(x);
+                            continue;
                         }
                     }
                 }
-                else {
-                    st.push(1);
-                }
-            }
-        }
-        int ans = 0;
-        while(!st.isEmpty()) {
-            int x = st.pop();
-            if(x != 0 && x != 1) {
-                ans = Math.max(ans, x);
+                st.push(2);
             }
         }
         return ans;
