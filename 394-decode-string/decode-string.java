67bb43a990fd2207249295cd87c3b29c5fdf04cf
Krishna Singh <krishnasinghks1225@gmail.com>
2026-01-26 15:48:20 +0530

    Time: 1 ms (83.18%) | Memory: 42.8 MB (64.73%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,49 +1,53 @@
 class Solution {
     public String decodeString(String s) {
+        StringBuilder sb = new StringBuilder();
+        int num = 0;
         Stack<String> st = new Stack<>();
-        int i = s.length()-1;
-        StringBuilder str = new StringBuilder();
-        while(i >= 0) {
-            if(s.charAt(i) == ']') {
-                st.push("]");
-                i--;
+        char[] ch = s.toCharArray();
+        int n = ch.length;
+        for(int i = 0; i < n;) {
+            if(Character.isDigit(ch[i])) {
+                num = 0;
+                while(i < n && Character.isDigit(ch[i])) {
+                    num = num*10 + (ch[i]-'0');
+                    i++;
+                }
+                st.push((num+""));
             }
-            else if(s.charAt(i) == '[') {
+            else if(ch[i] == '[') {
                 st.push("[");
-                i--;
+                i++;
             }
-            else if(Character.isDigit(s.charAt(i))) {
-                int val = 0;
-                int a = 1;
-                while(i >= 0 && Character.isDigit(s.charAt(i))) {
-                    int x = s.charAt(i) - '0';
-                    val = val + x*a;
-                    a *= 10;
-                    i--;
+            else if(Character.isLowerCase(ch[i])) {
+                while(i < n && Character.isLowerCase(ch[i])) {
+                    sb.append(ch[i]);
+                    i++;
                 }
-                st.pop();//to remove open bracket
-                while(val-- > 0) {
-                    str.append(st.peek());
+                s = sb.toString();
+                sb.setLength(0);
+                while(!st.isEmpty() && Character.isLowerCase(st.peek().charAt(0))) {
+                    sb.append(st.pop());
                 }
-                st.pop();//to remove string that is on top
-                st.pop();//to rmove close bracket
-                if(!st.isEmpty() && st.peek().charAt(0) != ']') {
-                    str.append(st.pop());
-                }
-                st.push(str.toString());
-                str.setLength(0);
+                sb.append(s);
+                st.push(sb.toString());
+                sb.setLength(0);
             }
             else {
-                while(i >= 0 && s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
-                    str.append(s.charAt(i));
-                    i--;
+                s = st.pop();
+                st.pop();
+                int repeat = Integer.parseInt(st.pop());
+                for(int j = 1; j <= repeat; j++) {
+                    sb.append(s);
                 }
-                str.reverse();
-                if(!st.isEmpty() && st.peek().charAt(0) != ']') {
-                    str.append(st.pop());
+                s = sb.toString();
+                sb.setLength(0);
+                while(!st.isEmpty() && Character.isLowerCase(st.peek().charAt(0))) {
+                    sb.append(st.pop());
                 }
-                st.push(str.toString());
-                str.setLength(0);
+                sb.append(s);
+                st.push(sb.toString());
+                sb.setLength(0);
+                i++;
             }
         }
         return st.pop();
