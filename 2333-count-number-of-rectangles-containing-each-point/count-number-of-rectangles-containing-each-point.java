e567391898ea9c1b4d02bcd9ed3bf822928b8895
Krishna Singh <krishnasinghks1225@gmail.com>
2026-01-28 22:55:49 +0530

    Time: 559 ms (5.39%) | Memory: 95.2 MB (28.74%) - LeetSync

---
+++ 2333-count-number-of-rectangles-containing-each-point/count-number-of-rectangles-containing-each-point.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[] countRectangles(int[][] rectangles, int[][] points) {
+        TreeMap<Integer, List<Integer>> map = new TreeMap<>();
+        for(int[] rect: rectangles) {
+            map.putIfAbsent(rect[1], new ArrayList<>());
+            map.get(rect[1]).add(rect[0]);
+        }
+        for(int key: map.keySet()) {
+            List<Integer> ll = map.get(key);
+            Collections.sort(ll);
+        }
+        int[] ans = new int[points.length];
+        for(int i = 0; i < points.length; i++) {
+            int x = points[i][0];
+            int y = points[i][1];
+            for(int key: map.keySet()) {
+                if(key >= y) {
+                    List<Integer> ll = map.get(key);
+                    int n = ll.size();
+                    int low = 0;
+                    int high = n-1;
+                    int idx = -1;
+                    while(low <= high) {
+                        int mid = low + (high - low)/2;
+                        if(ll.get(mid) >= x) {
+                            idx = mid;
+                            high = mid - 1;
+                        }
+                        else {
+                            low = mid + 1;
+                        }
+                    }
+                    if(idx != -1) {
+                        ans[i] += (n-idx);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
