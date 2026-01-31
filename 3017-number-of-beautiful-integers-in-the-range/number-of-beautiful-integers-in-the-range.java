579b410834ba93e3220f7aa3ff07c38845ab0cfb
Krishna Singh <krishnasinghks1225@gmail.com>
2026-01-31 19:05:05 +0530

    Time: 148 ms (38.23%) | Memory: 47 MB (41.18%) - LeetSync

--- 3017-number-of-beautiful-integers-in-the-range/number-of-beautiful-integers-in-the-range.java
+++ 3017-number-of-beautiful-integers-in-the-range/number-of-beautiful-integers-in-the-range.java
@@ -1,26 +1,26 @@
 class Solution {
-    Integer[][][][][][] dp;
+    Integer[][][][][] dp;
     public int numberOfBeautifulIntegers(int low, int high, int k) {
         return solve(low, high, k);
     }
     public int solve(int low, int high, int k) {
         String l = String.valueOf(low-1);
         String r = String.valueOf(high);
-        dp = new Integer[10][2][10][10][k][2];
-        int ans_r = count(r, 0, 1, 0, 0, 0, 1, k);
-        dp = new Integer[10][2][10][10][k][2];
-        int ans_l = count(l, 0, 1, 0, 0, 0, 1, k);
+        dp = new Integer[10][2][20][k][2];
+        int ans_r = count(r, 0, 1, 0, 0, 1, k);
+        dp = new Integer[10][2][20][k][2];
+        int ans_l = count(l, 0, 1, 0, 0, 1, k);
         return ans_r - ans_l;
     }
-    private int count(String s, int idx, int tight, int even, int odd, int rem, int lz, int k) {
+    private int count(String s, int idx, int tight, int cntEvenOdd, int rem, int lz, int k) {
         if(idx == s.length()) {
-            if(even == odd && rem == 0) {
+            if(cntEvenOdd == 0 && rem == 0) {
                 return 1;
             }
             return 0;
         }
-        if(dp[idx][tight][even][odd][rem][lz] != null) {
-            return dp[idx][tight][even][odd][rem][lz];
+        if(dp[idx][tight][cntEvenOdd+10][rem][lz] != null) {
+            return dp[idx][tight][cntEvenOdd+10][rem][lz];
         }
         int lb = 0;
         int ub = (tight==1)?(s.charAt(idx)-'0'):9;
@@ -29,16 +29,15 @@ class Solution {
             int newRem = (rem*10 + digit) % k;
             int newTight = (tight == 1 && digit == ub)?1:0;
             int newLz = (lz == 1 && digit == 0)?1:0;
-            int newEven = even;
-            int newOdd = odd;
+            int newcntEvenOdd = cntEvenOdd;
             if((digit & 1) == 1) {
-                newOdd++;
+                newcntEvenOdd++;
             }
             else if((digit == 0 && lz == 0) || digit > 0) {
-                newEven++;
+                newcntEvenOdd--;
             }
-            res += count(s, idx+1, newTight, newEven, newOdd, newRem, newLz, k);
+            res += count(s, idx+1, newTight, newcntEvenOdd, newRem, newLz, k);
         }
-        return dp[idx][tight][even][odd][rem][lz] = res;
+        return dp[idx][tight][cntEvenOdd+10][rem][lz] = res;
     }
 }
