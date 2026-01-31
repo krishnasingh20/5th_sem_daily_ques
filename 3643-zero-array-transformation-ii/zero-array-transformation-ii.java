6c6f4496beb62b8d78a3ec8bbd2ed47d63cc3ede
Krishna Singh <krishnasinghks1225@gmail.com>
2026-01-31 11:51:50 +0530

    Time: 33 ms (31.03%) | Memory: 230.8 MB (55.67%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
@@ -1,11 +1,6 @@
 class Solution {
-    int[] prefix;
-    int n;
-    int m;
     public int minZeroArray(int[] nums, int[][] queries) {
-        n = nums.length;
-        m = queries.length;
-        prefix = new int[n+1];
+        int m = queries.length;
         int ans = -1;
         int low = -1;
         int high = m-1;
@@ -22,19 +17,20 @@ class Solution {
         return ans;
     }
     private boolean possible(int[][] queries, int[] nums, int mid) {
-        Arrays.fill(prefix, 0);
+        int n = nums.length;
+        int[] diff = new int[n+1];
         for(int i = 0; i <= mid; i++) {
             int l = queries[i][0];
             int r = queries[i][1]+1;
             int val = queries[i][2];
-            prefix[l] += val;
-            prefix[r] -= val;
+            diff[l] += val;
+            diff[r] -= val;
         }
         for(int i = 1; i <= n; i++) {
-            prefix[i] += prefix[i-1];
+            diff[i] += diff[i-1];
         }
         for(int i = 0; i < n; i++) {
-            if(prefix[i] < nums[i]) {
+            if(diff[i] < nums[i]) {
                 return false;
             }
         }
