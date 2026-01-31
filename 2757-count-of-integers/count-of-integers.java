87dfaf66389d182090923d411d5b34fc0577f131
Krishna Singh <krishnasinghks1225@gmail.com>
2026-02-01 00:57:50 +0530

    Time: 20 ms (43.98%) | Memory: 47.2 MB (66.27%) - LeetSync

--- 2757-count-of-integers/count-of-integers.java
+++ 2757-count-of-integers/count-of-integers.java
@@ -1,11 +1,8 @@
-import java.math.BigInteger;
 class Solution {
     Long[][][] dp;
     static int mod = 1000000007;
     public int count(String num1, String num2, int minSum, int maxSum) {
-        BigInteger a = new BigInteger(num1);
-        BigInteger b = BigInteger.valueOf(1);
-        num1 = (a.subtract(b)).toString();
+        num1 = decreOne(num1);
         dp = new Long[23][2][maxSum+1];
         long ans_r = countGood(num2, 0, 1, 0, minSum, maxSum);
         dp = new Long[23][2][maxSum+1];
@@ -33,5 +30,39 @@ class Solution {
             ans += countGood(s, idx+1, newTight, sum+digit, minSum, maxSum) % mod;
         }
         return dp[idx][tight][sum] = ans % mod;
+    }
+    private String decreOne(String s) {
+        int[] arr = new int[s.length()];
+        for(int i = 0; i < s.length(); i++) {
+            arr[i] = s.charAt(i)-'0';
+        }
+        int carry = 0;
+        for(int i = arr.length-1; i >= 0; i--) {
+            if(arr[i] == 0) {
+                arr[i] = 9;
+                carry = 1;
+            }
+            else if(arr[i] > 0) {
+                if(carry == 1) {
+                    arr[i] -= 1;
+                    break;
+                }
+                arr[i] -= 1;
+                break;
+            }
+        }
+        int i = 0;
+        while(i < arr.length && arr[i] == 0) {
+            i++;
+        }
+        if(i == arr.length) {
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        while(i < arr.length) {
+            sb.append(arr[i]);
+            i++;
+        }
+        return sb.toString();
     }
 }
