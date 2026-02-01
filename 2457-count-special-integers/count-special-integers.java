5eb443ee45aea5c38dd735631a6c025f4e9ae05a
Krishna Singh <krishnasinghks1225@gmail.com>
2026-02-01 11:42:28 +0530

    Time: 1845 ms (6.33%) | Memory: 42.1 MB (97.47%) - LeetSync

--- 2457-count-special-integers/count-special-integers.java
+++ 2457-count-special-integers/count-special-integers.java
@@ -1,8 +1,8 @@
 class Solution {
     public int countSpecialNumbers(int n) {
-        return count(String.valueOf(n), 0, 1, 1, new boolean[10])-1;//-1 for case when it will count 0 also but needed in range [1, n].
+        return count(String.valueOf(n), 0, 1, 1, 0)-1;//-1 for case when it will count 0 also but needed in range [1, n].
     }
-    private int count(String s, int idx, int tight, int lz, boolean[] visited) {
+    private int count(String s, int idx, int tight, int lz, int mask) {
         if(idx == s.length()) {
             return 1;
         }
@@ -10,19 +10,19 @@ class Solution {
         int ub = tight==1?(s.charAt(idx)-'0'):9;
         int ans = 0;
         for(int digit = lb; digit <= ub; digit++) {
-            if(lz == 0 && visited[digit]) {
+            if(lz == 0 && (mask & (1<<digit)) != 0) {
                 continue;
             }
             int newTight = (tight==1 && digit==ub)?1:0;
             int newLz = (lz==1 && digit == 0)?1:0;
-            if(digit == 0 && lz == 0) {
-                visited[digit] = true;
+            int newMask = mask;
+            if(lz == 0 && digit == 0) {
+                newMask = mask | (1<<digit);
             }
             else if(digit > 0) {
-                visited[digit] = true;
+                newMask = mask | (1<<digit);
             }
-            ans += count(s, idx+1, newTight, newLz, visited);
-            visited[digit] = false;
+            ans += count(s, idx+1, newTight, newLz, newMask);
         }
         return ans;
     }
