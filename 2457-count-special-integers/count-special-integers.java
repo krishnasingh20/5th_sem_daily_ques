37e4f0373dceaafef0e84db7b4b0cbb30e7dbf5c
Krishna Singh <krishnasinghks1225@gmail.com>
2026-02-01 10:36:39 +0530

    Time: 1874 ms (6.33%) | Memory: 42.4 MB (88.61%) - LeetSync

---
+++ 2457-count-special-integers/count-special-integers.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int countSpecialNumbers(int n) {
+        return count(String.valueOf(n), 0, 1, 1, new boolean[10])-1;//-1 for case when it will count 0 for also but needed in range [1, n].
+    }
+    private int count(String s, int idx, int tight, int lz, boolean[] visited) {
+        if(idx == s.length()) {
+            return 1;
+        }
+        int lb = 0;
+        int ub = tight==1?(s.charAt(idx)-'0'):9;
+        int ans = 0;
+        for(int digit = lb; digit <= ub; digit++) {
+            if(lz == 0 && visited[digit]) {
+                continue;
+            }
+            int newTight = (tight==1 && digit==ub)?1:0;
+            int newLz = (lz==1 && digit == 0)?1:0;
+            if(digit == 0 && lz == 0) {
+                visited[digit] = true;
+            }
+            else if(digit > 0) {
+                visited[digit] = true;
+            }
+            ans += count(s, idx+1, newTight, newLz, visited);
+            visited[digit] = false;
+        }
+        return ans;
+    }
+}
