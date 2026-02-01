543ab57a7168346909a352e4f364be0bdab6e6eb
Krishna Singh <krishnasinghks1225@gmail.com>
2026-02-01 22:30:58 +0530

    Time: 113 ms (10.33%) | Memory: 88.8 MB (91.55%) - LeetSync

--- 1946-minimum-absolute-sum-difference/minimum-absolute-sum-difference.java
+++ 1946-minimum-absolute-sum-difference/minimum-absolute-sum-difference.java
@@ -1,27 +1,58 @@
 class Solution {
+    int[] arr;
     public int minAbsoluteSumDiff(int[] nums1, int[] nums2) {
-        TreeSet<Integer> set = new TreeSet<>();
-        for(int num:nums1) {
-            set.add(num);
-        }
+        arr = nums1.clone();
+        Arrays.sort(arr);
+        long ans = 0;
         int n = nums1.length;
-        long ans  = 0;
         for(int i = 0; i < n; i++) {
             ans += Math.abs(nums1[i]-nums2[i]);
         }
         long ans1 = ans;
         for(int i = 0; i < n; i++) {
             int curr = Math.abs(nums1[i]-nums2[i]);
-            Integer lb = set.floor(nums2[i]);
-            Integer ub = set.ceiling(nums2[i]);
-            if(lb != null) {
-                ans1 = Math.min(ans1, ans-curr+Math.abs(lb-nums2[i]));
+            int idx1 = lowerBound(nums2[i]);
+            int idx2 = upperBound(nums2[i]);
+            if(idx1 != -1) {
+                ans1 = Math.min(ans1, ans-curr+Math.abs(arr[idx1]-nums2[i]));
             }
-            if(ub != null) {
-                ans1 = Math.min(ans1, ans-curr+Math.abs(ub-nums2[i]));
+            if(idx2 != -1) {
+                ans1 = Math.min(ans1, ans-curr+Math.abs(arr[idx2]-nums2[i]));
             }
         }
-        ans = (Math.min(ans, ans1)) % 1000000007;
+        ans = Math.min(ans, ans1) % 1000000007;
         return (int)ans;
+    }
+    private int lowerBound(int target) {
+        int idx = -1;
+        int low = 0;
+        int high = arr.length-1;
+        while(low <= high) {
+            int mid = low + (high-low)/2;
+            if(arr[mid] <= target) {
+                idx = mid;
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return idx;
+    }
+    private int upperBound(int target) {
+        int idx = -1;
+        int low = 0;
+        int high = arr.length-1;
+        while(low <= high) {
+            int mid = low + (high-low)/2;
+            if(arr[mid] >= target) {
+                idx = mid;
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return idx;
     }
 }
