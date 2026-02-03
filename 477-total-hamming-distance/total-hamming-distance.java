b47affc9a14ceb7b8937b42ea311206f5110a191
Krishna Singh <krishnasinghks1225@gmail.com>
2026-02-03 21:25:46 +0530

    Time: 16 ms (43.37%) | Memory: 48.1 MB (66.33%) - LeetSync

--- 477-total-hamming-distance/total-hamming-distance.java
+++ 477-total-hamming-distance/total-hamming-distance.java
@@ -5,21 +5,15 @@ class Solution {
     private int totalHammingDis(int[] nums) {
         int n = nums.length;
         int ans = 0;
-        int[][] count = new int[n][31];
-        for(int i = 0; i <= 30; i++) {
-            if((nums[0] & (1<<i)) != 0) {
-                count[0][i] = 1;
-            }
-        }
-        for(int i = 1; i < n; i++) {
-            for(int j = 0; j <= 30; j++) {
-                if((nums[i] & (1<<j)) != 0) {
-                    ans += (i - count[i-1][j]);
-                    count[i][j] = count[i-1][j] + 1;
+        for(int i = 0; i<= 30; i++) {
+            int one = 0;
+            for(int j = 0; j < n; j++) {
+                if((nums[j] & (1<<i)) != 0) {
+                    ans += (j - one);
+                    one++;
                 }
                 else {
-                    ans += count[i-1][j];
-                    count[i][j] = count[i-1][j];
+                    ans += one;
                 }
             }
         }
