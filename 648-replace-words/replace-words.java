5e4c0cfc035544ef8ccf39a40213e563238612fd
Krishna Singh <krishnasinghks1225@gmail.com>
2026-02-05 23:56:47 +0530

    Time: 14 ms (60.93%) | Memory: 66.7 MB (13.54%) - LeetSync

---
+++ 648-replace-words/replace-words.java
@@ -0,0 +1,63 @@
+class Solution {
+    class Trie {
+        class Node {
+            char ch;
+            String isTerminal;
+            HashMap<Character, Node> child;
+            Node(char ch) {
+                this.ch = ch;
+                this.child = new HashMap<>();
+            }
+        }
+        private Node root;
+        public Trie() {
+            root = new Node('*');
+        }
+
+        public void insert(String word) {
+            Node curr = root;
+            for(int i = 0; i < word.length(); i++) {
+                char ch = word.charAt(i);
+                if(curr.child.containsKey(ch)) {
+                    curr = curr.child.get(ch);
+                }
+                else {
+                    Node nn = new Node(ch);
+                    curr.child.put(ch, nn);
+                    curr = nn;
+                }
+            }
+            curr.isTerminal = word;
+        }
+        public String search(String word) {
+            Node curr = root;
+            for(int i = 0; i < word.length(); i++) {
+                char ch = word.charAt(i);
+                if(curr.child.containsKey(ch)) {
+                    curr = curr.child.get(ch);
+                    if(curr.isTerminal != null) {
+                        return curr.isTerminal;
+                    }
+                }
+                else {
+                    if(curr.isTerminal != null) {
+                        return curr.isTerminal;
+                    }
+                    return word;
+                }
+            }
+            return word;
+        }
+    }
+    public String replaceWords(List<String> dictionary, String sentence) {
+        Trie trie = new Trie();
+        for(String dict: dictionary) {
+            trie.insert(dict);
+        }
+        String[] arr = sentence.split(" ");
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = trie.search(arr[i]);
+        }
+        return String.join(" ", arr);
+    }
+}
