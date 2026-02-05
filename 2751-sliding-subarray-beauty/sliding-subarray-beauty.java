a6dc0f4bec7bb0a15a5ce6a94591167c1cfa0aaf
Krishna Singh <krishnasinghks1225@gmail.com>
2026-02-05 23:30:33 +0530

    Time: 14 ms (99.11%) | Memory: 184.6 MB (36.01%) - LeetSync

--- 2751-sliding-subarray-beauty/sliding-subarray-beauty.java
+++ 2751-sliding-subarray-beauty/sliding-subarray-beauty.java
@@ -1,5 +1,5 @@
 class Solution {
-    TreeMap<Integer, Integer> map = new TreeMap<>();
+    int[] freq = new int[101];
     public int[] getSubarrayBeauty(int[] nums, int k, int x) {
         int n = nums.length;
         int[] ans = new int[n-k+1];
@@ -7,18 +7,18 @@ class Solution {
         for(int i = 0; i < k; i++) {
             if(nums[i] < 0) {
                 cnt++;
-                map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+                freq[nums[i]+50]++;
             }
         }
         ans[0] = cnt < x?0:getFreq(x);
         for(int i = k; i < n; i++) {
             if(nums[i-k] < 0) {
-                map.put(nums[i-k], map.get(nums[i-k])-1);
+                freq[nums[i-k]+50]--;
                 cnt--;
             }
             if(nums[i] < 0) {
                 cnt++;
-                map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
+                freq[nums[i]+50]++;
             }
             ans[i-k+1] = cnt < x?0:getFreq(x);
         }
@@ -26,10 +26,10 @@ class Solution {
     }
     private int getFreq(int x) {
         int cnt = 0;
-        for(int key: map.keySet()) {
-            cnt += map.get(key);
+        for(int i = 0; i < 101; i++) {
+            cnt += freq[i];
             if(cnt >= x) {
-                return key;
+                return i-50;
             }
         }
         return -1;
