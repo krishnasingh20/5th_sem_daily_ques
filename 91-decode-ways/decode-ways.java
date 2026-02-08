d3846e3fc6f854e693cdfb153f49132b5da33bbb
Krishna Singh <krishnasinghks1225@gmail.com>
2026-02-08 17:39:15 +0530

    Time: 1 ms (85.22%) | Memory: 43.2 MB (64.98%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -1,27 +1,24 @@
 class Solution {
-    int[] dp;
     public int numDecodings(String s) {
-        dp = new int[s.length()];
-        Arrays.fill(dp, -1);
-        return decode(s.toCharArray(), 0);
+        return bottomUp(s.toCharArray());
     }
-    public int decode(char[] ch, int i) {
-        if(i == ch.length) {
-            return 1;
-        }
-        if(dp[i] != -1) {
-            return dp[i];
-        }
-        int ans = 0;
-        if(ch[i] != '0') {
-            ans += decode(ch, i+1);
-        }
-        if(i+1 < ch.length) {
-            int num = (ch[i]-'0')*10 + (ch[i+1]-'0');
-            if(num >= 10 && num <= 26) {
-                ans += decode(ch, i+2);
+    public int bottomUp(char[] ch) {
+        int n = ch.length;
+        int[] dp = new int[n+2];
+        dp[n] = 1;
+        for(int i = n-1; i >= 0; i--) {
+            int ans = 0;
+            if(ch[i] != '0') {
+                ans += dp[i+1];
             }
+            if(i+1 < ch.length) {
+                int num = (ch[i]-'0')*10 + (ch[i+1]-'0');
+                if(num >= 10 && num <= 26) {
+                    ans += dp[i+2];
+                }
+            }
+            dp[i] = ans;
         }
-        return dp[i] = ans;
+        return dp[0];
     }
 }
