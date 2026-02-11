f032d01e3fec8fd972e47ddf15f584950be88a70
Krishna Singh <krishnasinghks1225@gmail.com>
2026-02-11 18:51:24 +0530

    Time: 1 ms (100.00%) | Memory: 59.7 MB (46.49%) - LeetSync

--- 2402-maximum-xor-after-operations/maximum-xor-after-operations.java
+++ 2402-maximum-xor-after-operations/maximum-xor-after-operations.java
@@ -2,12 +2,7 @@ class Solution {
     public int maximumXOR(int[] nums) {
         int ans = 0;
         for(int num: nums) {
-            for(int i = 0; i <= 30; i++) {
-                int mask = (1<<i);
-                if((num & mask) != 0) {
-                    ans |= mask;
-                }
-            }
+            ans |= num;
         }
         return ans;
     }
