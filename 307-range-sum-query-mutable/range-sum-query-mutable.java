040c99a81c62aef4f5a265dadd30e36dfd582d7c
Krishna Singh <krishnasinghks1225@gmail.com>
2026-02-15 19:15:42 +0530

    Time: 93 ms (59.75%) | Memory: 134.1 MB (66.21%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/range-sum-query-mutable.java
@@ -1,50 +1,51 @@
 class NumArray {
-    int[] arr;
-    Fenwick ft;
+    int[] tree;
+    int n;
     public NumArray(int[] nums) {
-        int n = nums.length;
-        arr = new int[n];
-        ft = new Fenwick(n);
-
-        for(int i = 0; i < n; i++) {
-            arr[i] = nums[i];
-            ft.update(i+1, nums[i]);
-        }
+        this.n = nums.length;
+        this.tree = new int[4*n];
+        build(nums, 1, 0, n-1);
     }
-    
+    void build(int[] arr, int idx, int start, int end) {
+        if(start == end) {
+            tree[idx] = arr[start];
+            return;
+        }
+        int mid = (start + end)/2;
+        build(arr, 2*idx, start, mid);
+        build(arr, 2*idx+1, mid+1, end);
+        tree[idx] = tree[2*idx]+tree[2*idx+1];
+    }
+    void update(int idx, int start, int end, int pos, int val) {
+        if(start == end) {
+            tree[idx] = val;
+            return;
+        }
+        int mid = (start+end)/2;
+        if(pos <= mid) {
+            update(2*idx, start, mid, pos, val);
+        }
+        else {
+            update(2*idx+1, mid+1, end, pos, val);
+        }
+        tree[idx] = tree[2*idx] + tree[2*idx+1];
+    }
+    int query(int idx, int start, int end, int ql, int qr) {
+        if(qr < start || ql > end) {
+            return 0;
+        }
+        if(ql <= start && end <= qr) {
+            return tree[idx];
+        }
+        int mid = (start+end)/2;
+        return query(2*idx, start, mid, ql, qr) + query(2*idx+1, mid+1, end, ql, qr);
+    }
     public void update(int index, int val) {
-        int delta = val - arr[index];
-        arr[index] = val;
-        ft.update(index+1, delta);
+        update(1, 0, n-1, index, val);
     }
     
     public int sumRange(int left, int right) {
-        return ft.query(right+1) - ft.query(left);
-    }
-    class Fenwick {
-        int n;
-        int[] bit;
-        Fenwick(int n) {
-            this.n = n;
-            bit = new int[n+1];
-        }
-        int lsb(int x) {
-            return x & -x;
-        }
-        void update(int i, int delta) {
-            while(i <= n) {
-                bit[i] += delta;
-                i += lsb(i);
-            }
-        }
-        int query(int i) {
-            int sum = 0;
-            while(i > 0) {
-                sum += bit[i];
-                i -= lsb(i);
-            }
-            return sum;
-        }
+        return query(1, 0, n-1, left, right);
     }
 }
 
