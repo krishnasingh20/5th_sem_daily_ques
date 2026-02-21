1b842cd73a5119e3982b13e54e2989ee65f674e3
Krishna Singh <krishnasinghks1225@gmail.com>
2026-02-21 15:10:45 +0530

    Time: 0 ms (100.00%) | Memory: 72.4 MB (49.14%) - LeetSync

--- 687-longest-univalue-path/longest-univalue-path.cpp
+++ 687-longest-univalue-path/longest-univalue-path.cpp
@@ -13,38 +13,38 @@
     int val;
     int count;
     int maxPath;
-    Pair(int val, int count, int maxPath) {
-        this->val = val;
-        this->count = count;
-        this->maxPath = maxPath;
+    Pair(int v, int c, int m) {
+        val = v;
+        count = c;
+        maxPath = m;
     }
  };
 class Solution {
 public:
     int longestUnivaluePath(TreeNode* root) {
-        return univaluePath(root)->maxPath;
+        return univaluePath(root).maxPath;
     }
-    Pair* univaluePath(TreeNode* root) {
+    Pair univaluePath(TreeNode* root) {
         if(root == nullptr) {
-            return new Pair(-1001, 0, 0);
+            return Pair(-1001, 0, 0);
         }
-        Pair* lp = univaluePath(root->left);
-        Pair* rp = univaluePath(root->right);
-        Pair* sp = new Pair(root->val, 1, 0);
-        if(lp->val == root->val && rp->val == root->val) {
-            sp->maxPath = max(max(lp->maxPath, rp->maxPath), lp->count+rp->count);
-            sp->count = max(lp->count, rp->count)+1;
+        Pair lp = univaluePath(root->left);
+        Pair rp = univaluePath(root->right);
+        Pair sp(root->val, 1, 0);
+        if(lp.val == root->val && rp.val == root->val) {
+            sp.maxPath = max(max(lp.maxPath, rp.maxPath), lp.count+rp.count);
+            sp.count = max(lp.count, rp.count)+1;
         }
-        else if(lp->val == root->val) {
-            sp->maxPath = max(lp->count, max(lp->maxPath, rp->maxPath));
-            sp->count = lp->count+1;
+        else if(lp.val == root->val) {
+            sp.maxPath = max(lp.count, max(lp.maxPath, rp.maxPath));
+            sp.count = lp.count+1;
         }
-        else if(rp->val == root->val) {
-            sp->maxPath = max(rp->count, max(lp->maxPath, rp->maxPath));
-            sp->count = rp->count+1;
+        else if(rp.val == root->val) {
+            sp.maxPath = max(rp.count, max(lp.maxPath, rp.maxPath));
+            sp.count = rp.count+1;
         }
         else {
-            sp->maxPath = max(lp->maxPath, rp->maxPath);
+            sp.maxPath = max(lp.maxPath, rp.maxPath);
         }
         return sp;
     }
