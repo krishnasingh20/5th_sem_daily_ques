b77a0c7e6488dd8f699e17bca4437da4cd9d04aa
Krishna Singh <krishnasinghks1225@gmail.com>
2026-03-10 14:44:26 +0530

    Time: 61 ms (27.78%) | Memory: 78.8 MB (55.56%) - LeetSync

--- 2513-number-of-pairs-satisfying-inequality/number-of-pairs-satisfying-inequality.java
+++ 2513-number-of-pairs-satisfying-inequality/number-of-pairs-satisfying-inequality.java
@@ -5,40 +5,60 @@ class Solution {
             nums1[i] = nums1[i] - nums2[i];
             nums2[i] = nums1[i];
         }
-        TreeMap<Integer, Integer> compress = new TreeMap<>();
-        int idx = 1;
+        ArrayList<int[]> compress = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         Arrays.sort(nums2);
-        for(int i = 0; i < n; i++) {
-            if(!compress.containsKey(nums2[i])) {
-                compress.put(nums2[i], idx++);
+        compress.add(new int[]{nums2[0], 1});
+        map.put(nums2[0], 1);
+        int idx = 1;
+        for(int i = 1; i < n; i++) {
+            if(compress.get(idx-1)[0] != nums2[i]) {
+                map.put(nums2[i], ++idx);
+                compress.add(new int[]{nums2[i], idx});
             }
         }
         Fenwick f = new Fenwick(idx);
         long ans = 0;
         for(int i = 0; i < n; i++) {
             int curr = nums1[i] + diff;
-            Integer id = compress.floorKey(curr);
-            if(id != null) {
-                ans += f.query(compress.get(id));
+            int id = binarySearch(compress, curr);
+            if(id != -1) {
+                ans += f.query(compress.get(id)[1]);
             }
-            f.update(compress.get(nums1[i]), 1);
+            f.update(map.get(nums1[i]), 1);
         }
         return ans;
+    }
+    private int binarySearch(ArrayList<int[]> compress, int val) {
+        int idx = -1;
+        int low = 0;
+        int high = compress.size()-1;
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            if(compress.get(mid)[0] <= val) {
+                idx=  mid;
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return idx;
     }
     class Fenwick {
         int n;
         int[] bit;
-        public Fenwick(int n) {
+        Fenwick(int n) {
             this.n = n;
             bit = new int[n+1];
         }
-        public void update(int i, int val) {
+        void update(int i, int val) {
             while(i <= n) {
                 bit[i] += val;
                 i += (i & -i);
             }
         }
-        public int query(int i) {
+        int query(int i) {
             int sum = 0;
             while(i > 0) {
                 sum += bit[i];
