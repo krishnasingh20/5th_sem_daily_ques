ed762ba83defa6f701b3975cebb28919bbccbae6
Krishna Singh <krishnasinghks1225@gmail.com>
2026-03-11 02:36:20 +0530

    Time: 204 ms (50.60%) | Memory: 197.4 MB (32.53%) - LeetSync

--- 4145-minimum-inversion-count-in-subarrays-of-fixed-length/minimum-inversion-count-in-subarrays-of-fixed-length.java
+++ 4145-minimum-inversion-count-in-subarrays-of-fixed-length/minimum-inversion-count-in-subarrays-of-fixed-length.java
@@ -2,16 +2,19 @@ class Solution {
     class Fenwick {
         int n;
         int[] bit;
+
         Fenwick(int n) {
             this.n = n;
             this.bit = new int[n+1];
         }
+
         void update(int i, int val) {
             while(i <= n) {
                 bit[i] += val;
                 i += (i & -i);
             }
         }
+
         int query(int i) {
             int sum = 0;
             while(i > 0) {
@@ -21,7 +24,10 @@ class Solution {
             return sum;
         }
     }
+    
     public long minInversionCount(int[] nums, int k) {
+
+        int n = nums.length;
 
         int[] temp = nums.clone();
         HashMap<Integer, Integer> compress = new HashMap<>();
@@ -29,14 +35,14 @@ class Solution {
         Arrays.sort(temp);
 
         int idx = 1;
-        for(int i = 0; i < temp.length; i++) {
+        for(int i = 0; i < n; i++) {
             if(!compress.containsKey(temp[i])) {
                 compress.put(temp[i], idx++);
             }
         }
 
         Fenwick f = new Fenwick(idx);
-        long ans = Long.MAX_VALUE;
+
         long curr = 0;
 
         for(int i = 0; i < k; i++) {
@@ -44,15 +50,19 @@ class Solution {
             curr += (i - f.query(id));
             f.update(id, 1);
         }
-        ans = Math.min(ans, curr);
 
-        for(int i = k; i < nums.length; i++) {
+        long ans = curr;
+
+        for(int i = k; i < n; i++) {
+
             int id = compress.get(nums[i-k]);
             curr -= f.query(id-1);
             f.update(id, -1);
+
             id = compress.get(nums[i]);
             curr += (k - f.query(id) - 1);
             f.update(id, 1);
+
             ans = Math.min(ans, curr);
         }
 
