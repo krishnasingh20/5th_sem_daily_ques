84a30167dea2da66dabd7de8e2104ddf74aade33
Krishna Singh <krishnasinghks1225@gmail.com>
2026-03-12 23:46:47 +0530

    Time: 43 ms (66.80%) | Memory: 65.5 MB (76.61%) - LeetSync

---
+++ 1554-minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
+        vector<vector<int>> tree(n);
+        for(int i = 0; i < edges.size(); i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            tree[u].push_back(v);
+            tree[v].push_back(u);
+        }
+        vector<bool> visited(n, false);
+        int time = dfs(0, tree, visited, hasApple);
+        return max(0, time-2);
+    }
+    int dfs(int src, vector<vector<int>>& tree, vector<bool>& visited, vector<bool>& hasApple) {
+        visited[src] = true;
+        int time = 0;
+        for(int child: tree[src]) {
+            if(!visited[child]) {
+                time += dfs(child, tree, visited, hasApple);
+            }
+        }
+        if(time > 0 || hasApple[src]) {
+            time += 2;
+        }
+        return time;
+    }
+};
