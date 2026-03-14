61b65378251f6eaba45973d965feef7fcb23be98
Krishna Singh <krishnasinghks1225@gmail.com>
2026-03-14 13:40:35 +0530

    Time: 10 ms (95.61%) | Memory: 109.9 MB (92.18%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
@@ -1,34 +1,58 @@
+class DSU {
+public:
+    vector<int> parent, size, minDis;
+    DSU(int n) {
+        parent.resize(n+1);
+        size.resize(n+1);
+        minDis.resize(n+1);
+        
+        for(int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+            minDis[i] = INT_MAX;
+        }
+    }
+
+    int findParent(int node) {
+        if(node == parent[node]) {
+            return node;
+        }
+        return parent[node] = findParent(parent[node]);
+    }
+
+    void unionBySize(int u, int v, int d) {
+        int p1 = findParent(u);
+        int p2 = findParent(v);
+
+        if(p1 == p2) {
+            minDis[p1] = min(minDis[p1], d);
+            return;
+        }
+        if(size[p1] < size[p2]) {
+            parent[p1] = p2;
+            size[p2] += size[p1];
+            minDis[p2] = min(minDis[p1], min(minDis[p2], d));
+        }
+        else {
+            parent[p2] = p1;
+            size[p1] += size[p2];
+            minDis[p1] = min(minDis[p1], min(minDis[p2], d));
+        }
+    }
+};
+
 class Solution {
 public:
     int minScore(int n, vector<vector<int>>& roads) {
+        
+        DSU dsu(n);
 
-        vector<vector<pair<int, int>>> tree(n+1);
-
-        for(int i = 0; i < roads.size(); i++) {
-            tree[roads[i][0]].push_back({roads[i][1], roads[i][2]});
-            tree[roads[i][1]].push_back({roads[i][0], roads[i][2]});
+        for(auto &road: roads) {
+            dsu.unionBySize(road[0], road[1], road[2]);
         }
 
-        vector<int> minDis(n+1, INT_MAX);
-        queue<int> q;
+        int p1 = dsu.findParent(1);
 
-        q.push(1);
-
-        while(!q.empty()) {
-
-            int rv = q.front();
-            q.pop();
-
-            for(pair<int, int>& p: tree[rv]) {
-                int curr = min(minDis[rv], p.second);
-
-                if(curr < minDis[p.first]) {
-                    minDis[p.first] = curr;
-                    q.push(p.first);
-                }
-            }
-        }
-
-        return minDis[n];
+        return dsu.minDis[p1];
     }
 };
