8db276f5c4110ce19e756c45585f3020357086d9
Krishna Singh <krishnasinghks1225@gmail.com>
2026-03-14 23:10:19 +0530

    Time: 138 ms (93.30%) | Memory: 254.6 MB (93.38%) - LeetSync

--- 4253-minimum-cost-to-equalize-arrays-using-swaps/minimum-cost-to-equalize-arrays-using-swaps.cpp
+++ 4253-minimum-cost-to-equalize-arrays-using-swaps/minimum-cost-to-equalize-arrays-using-swaps.cpp
@@ -1,44 +1,22 @@
 class Solution {
 public:
     int minCost(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(), nums1.end());
-        sort(nums2.begin(), nums2.end());
-
-        bool flag = false;
+        unordered_map<int, int> map;
         int n = nums1.size();
 
         for(int i = 0; i < n; i++) {
-            if(nums1[i] != nums2[i]) {
-                flag = true;
-                break;
-            }
+            map[nums1[i]]++;
+            map[nums2[i]]--;
         }
+        int swap = 0;
 
-        if(!flag) {
-            return 0;
-        }
-
-        int mx = max(nums1[n-1], nums2[n-1]);
-        vector<int> freq1(mx+1);
-        vector<int> freq2(mx+1);
-
-        for(int i = 0; i < n; i++) {
-            freq1[nums1[i]]++;
-            freq2[nums2[i]]++;
-        }
-
-        int swap1 = 0;
-        int swap2 = 0;
-
-        for(int i = 1; i <= mx; i++) {
-            if((freq1[i] + freq2[i]) % 2 != 0) {
+        for(auto &p: map) {
+            if(abs(p.second) % 2 != 0) {
                 return -1;
             }
-            int total = (freq1[i]+freq2[i])/2;;
-            swap1 += abs(total - freq1[i]);
-            swap2 += abs(total - freq2[i]);
+            swap += (abs(p.second))/2;
         }
 
-        return swap1/2;
+        return swap/2;
     }
 };
