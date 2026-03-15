46b1bc27da1085fa920779ac0ffb8e0c66f0acc9
Krishna Singh <krishnasinghks1225@gmail.com>
2026-03-15 22:30:41 +0530

    Time: 177 ms (24.05%) | Memory: 287.3 MB (10.00%) - LeetSync

--- 4249-maximum-points-activated-with-one-addition/maximum-points-activated-with-one-addition.java
+++ 4249-maximum-points-activated-with-one-addition/maximum-points-activated-with-one-addition.java
@@ -1,90 +1,80 @@
+import java.util.*;
+
 class Solution {
+
     class DSU {
         int[] parent;
         int[] size;
-        int n;
 
-        public DSU(int n) {
-            this.n = n;
-            parent = new int[2*n+10];
-            size = new int[2*n+10];
-            for(int i = 0; i < 2*n+10; i++) {
+        DSU(int n) {
+            parent = new int[n];
+            size = new int[n];
+            for(int i = 0; i < n; i++) {
                 parent[i] = i;
                 size[i] = 1;
             }
         }
 
-        int find(int node) {
-            if(node == parent[node]) {
-                return node;
+        int find(int x) {
+            if(parent[x] != x) {
+                parent[x] = find(parent[x]);
             }
-            return parent[node] = find(parent[node]);
+            return parent[x];
         }
 
         void union(int a, int b) {
             int p1 = find(a);
             int p2 = find(b);
 
-            if(p1 == p2) {
-                return;
-            }
+            if(p1 == p2) return;
 
-            if(size[p1] > size[p2]) {
+            if(size[p1] < size[p2]) {
+                parent[p1] = p2;
+                size[p2] += size[p1];
+            } else {
                 parent[p2] = p1;
                 size[p1] += size[p2];
             }
-            else {
-                parent[p1] = p2;
-                size[p2] += size[p1];
-            }
         }
     }
-    
+
     public int maxActivated(int[][] points) {
+
         int n = points.length;
-        int[] temp = new int[n];
 
-        // first we will do coordinate compression for x
-        for(int i = 0; i < n; i++) {
-            temp[i] = points[i][0];
+        int[] xs = new int[n];
+        int[] ys = new int[n];
+
+        for(int i = 0; i < n; i++){
+            xs[i] = points[i][0];
+            ys[i] = points[i][1];
         }
 
-        Arrays.sort(temp);
+        Arrays.sort(xs);
+        Arrays.sort(ys);
 
-        int idx1 = 0;
-        HashMap<Integer, Integer> compressX = new HashMap<>();
-        compressX.put(temp[0], idx1);
+        HashMap<Integer,Integer> cx = new HashMap<>();
+        HashMap<Integer,Integer> cy = new HashMap<>();
 
-        for(int i = 1; i < n; i++) {
-            if(temp[i] != temp[i-1]) {
-                compressX.put(temp[i], ++idx1);
+        int id = 0;
+        for(int x : xs){
+            if(!cx.containsKey(x)){
+                cx.put(x, id++);
             }
         }
 
-        //coordinate compression for y
-        for(int i = 0; i < n; i++) {
-            temp[i] = points[i][1];
-        }
-
-        Arrays.sort(temp);
-
-        int idx2 = 0;
-        HashMap<Integer, Integer> compressY = new HashMap<>();
-        compressY.put(temp[0], idx2);
-
-        for(int i = 1; i < n; i++) {
-            if(temp[i] != temp[i-1]) {
-                compressY.put(temp[i], ++idx2);
+        for(int y : ys){
+            if(!cy.containsKey(y)){
+                cy.put(y, id++);
             }
         }
 
-        DSU dsu = new DSU(n);
-        int offSet = n;//to avoid (x, y) overlap
+        DSU dsu = new DSU(id);
         HashSet<Integer> used = new HashSet<>();
 
         for(int[] p: points) {
-            int x = compressX.get(p[0]);
-            int y = compressY.get(p[1]) + offSet;
+            int x = cx.get(p[0]);
+            int y = cy.get(p[1]);
             used.add(x);
             used.add(y);
             dsu.union(x, y);
