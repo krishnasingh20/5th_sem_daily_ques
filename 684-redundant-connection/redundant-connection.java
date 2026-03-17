a3698af23ad89848a081876f200aece8701395c8
Krishna Singh <krishnasinghks1225@gmail.com>
2026-03-17 16:14:02 +0530

    Time: 1 ms (82.36%) | Memory: 45.3 MB (24.35%) - LeetSync

--- 684-redundant-connection/redundant-connection.java
+++ 684-redundant-connection/redundant-connection.java
@@ -1,67 +1,60 @@
 class Solution {
-    public int[] findRedundantConnection(int[][] edges) {
-        int v = edges.length;
-        DSU dsu = new DSU();
-        for(int i = 1; i <= v; i++) {
-            dsu.create(i);
+    
+    class DSU {
+        int[] parent;
+        int[] size;
+        int n;
+        
+        public DSU(int n) {
+            this.n = n;
+            parent = new int[n+1];
+            size = new int[n+1];
+
+            for(int i = 0; i <= n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
         }
+
+        int find(int node) {
+            if(node == parent[node]) {
+                return node;
+            }
+            return parent[node] = find(parent[node]);
+        }
+
+        boolean union(int a, int b) {
+            int p1 = find(a);
+            int p2 = find(b);
+
+            if(p1 == p2) {
+                return true;
+            }
+
+            if(size[p1] > size[p2]) {
+                parent[p2] = p1;
+                size[p1] += size[p2];
+            }
+            else {
+                parent[p1] = p2;
+                size[p2] += size[p1];
+            }
+
+            return false;
+        }
+    }
+    
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+
+        DSU dsu = new DSU(n);
+
         for(int[] edge: edges) {
             if(dsu.union(edge[0], edge[1])) {
                 return edge;
             }
         }
-        return new int[]{};
-    }
-    class DSU{
-        private HashMap<Integer, Node> map = new HashMap<>();
-        class Node {
-            int val;
-            int rank;
-            Node parent;
-            public Node(int val, int rank, Node parent) {
-                this.val = val;
-                this.rank = rank;
-                this.parent = parent;
-            }
-        }
-        public void create(int v) {
-            Node nn = new Node(v, 0, null);
-            nn.parent = nn;
-            map.put(v, nn);
-        }
-        public int find(int v) {
-            Node nn = map.get(v);
-            return find(nn).val;
-        }
-        private Node find(Node node) {
-            if(node.parent == node) {
-                return node;
-            }
-            Node nn = find(node.parent);
-            node.parent = nn;
-            return nn;
-        }
-        public boolean union(int v1, int v2) {
-            Node n1 = map.get(v1);
-            Node n2 = map.get(v2);
-            Node r1 = find(n1);
-            Node r2 = find(n2);
-            if(r1 == r2) {
-                return true;
-            }
-            else {
-                if(r1.rank == r2.rank) {
-                    r1.parent = r2;
-                    r2.rank++;
-                }
-                else if(r1.rank < r2.rank) {
-                    r1.parent = r2;
-                }
-                else {
-                    r2.parent = r1;
-                }
-                return false;
-            }
-        }
+
+        return new int[]{-1, -1};
     }
 }
