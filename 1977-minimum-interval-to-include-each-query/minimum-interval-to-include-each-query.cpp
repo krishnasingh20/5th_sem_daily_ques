d07ce11eba63c189ae12ef1dc46b8b769f66b29d
Krishna Singh <krishnasinghks1225@gmail.com>
2026-03-23 22:43:17 +0530

    Time: 90 ms (55.23%) | Memory: 118.8 MB (49.85%) - LeetSync

--- 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.cpp
+++ 1977-minimum-interval-to-include-each-query/minimum-interval-to-include-each-query.cpp
@@ -12,11 +12,11 @@ struct Compare {
         return a.first < b.first;
     }
     struct cmp {
-        bool operator()(vector<int>& a, vector<int>& b) const {
-            if(a[2] == b[2]) {
-                return a[1] < b[1];
+        bool operator()(pair<int, int>& a, pair<int, int>& b) const {
+            if(a.second == b.second) {
+                return a.first < b.first;
             }
-            return a[2] > b[2];
+            return a.second > b.second;
         }
     };
 };
@@ -26,15 +26,7 @@ public:
     vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
         int n = intervals.size();
 
-        vector<vector<int>> arr(n, vector<int>(3));
-
-        for(int i = 0; i < n; i++) {
-            arr[i][0] = intervals[i][0];
-            arr[i][1] = intervals[i][1];
-            arr[i][2] = arr[i][1] - arr[i][0] + 1;
-        }
-
-        sort(arr.begin(), arr.end(), Compare::sortArr1);
+        sort(intervals.begin(), intervals.end(), Compare::sortArr1);
 
         int q = queries.size();
 
@@ -46,20 +38,20 @@ public:
 
         sort(query.begin(), query.end(), Compare::sortArr2);
 
-        priority_queue<vector<int>, vector<vector<int>>, Compare::cmp> pq;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, Compare::cmp> pq;
         vector<int> ans(q, -1);
         int j = 0;
 
         for(int i = 0; i < q; i++) {
-            while(j < n && arr[j][0] <= query[i].first) {
-                pq.push(arr[j]);
+            while(j < n && intervals[j][0] <= query[i].first) {
+                pq.push({intervals[j][1], intervals[j][1]-intervals[j][0]+1});
                 j++;
             }
-            while(!pq.empty() && pq.top()[1] < query[i].first) {
+            while(!pq.empty() && pq.top().first < query[i].first) {
                 pq.pop();
             }
             if(!pq.empty()) {
-                ans[query[i].second] = pq.top()[2];
+                ans[query[i].second] = pq.top().second;
             }
         }
 
