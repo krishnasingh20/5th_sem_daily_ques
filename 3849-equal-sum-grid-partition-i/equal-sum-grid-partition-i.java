68da11fc9be0a1df7fe11b602b523b5b49aaf8b8
Krishna Singh <krishnasinghks1225@gmail.com>
2026-03-26 00:53:07 +0530

    Time: 5 ms (47.55%) | Memory: 161.6 MB (41.96%) - LeetSync

--- 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.java
+++ 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.java
@@ -1,31 +1,43 @@
 class Solution {
     public boolean canPartitionGrid(int[][] grid) {
+        
         int m = grid.length;
         int n = grid[0].length;
         long totalSum = 0;
+
         for(int i = 0; i < m; i++) {
+
             for(int j = 0; j < n; j++) {
                 totalSum += grid[i][j];
             }
         }
-        long rowSum = 0;
-        for(int i = 0; i < m - 1; i++) {
+
+        //horizontal cut
+        long currSum = 0;
+        for(int i = 0; i < m-1; i++) {
+
             for(int j = 0; j < n; j++) {
-                rowSum += grid[i][j];
+                currSum += grid[i][j];
             }
-            if(totalSum - rowSum == rowSum) {
+
+            if(2*currSum == totalSum) {
                 return true;
             }
         }
-        long colSum = 0;
-        for(int j = 0; j < n - 1; j++) {
+
+        //vertical cur
+        currSum = 0;
+        for(int j = 0; j < n-1; j++) {
+
             for(int i = 0; i < m; i++) {
-                colSum += grid[i][j];
+                currSum += grid[i][j];
             }
-            if(totalSum - colSum == colSum) {
+            
+            if(2*currSum == totalSum) {
                 return true;
             }
         }
+
         return false;
     }
 }
