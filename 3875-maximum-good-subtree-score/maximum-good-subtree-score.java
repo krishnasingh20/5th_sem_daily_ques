c2f6b5735901cc9b2eff2f18ff65b4b8cc379b71
Krishna Singh <krishnasinghks1225@gmail.com>
2026-03-27 12:11:46 +0530

    Time: 353 ms (55.17%) | Memory: 274.8 MB (6.90%) - LeetSync

--- 3875-maximum-good-subtree-score/maximum-good-subtree-score.java
+++ 3875-maximum-good-subtree-score/maximum-good-subtree-score.java
@@ -1,32 +1,13 @@
 class Solution {
 
+    static final int mod = 1000000007;
+
     List<List<Integer>> adj = new ArrayList<>();
-    int[] masks;
-    long score = 0;
-    List<Integer> arr = new ArrayList<>();
+    int score = 0;
 
     public int goodSubtreeSum(int[] vals, int[] par) {
 
         int n = vals.length;
-        masks = new int[n];
-
-        for(int i = 0; i < n; i++) {
-
-            int mask = 0;
-            int num = vals[i];
-
-            while(num > 0) {
-                if((mask & (1 << (num % 10))) != 0) {
-                    vals[i] = 0;
-                    mask = 0;
-                    break;
-                }
-                mask |= (1 << (num % 10));
-                num /= 10;
-            }
-
-            masks[i] = mask;
-        }
 
         for(int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
@@ -36,30 +17,29 @@ class Solution {
             adj.get(par[i]).add(i);
         }
 
-        for(int i = 0; i < n; i++) {
-            arr.clear();
+        dfs(0, vals);
 
-            dfs(i, vals);
-
-            dp = new Long[arr.size()][1024];
-
-            score += maxScore(0, 0, vals);
-
-        }
-
-        return (int)(score % 1000000007);
+        return score;
     }
 
-    Long[][] dp;
-
-    private void dfs(int src, int[] vals) {
-        for(int nbrs: adj.get(src)) {
-            dfs(nbrs, vals);
-        }
+    private List<Integer> dfs(int src, int[] vals) {
+        List<Integer> arr = new ArrayList<>();
         arr.add(src);
+
+        for(int nbrs: adj.get(src)) {
+            arr.addAll(dfs(nbrs, vals));
+        }
+
+        dp = new Integer[arr.size()][1024];
+
+        score = (score + maxScore(arr, 0, 0, vals)) % mod;
+
+        return arr;
     }
 
-    private long maxScore(int i, int mask, int[] vals) {
+    Integer[][] dp;
+
+    private int maxScore(List<Integer> arr, int i, int mask, int[] vals) {
         if(i == arr.size()) {
             return 0;
         }
@@ -68,12 +48,37 @@ class Solution {
             return dp[i][mask];
         }
 
-        long pick = 0;
-        if((mask & masks[arr.get(i)]) == 0) {
-            pick = vals[arr.get(i)] + maxScore(i+1, (mask | masks[arr.get(i)]), vals);
+        int currMask = getMask(vals[arr.get(i)]);
+        int val = vals[arr.get(i)];
+
+        
+        if(currMask == -1) {
+           currMask = 0;
+           val = 0;
         }
-        long notPick = maxScore(i+1, mask, vals);
+
+        int pick = 0;
+        if((mask & currMask) == 0) {
+            pick = (val + maxScore(arr, i+1, (mask | currMask), vals)) % mod;
+        }
+
+        int notPick = maxScore(arr, i+1, mask, vals) % mod;
 
         return dp[i][mask] = Math.max(pick, notPick);
+    }
+
+    private int getMask(int num) {
+        int mask = 0;
+        while(num > 0) {
+            int rem = num % 10;
+            if((mask & (1 << rem)) != 0) {
+                return -1;
+            }
+
+            mask |= (1 << rem);
+            num /= 10;
+        }
+
+        return mask;
     }
 }
