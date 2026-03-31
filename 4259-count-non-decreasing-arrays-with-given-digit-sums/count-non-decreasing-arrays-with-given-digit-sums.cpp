83edc491f6a1226f59da539dbaadad376257c185
Krishna Singh <krishnasinghks1225@gmail.com>
2026-03-31 20:51:22 +0530

    Time: 994 ms (63.49%) | Memory: 418.3 MB (17.77%) - LeetSync

---
+++ 4259-count-non-decreasing-arrays-with-given-digit-sums/count-non-decreasing-arrays-with-given-digit-sums.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int countArrays(vector<int>& digitSum) {
+        int n = digitSum.size();
+        int mod = 1000000007;
+        if(digitSum[0] > 31) {
+            return 0;
+        }
+        vector<vector<int>> arr(51);
+
+        for(int i = 0; i <= 5000; i++) {
+            int num = i;
+            int sum = 0;
+
+            while(num > 0) {
+                sum +=(num % 10);
+                num /= 10;
+            }
+
+            arr[sum].push_back(i);
+        }
+
+        vector<int> dp(5001);
+        for(int a: arr[digitSum[0]]) {
+            dp[a] = 1;
+        }
+
+        for(int i = 1; i < n; i++) {
+            vector<int> dp1(5001);
+            vector<int> &a = arr[digitSum[i]];
+
+            int j = 0;
+            int k = 0;
+            long long sum = 0;
+
+            while(k < 5001 && j < a.size()) {
+                if(a[j] < k) {
+                    dp1[a[j]] = sum % mod;
+                    j++;
+                }
+                sum = (sum + dp[k]) % mod;
+                k++;
+            }
+
+            while(j < a.size()) {
+                dp1[a[j]] = sum;
+                j++;
+            }
+
+            dp = dp1;
+        }
+
+        int ans = 0;
+        
+        for(int i = 0; i < 5001; i++) {
+            ans = (ans + dp[i]) % mod;
+        }
+
+        return (int)ans;
+    }
+};
