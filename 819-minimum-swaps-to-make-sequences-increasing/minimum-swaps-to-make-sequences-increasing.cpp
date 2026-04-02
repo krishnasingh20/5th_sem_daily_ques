336633d3ad2fc56d4cc4d24569092fb4eca9d4ad
Krishna Singh <krishnasinghks1225@gmail.com>
2026-04-02 22:41:48 +0530

    Time: 73 ms (51.76%) | Memory: 124.3 MB (49.60%) - LeetSync

--- 819-minimum-swaps-to-make-sequences-increasing/minimum-swaps-to-make-sequences-increasing.cpp
+++ 819-minimum-swaps-to-make-sequences-increasing/minimum-swaps-to-make-sequences-increasing.cpp
@@ -1,55 +1,47 @@
 class Solution {
 public:
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
-
-        vector<vector<int>> dp(nums1.size(), vector<int>(2, -1));
-
-        return minOpr(nums1, nums2, 0, 0, dp);
+        return bottomUp(nums1, nums2);
     }
 
-    int minOpr(vector<int>& nums1, vector<int>& nums2, int i, int state, vector<vector<int>>& dp) {
-        if(i == nums1.size()) {
-            return 0;
-        }
+    int bottomUp(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
 
-        if(dp[i][state] != -1) {
-            return dp[i][state];
-        }
+        vector<vector<int>> dp(n+1, vector<int>(2));
 
-        int ans = INT_MAX / 2;
+        for(int i = n-1; i >= 0; i--) {
+            if(i == 0) {
+                dp[0][0] = min(dp[i+1][0], dp[i+1][1]+1);
+                continue;
+            }
+            for(int state = 0; state <= 1; state++) {
+                int ans = INT_MAX/2;
 
-        if(i == 0) {
-            //check without swap possible to make strictly increasing
-            int opr1 = minOpr(nums1, nums2, i+1, 0, dp);
-            //check after swap possible to make strictly increasing
-            int opr2 = 1 + minOpr(nums1, nums2, i+1, 1, dp);
-            ans = min(opr1, opr2);
-        }
-        else if(state == 0) {
-            //check without swap possible to make strictly increasing
-            if(nums1[i] > nums1[i-1] && nums2[i] > nums2[i-1]) {
-                int opr = minOpr(nums1, nums2, i+1, 0, dp);
-                ans = opr;
-            }
-            //check after swap possible to make strictly increasing
-            if(nums2[i] > nums1[i-1] && nums1[i] > nums2[i-1]) {
-                int opr = 1 + minOpr(nums1, nums2, i+1, 1, dp);
-                ans = min(ans, opr);
-            }
-        }
-        else {
-            //check without swap possible to make strictly increasing
-            if(nums1[i] > nums2[i-1] && nums2[i] > nums1[i-1]) {
-                int opr = minOpr(nums1, nums2, i+1, 0, dp);
-                ans = opr;
-            }
-            //check after swap possible to make strictly increasing
-            if(nums2[i] >  nums2[i-1] && nums1[i] > nums1[i-1]) {
-                int opr = 1 + minOpr(nums1, nums2, i+1, 1, dp);
-                ans = min(ans, opr);
+                if(state == 0) {
+                    if(nums1[i] > nums1[i-1] && nums2[i] > nums2[i-1]) {
+                        int opr = dp[i+1][0];
+                        ans = opr;
+                    }
+                    if(nums2[i] > nums1[i-1] && nums1[i] > nums2[i-1]) {
+                        int opr = 1 + dp[i+1][1];
+                        ans = min(ans, opr);
+                    }
+                }
+                else {
+                    if(nums1[i] > nums2[i-1] && nums2[i] > nums1[i-1]) {
+                        int opr = dp[i+1][0];
+                        ans = opr;
+                    }
+                    if(nums2[i] >  nums2[i-1] && nums1[i] > nums1[i-1]) {
+                        int opr = 1 + dp[i+1][1];
+                        ans = min(ans, opr);
+                    }
+                }
+
+                dp[i][state] = ans;
             }
         }
 
-        return dp[i][state] = ans;
+        return dp[0][0];
     }
 };
