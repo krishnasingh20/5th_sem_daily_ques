196516a783ee4de3ba0526b99ea5f021044f2b59
Krishna Singh <krishnasinghks1225@gmail.com>
2026-04-06 22:00:41 +0530

    Time: 2 ms (100.00%) | Memory: 44.2 MB (43.76%) - LeetSync

--- 576-out-of-boundary-paths/out-of-boundary-paths.java
+++ 576-out-of-boundary-paths/out-of-boundary-paths.java
@@ -1,22 +1,34 @@
 class Solution {
+
+    static final int MOD = 1000000007;
+
     public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        Long[][][] dp = new Long[m][n][maxMove+1];
-        return (int)(path(m, n, maxMove, startRow, startColumn, dp) % 1000000007);
+
+        dp = new Integer[m][n][maxMove+1];
+
+        return path(startRow, startColumn, m, n, maxMove);
     }
-    public long path(int m, int n, int maxMove, int r, int c, Long[][][] dp) {
+
+    Integer[][][] dp;
+
+    private int path(int r, int c, int m, int n, int maxMove) {
         if(r < 0 || c < 0 || r >= m || c >= n) {
             return 1;
         }
-        if(maxMove == 0) {
-            return 0;
-        }
+
         if(dp[r][c][maxMove] != null) {
             return dp[r][c][maxMove];
         }
-        long down = path(m, n,  maxMove - 1, r+1, c, dp) % 1000000007;
-        long up = path(m, n, maxMove - 1, r-1, c, dp) % 1000000007;
-        long left = path(m, n, maxMove - 1, r, c-1, dp) % 1000000007;
-        long right = path(m, n, maxMove - 1, r, c+1, dp) % 1000000007;
-        return dp[r][c][maxMove] = (down + up + left + right) % 1000000007;
+
+        int ans = 0;
+
+        if(maxMove > 0) {
+            ans = (ans + path(r+1, c, m, n, maxMove-1)) % MOD;//down direction
+            ans = (ans + path(r-1, c, m, n, maxMove-1)) % MOD;//up direction
+            ans = (ans + path(r, c-1, m, n, maxMove-1)) % MOD;//left direction
+            ans = (ans + path(r, c+1, m, n, maxMove-1)) % MOD;//right direction
+        }
+
+        return dp[r][c][maxMove] = ans;
     }
 }
