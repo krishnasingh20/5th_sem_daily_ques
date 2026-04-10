cac325619cbe0d9a76df61a402ca02190b8bbb5a
Krishna Singh <krishnasinghks1225@gmail.com>
2026-04-10 12:28:06 +0530

    Time: 12 ms (43.35%) | Memory: 18.3 MB (44.51%) - LeetSync

--- 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.cpp
+++ 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.cpp
@@ -1,86 +1,31 @@
 class Solution {
 public:
-    int n;
-    int minFlipsMonoIncr(string &s) {
-        n = s.length();
+    int minFlipsMonoIncr(string s) {
+        int n = s.length();
 
-        // vector<vector<int>> dp(n, vector<int>(2, -1));
+        vector<int> suffix(n+1);
 
-        // return minFlip(s, 0, 0, dp);
+        for(int i = n-1; i >= 0; i--) {
+            if(s[i] == '0') {
+                suffix[i] = 1;
+            }
 
-        return bottomUp(s);
-    }
-
-    int minFlip(string &s, int i, int state, vector<vector<int>>& dp) {
-        if(i == n) {
-            return 0;
-        }
-
-        if(dp[i][state] != -1) {
-            return dp[i][state];
+            suffix[i] = suffix[i] + suffix[i+1];
         }
 
         int ans = INT_MAX;
 
-        if(state) {
-            int curr = minFlip(s, i+1, 1, dp);
-            if(s[i] == '0') {
-                curr++;
-            }
-            ans = min(ans, curr);
-        }
-        else {
-            int a = minFlip(s, i+1, 0, dp);
+        int prefix_one = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            ans = min(ans, prefix_one+suffix[i+1]);
+            
             if(s[i] == '1') {
-                a++;
-            }
-
-            int b = minFlip(s, i+1, 1, dp);
-            if(s[i] == '0') {
-                b++;
-            }
-
-            ans = min(ans, min(a, b));
-        }
-
-        return dp[i][state] = ans;
-    }
-
-    int bottomUp(string &s) {
-
-        vector<vector<int>> dp(n+1, vector<int>(2));
-
-        for(int i = n-1; i >= 0; i--) {
-            for(int state = 0; state <= 1; state++) {
-
-                int ans = INT_MAX;
-
-                if(state) {
-                    int curr = dp[i+1][1];
-                    if(s[i] == '0') {
-                        curr++;
-                    }
-
-                    ans = min(ans, curr);
-                }
-                else {
-                    int a = dp[i+1][0];
-                    if(s[i] == '1') {
-                        a++;
-                    }
-
-                    int b = dp[i+1][1];
-                    if(s[i] == '0') {
-                        b++;
-                    }
-
-                    ans = min(ans, min(a, b));
-                }
-
-                dp[i][state] = ans;
+                prefix_one++;
             }
         }
 
-        return dp[0][0];
+        return ans;
     }
 };
