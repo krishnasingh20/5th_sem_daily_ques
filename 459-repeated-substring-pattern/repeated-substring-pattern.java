20fbe791a7f73202f2ab00ed2ff9172743b6628b
Krishna Singh <krishnasinghks1225@gmail.com>
2026-04-13 22:38:39 +0530

    Time: 15 ms (82.30%) | Memory: 47 MB (22.45%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.java
+++ 459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -1,23 +1,84 @@
 class Solution {
+
     public boolean repeatedSubstringPattern(String s) {
-        int len = s.length()/2;
-        for(int i = 1; i <= len; i++) {
-            if(s.length() % i != 0) {
-                continue;
-            }
-            boolean flag = false;
-            String s1 = s.substring(0, i);
-            // System.out.println(s1);
-            for(int j = i; j <= s.length()-i; j += i) {
-                if(s.substring(j, j+i).equals(s1) == false) {
-                    flag = true;
+        return isRepeatedPattern(s);
+    }
+
+    public static boolean isRepeatedPattern(String s) {
+        int n = s.length();
+
+        List<Integer> factor = generateFactor(s, n);
+
+        int MOD = 1000000007;
+
+        long[] arr = new long[n];
+        long[] pow = new long[n + 2];
+
+        pow[0] = 1;
+
+        for (int i = 1; i <= n + 1; i++) {
+            pow[i] = (pow[i - 1] * 31) % MOD;
+        }
+
+        long p = 1;
+
+        arr[0] = (p * (s.charAt(0) - 'a')) % MOD;
+
+        for (int i = 1; i < n; i++) {
+            p = (p * 31) % MOD;
+            arr[i] = ((s.charAt(i) - 'a') * p) % MOD;
+        }
+
+        // prefix sum hash
+        for (int i = 1; i < n; i++) {
+            arr[i] = (arr[i] + arr[i - 1]) % MOD;
+        }
+
+        for (int k : factor) {
+
+            long pat = arr[k - 1];
+
+            int i = 2 * k - 1;
+
+            boolean flag = true;
+
+            for (; i < n; i += k) {
+                long curr = arr[i] - arr[i - k];
+
+                // handle negative
+                if (curr < 0) curr += MOD;
+
+                if (curr != (pat * pow[i - k + 1]) % MOD) {
+                    flag = false;
                     break;
                 }
             }
-            if(!flag) {
-                return true;
+
+            if (flag) return true;
+        }
+
+        return false;
+    }
+
+    public static List<Integer> generateFactor(String s, int n) {
+        List<Integer> factor = new ArrayList<>();
+
+        int limit = (int) Math.sqrt(n);
+
+        for (int i = 1; i <= limit; i++) {
+            if (n % i == 0) {
+                factor.add(i);
+                if (i != n / i) {
+                    factor.add(n / i);
+                }
             }
         }
-        return false;
+
+        Collections.sort(factor);
+
+        // remove n itself
+        factor.remove(factor.size() - 1);
+
+        return factor;
     }
 }
