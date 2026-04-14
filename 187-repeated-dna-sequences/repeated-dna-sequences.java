788730d125663071534f2a4665fe877f49698c4a
Krishna Singh <krishnasinghks1225@gmail.com>
2026-04-14 16:43:00 +0530

    Time: 26 ms (32.92%) | Memory: 55.5 MB (75.00%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.java
+++ 187-repeated-dna-sequences/repeated-dna-sequences.java
@@ -1,6 +1,9 @@
 class Solution {
+
     static final long MOD = 1000000007;
+
     public List<String> findRepeatedDnaSequences(String s) {
+        
         if(s.length() <= 10) {
             return new ArrayList<>();
         }
@@ -10,8 +13,11 @@ class Solution {
         int n = s.length();
 
         for(int i = 0; i <= n-10; i++) {
+
             String s1 = s.substring(i, i+10);
+
             map.put(s1, map.getOrDefault(s1, 0)+1);
+
             if(map.get(s1) == 2) {
                 ans.add(s1);
             }
