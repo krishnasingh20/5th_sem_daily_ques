6a30ed833c0ec5ec5bd6ac226ef57d8294f1de98
Krishna Singh <krishnasinghks1225@gmail.com>
2026-04-14 22:56:47 +0530

    Time: 13 ms (94.85%) | Memory: 47.9 MB (44.12%) - LeetSync

--- 839-short-encoding-of-words/short-encoding-of-words.java
+++ 839-short-encoding-of-words/short-encoding-of-words.java
@@ -1,9 +1,12 @@
 class Solution {
+
     class Trie {
+
         class Node {
             boolean isTerminal;
             Node[] child = new Node[26];
         }
+
         private Node root;
 
         public Trie() {
@@ -11,12 +14,16 @@ class Solution {
         }
 
         public void insert(String s) {
+
             Node curr = root;
             boolean change = false;
+
             for(int i = s.length()-1; i >= 0; i--) {
                 int x = s.charAt(i)-'a';
+
                 if(curr.child[x] != null) {
                     curr = curr.child[x];
+
                     if(curr.isTerminal) {
                         curr.isTerminal = false;
                         change = true;
@@ -28,30 +35,33 @@ class Solution {
                     change = true;
                 }
             }
+
             if(change) {
                 curr.isTerminal = true;
             }
         }
 
-        int ans;
         public int find() {
-            ans = 0;
-            dfs(root, 0);
-            return ans;
+            return dfs(root, 0);
         }
 
-        private void dfs(Node curr, int c) {
+        private int dfs(Node curr, int c) {
             if(curr.isTerminal) {
-                ans += c+1;
-                return;
+                return c+1;
             }
+
+            int res = 0;
+
             for(int i = 0; i < 26; i++) {
                 if(curr.child[i] != null) {
-                    dfs(curr.child[i], c+1);
+                    res += dfs(curr.child[i], c+1);
                 }
             }
+
+            return res;
         }
     }
+
     public int minimumLengthEncoding(String[] words) {
         Trie trie = new Trie();
         int n = words.length;
