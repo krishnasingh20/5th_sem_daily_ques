21b5e172eab6daa6e333ea431c39a7c4af57cb89
Krishna Singh <krishnasinghks1225@gmail.com>
2026-04-14 16:34:28 +0530

    Time: 26 ms (32.92%) | Memory: 55.6 MB (75.00%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.java
+++ 187-repeated-dna-sequences/repeated-dna-sequences.java
@@ -6,26 +6,15 @@ class Solution {
         }
 
         List<String> ans = new ArrayList<>();
-        HashMap<Long, HashMap<String, Integer>> map = new HashMap<>();
-        long base = 31;
+        HashMap<String, Integer> map = new HashMap<>();
         int n = s.length();
 
         for(int i = 0; i <= n-10; i++) {
-            long p = 1;
-            long hash = (s.charAt(i)-'A')+1;
-
-            for(int j = i+1; j < i+10; j++) {
-                p = (p * base) % MOD;
-                hash = (hash + ((p * (s.charAt(j)-'A')+1) % MOD)) % MOD;
-            }
-
-            map.putIfAbsent(hash, new HashMap<>());
             String s1 = s.substring(i, i+10);
-
-            if(map.get(hash).getOrDefault(s1, -1) == 1) {
+            map.put(s1, map.getOrDefault(s1, 0)+1);
+            if(map.get(s1) == 2) {
                 ans.add(s1);
             }
-            map.get(hash).put(s1, map.get(hash).getOrDefault(s1, 0)+1);
         }
 
         return ans;
