fdcbf612e7c565461f542beb812cc960604d02a4
Krishna Singh <krishnasinghks1225@gmail.com>
2026-04-15 00:58:47 +0530

    Time: 12 ms (36.44%) | Memory: 11.9 MB (62.38%) - LeetSync

--- 1989-minimum-xor-sum-of-two-arrays/minimum-xor-sum-of-two-arrays.cpp
+++ 1989-minimum-xor-sum-of-two-arrays/minimum-xor-sum-of-two-arrays.cpp
@@ -3,18 +3,20 @@ public:
     int minimumXORSum(vector<int>& nums1, vector<int>& nums2) {
 
         int n = nums1.size();
-        vector<vector<int>> dp(n, vector<int>((1 << n), -1));
+        vector<int> dp((1 << n), -1);
 
-        return minXor(0, 0, n, nums1, nums2, dp);
+        return minXor(0, n, nums1, nums2, dp);
     }
 
-    int minXor(int i, int mask, int n, vector<int>& nums1, vector<int>& nums2, vector<vector<int>>& dp){
-        if (i == n) {
+    int minXor(int mask, int n, vector<int>& nums1, vector<int>& nums2, vector<int>& dp){
+        int i = setBitCount(mask);
+
+        if(i == n) {
             return 0;
         }
 
-        if(dp[i][mask] != -1) {
-            return dp[i][mask];
+        if(dp[mask] != -1) {
+            return dp[mask];
         }
 
         int ans = INT_MAX;;
@@ -24,11 +26,22 @@ public:
                 continue;
             }
 
-            int curr = (nums1[i] ^ nums2[j]) + minXor(i+1, (mask | (1 << j)), n, nums1, nums2, dp);
+            int curr = (nums1[i] ^ nums2[j]) + minXor((mask | (1 << j)), n, nums1, nums2, dp);
 
             ans = min(ans, curr);
         }
 
-        return dp[i][mask] = ans;
+        return dp[mask] = ans;
+    }
+
+    int setBitCount(int n) {
+        int c = 0;
+        
+        while(n > 0) {
+            c++;
+            n = (n & (n-1));
+        }
+
+        return c;
     }
 };
