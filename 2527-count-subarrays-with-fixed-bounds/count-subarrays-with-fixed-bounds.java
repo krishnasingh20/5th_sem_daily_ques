4196e2fd43ae0c5ecb10c2c4ccdc920540b832f0
Krishna Singh <krishnasinghks1225@gmail.com>
2026-04-23 21:16:29 +0530

    Time: 7 ms (97.32%) | Memory: 94.7 MB (62.95%) - LeetSync

--- 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.java
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.java
@@ -1,56 +1,35 @@
 class Solution {
     public long countSubarrays(int[] nums, int minK, int maxK) {
+
+        int min = -1;//it will hold nearest minK value index in nums
+        int max = -1;//it will hold nearest maxK value index in nums
+        int start = -1;//it will represent the boundary after which every element are greater then equal to minK and less then equal to maxK for calculating numder of subarrays
+
         int n = nums.length;
-        int valid = -1;
-        int si = 0;
-        int ei = 0;
-        int min = 0;
-        int max = 0;
         long ans = 0;
-        while(ei < n) {
-            if(nums[ei] < minK || nums[ei] > maxK) {
-                valid = ei;
-                min = 0;
-                max = 0;
-                ei++;
-                si = ei;
+
+        for (int i = 0; i < n; i++) {
+            
+            if (nums[i] < minK || nums[i] > maxK) {
+                start = i;
+                min = max = -1;
                 continue;
             }
-            if(nums[ei] == minK) {
-                min++;
+
+            if (nums[i] == minK) {
+                min = i;
             }
-            if(nums[ei] == maxK) {
-                max++;
+
+            if (nums[i] == maxK) {
+                max = i;
             }
-            while(min > 1 && max > 1) {
-                if(nums[si] == minK) {
-                    min--;
-                }
-                if(nums[si] == maxK) {
-                    max--;
-                }
-                si++;
+
+            if (min != -1 && max != -1) {
+                int end = Math.min(min, max);
+                ans += (end - start);
             }
-            while(si <= ei && (min == 1 && max >= 2 && nums[si] != minK)) {
-                if(nums[si] == maxK) {
-                    max--;
-                }
-                si++;
-            }
-            while(si <= ei && (min >= 2 && max == 1 && nums[si] != maxK)) {
-                if(nums[si] == minK) {
-                    min--;
-                }
-                si++;
-            }
-            while(min > 0 && max > 0 && nums[si] != minK && nums[si] != maxK) {
-                si++;
-            }
-            if(min > 0 && max > 0) {
-                ans += (si - valid);
-            }
-            ei++;
         }
+
         return ans;
     }
 }
