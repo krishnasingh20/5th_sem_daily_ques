862b8982bf8db2ff0e4521d5aa638e11a1b10bb2
Krishna Singh <krishnasinghks1225@gmail.com>
2026-04-26 04:52:21 +0530

    Time: 449 ms (0.00%) | Memory: 250.6 MB (0.00%) - LeetSync

--- 4287-k-th-smallest-remaining-even-integer-in-subarray-queries/k-th-smallest-remaining-even-integer-in-subarray-queries.java
+++ 4287-k-th-smallest-remaining-even-integer-in-subarray-queries/k-th-smallest-remaining-even-integer-in-subarray-queries.java
@@ -1,66 +1,80 @@
 class Solution {
+    
     public int[] kthRemainingInteger(int[] nums, int[][] queries) {
+
         int n = nums.length;
-        int[] prefix = new int[n+1];
+        int[] prefix = new int[n + 1];
         TreeSet<Long> set = new TreeSet<>();
         HashMap<Long, Integer> map = new HashMap<>();
-        
-        for(int i = 0; i < n; i++) {
-            if((nums[i] & 1) == 0) {
-                prefix[i+1] = 1;
-                set.add((long)nums[i]);
-                map.put((long)nums[i], i);
+
+        for (int i = 0; i < n; i++) {
+
+            if ((nums[i] & 1) == 0) {
+                prefix[i + 1] = 1;
+                set.add((long) nums[i]);
+                map.put((long) nums[i], i);
             }
-            prefix[i+1] += prefix[i];
+
+            prefix[i + 1] += prefix[i];
         }
 
         int m = queries.length;
         int[] ans = new int[m];
 
-        for(int i = 0; i < m; i++) {
+        for (int i = 0; i < m; i++) {
+
             int l = queries[i][0];
             int r = queries[i][1];
             int k = queries[i][2];
 
             long low = 0;
-            long high = 2L*(k+(prefix[r+1]-prefix[l]));
+            long high = 2L * (k + (prefix[r + 1] - prefix[l]));
             long temp = 0;
 
-            while(low <= high) {
-                long mid = low + (high - low)/2;
+            while (low <= high) {
 
-                Long val = mid > nums[r]?set.floor((long)nums[r]):set.floor(mid);
-                long curr = 0;
+                long mid = low + (high - low) / 2;
 
-                if(val != null) {
+                int idx = upperBound(nums, mid, l, r);
+                long curr = prefix[idx] - prefix[l];
 
-                    int idx = map.get(val);
-
-                    if(idx >= l && idx <= r) {
-                        curr = prefix[idx+1] - prefix[l];
-                    }
-
-                }
-
-                long total = mid/2;
+                long total = mid / 2;
                 long count = total - curr;
 
-                if(count >= k) {
+                if (count >= k) {
                     temp = mid;
                     high = mid - 1;
-                }
-                else {
+                } else {
                     low = mid + 1;
                 }
             }
 
-            if((temp & 1) == 1) {
+            if ((temp & 1) == 1) {
                 temp--;
             }
 
-            ans[i] = (int)temp;
+            ans[i] = (int) temp;
         }
 
         return ans;
+    }
+
+    public int upperBound(int[] nums, long target, int l, int r) {
+
+        int low = l;
+        int high = r;
+        int idx = l;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] <= target) {
+                idx = mid + 1;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return idx;
     }
 }
