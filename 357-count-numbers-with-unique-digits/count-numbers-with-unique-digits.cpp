16b51a1ec4290975a70c6026c9dd0965c7326e00
Krishna Singh <krishnasinghks1225@gmail.com>
2026-04-25 08:43:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (5.10%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
@@ -4,13 +4,18 @@ public:
 
         int x = pow(10, n) - 1;
         string s = to_string(x);
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(1024, -1)));
 
-        return count(s, 0, 1, 0);
+        return count(s, 0, 1, 0, dp);
     }
-    int count(string& s, int idx, int t, int mask) {
+    int count(string& s, int idx, int t, int mask, vector<vector<vector<int>>>& dp) {
 
         if (idx == s.length()) {
             return 1;
+        }
+
+        if(t == 0 && dp[idx][t][mask] != -1) {
+            return dp[idx][t][mask];
         }
 
         int lb = 0;
@@ -30,7 +35,11 @@ public:
                 newMask = (mask | (1 << d));
             }
 
-            res += count(s, idx + 1, newT, newMask);
+            res += count(s, idx + 1, newT, newMask, dp);
+        }
+
+        if(t == 0) {
+            dp[idx][t][mask] = res;
         }
 
         return res;
