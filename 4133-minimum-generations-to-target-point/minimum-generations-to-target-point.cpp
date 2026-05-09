dd75b1bb81904b9f8ef5f67fa5af653efd7b18f7
Krishna Singh <krishnasinghks1225@gmail.com>
2026-05-10 00:49:52 +0530

    Time: 129 ms (78.88%) | Memory: 65.5 MB (66.62%) - LeetSync

--- 4133-minimum-generations-to-target-point/minimum-generations-to-target-point.cpp
+++ 4133-minimum-generations-to-target-point/minimum-generations-to-target-point.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    int minGenerations(vector<vector<int>>& points, vector<int>& t) {
+    int minGenerations(vector<vector<int>>& p, vector<int>& t) {
         vector<vector<vector<bool>>> visited(7, vector<vector<bool>>(7, vector<bool>(7, false)));
-        for(vector<int>& p: points) {
-            if(p[0] == t[0] && p[1] == t[1] && p[2] == t[2]) {
+        for(vector<int>& p1: p) {
+            if(p1[0] == t[0] && p1[1] == t[1] && p1[2] == t[2]) {
                 return 0;
             }
-            visited[p[0]][p[1]][p[2]] = true;
+            visited[p1[0]][p1[1]][p1[2]] = true;
         }
         for(int k = 1; k <= 15; k++) {
-            int size = points.size();
+            int size = p.size();
             for(int j = 0; j < size; j++) {
-                vector<int> p = points[j];
-                if(p[0] == t[0] && p[1] == t[1] && p[2] == t[2]) {
+                if(p[j][0] == t[0] && p[j][1] == t[1] && p[j][2] == t[2]) {
                     return k-1;
                 }
                 for(int i = j+1; i < size; i++) {
-                    vector<int> p1 = points[i];
-                    int a = (p[0] + p1[0])/2;
-                    int b = (p[1] + p1[1])/2;
-                    int c = (p[2] + p1[2])/2;
+                    int a = (p[j][0] + p[i][0])/2;
+                    int b = (p[j][1] + p[i][1])/2;
+                    int c = (p[j][2] + p[i][2])/2;
                     if(!visited[a][b][c]) {
                         vector<int> arr;
                         arr.push_back(a);
                         arr.push_back(b);
                         arr.push_back(c);
-                        points.push_back(arr);
+                        p.push_back(arr);
                         visited[a][b][c] = true;
                     }
                 }
