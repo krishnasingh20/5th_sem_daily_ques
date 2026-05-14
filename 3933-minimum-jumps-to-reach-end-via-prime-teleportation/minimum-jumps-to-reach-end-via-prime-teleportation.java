baee8b72c03edf29f6119b303d91b5c1753a5a10
Krishna Singh <krishnasinghks1225@gmail.com>
2026-05-14 18:58:18 +0530

    Time: 185 ms (68.79%) | Memory: 187.5 MB (59.88%) - LeetSync

--- 3933-minimum-jumps-to-reach-end-via-prime-teleportation/minimum-jumps-to-reach-end-via-prime-teleportation.java
+++ 3933-minimum-jumps-to-reach-end-via-prime-teleportation/minimum-jumps-to-reach-end-via-prime-teleportation.java
@@ -1,13 +1,17 @@
 class Solution {
-    static boolean[] isPrime = new boolean[1000001];
+    static int MAX = 1000000;
+    static int[] spf = new int[1000001];
     static {
-        Arrays.fill(isPrime, true);
-        isPrime[0] = isPrime[1] = false;
-
-        for(int i = 2; (long)i * i <= 1000000; i++) {
-            if(isPrime[i]) {
-                for(int j = i * i; j <= 1000000; j += i) {
-                    isPrime[j] = false;
+        for(int i = 0; i <= MAX; i++) {
+            spf[i] = i;
+        }
+        spf[0] = spf[1] = -1;
+        for(int i = 2; i * i <= MAX; i++) {
+            if(spf[i] == i) {
+                for(int j = i * i; j <= MAX; j += i) {
+                    if(spf[j] == j) {
+                        spf[j] = i;
+                    }
                 }
             }
         }
@@ -16,16 +20,18 @@ class Solution {
         int n = nums.length;
         HashMap<Integer, List<Integer>> map = new HashMap<>();
         for(int i = 0; i < n; i++) {
-            int limit = (int)Math.sqrt(nums[i]);
-            for(int j = 1; j <= limit; j++) {
-                if(nums[i] % j == 0) {
-                    if(isPrime[j]) {
-                        map.putIfAbsent(j, new ArrayList<>());
-                        map.get(j).add(i);
-                    }
-                    if(nums[i]/j != j && isPrime[nums[i]/j]) {
-                        map.putIfAbsent(nums[i]/j, new ArrayList<>());
-                        map.get(nums[i]/j).add(i);
+            if(spf[nums[i]] == nums[i]) {
+                map.putIfAbsent(nums[i], new ArrayList<>());
+                map.get(nums[i]).add(i);
+            }
+            else {
+                int num = nums[i];
+                while(num > 1) {
+                    int x = spf[num];
+                    map.putIfAbsent(x, new ArrayList<>());
+                    map.get(x).add(i);
+                    while(num % x == 0) {
+                        num /= x;
                     }
                 }
             }
@@ -54,7 +60,7 @@ class Solution {
                     visited[rv+1] = true;
                     q.add(rv+1);
                 }
-                if(isPrime[nums[rv]] && map.containsKey(nums[rv])) {
+                if(spf[nums[rv]] == nums[rv] && map.containsKey(nums[rv])) {
                     List<Integer> ll = map.get(nums[rv]);
                     for(int j = 0; j < ll.size(); j++) {
                         if(!visited[ll.get(j)]) {
