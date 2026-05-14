32a9ff06fa2e77689eb31f71f81bbd923c2432d3
Krishna Singh <krishnasinghks1225@gmail.com>
2026-05-14 18:46:11 +0530

    Time: 783 ms (5.00%) | Memory: 175.9 MB (66.65%) - LeetSync

--- 3933-minimum-jumps-to-reach-end-via-prime-teleportation/minimum-jumps-to-reach-end-via-prime-teleportation.java
+++ 3933-minimum-jumps-to-reach-end-via-prime-teleportation/minimum-jumps-to-reach-end-via-prime-teleportation.java
@@ -1,42 +1,41 @@
 class Solution {
-    static final int MAX = 1000000;
-    static List<Integer>[] factors = new ArrayList[MAX + 1];
+    static boolean[] isPrime = new boolean[1000001];
     static {
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
 
-        for(int i = 0; i <= MAX; i++) {
-            factors[i] = new ArrayList<>();
-        }
-
-        for(int i = 2; i <= MAX; i++) {
-
-            if(factors[i].isEmpty()) {
-
-                for(int j = i; j <= MAX; j += i) {
-                    factors[j].add(i);
+        for(int i = 2; (long)i * i <= 1000000; i++) {
+            if(isPrime[i]) {
+                for(int j = i * i; j <= 1000000; j += i) {
+                    isPrime[j] = false;
                 }
             }
         }
     }
     public int minJumps(int[] nums) {
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
         int n = nums.length;
-
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
         for(int i = 0; i < n; i++) {
-
-            for(int p : factors[nums[i]]) {
-
-                map.putIfAbsent(p, new ArrayList<>());
-
-                map.get(p).add(i);
+            int limit = (int)Math.sqrt(nums[i]);
+            for(int j = 1; j <= limit; j++) {
+                if(nums[i] % j == 0) {
+                    if(isPrime[j]) {
+                        map.putIfAbsent(j, new ArrayList<>());
+                        map.get(j).add(i);
+                    }
+                    if(nums[i]/j != j && isPrime[nums[i]/j]) {
+                        map.putIfAbsent(nums[i]/j, new ArrayList<>());
+                        map.get(nums[i]/j).add(i);
+                    }
+                }
             }
         }
-
         return minJump(nums, map);
     }
     private int minJump(int[] nums, HashMap<Integer, List<Integer>> map) {
         int n = nums.length;
         boolean[] visited = new boolean[n];
-        Queue<Integer> q = new ArrayDeque<>();
+        Queue<Integer> q = new LinkedList<>();
         q.add(0);
         int move = 0;
         visited[0] = true;
@@ -55,26 +54,15 @@ class Solution {
                     visited[rv+1] = true;
                     q.add(rv+1);
                 }
-                // teleport only if current number is prime
-                if(factors[nums[rv]].size() == 1) {
-
-                    int p = nums[rv];
-
-                    if(map.containsKey(p)) {
-
-                        for(int idx : map.get(p)) {
-
-                            if(!visited[idx]) {
-
-                                visited[idx] = true;
-
-                                q.add(idx);
-                            }
+                if(isPrime[nums[rv]] && map.containsKey(nums[rv])) {
+                    List<Integer> ll = map.get(nums[rv]);
+                    for(int j = 0; j < ll.size(); j++) {
+                        if(!visited[ll.get(j)]) {
+                            q.add(ll.get(j));
+                            visited[ll.get(j)] = true;
                         }
-
-                        // important optimization
-                        map.get(p).clear();
                     }
+                    map.remove(nums[rv]);
                 }
             }
             move++;
