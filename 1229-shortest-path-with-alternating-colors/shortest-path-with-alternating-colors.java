6b84d07614cf485f3691f6f69199cfdaff6cc065
Krishna Singh <krishnasinghks1225@gmail.com>
2026-05-19 19:27:02 +0530

    Time: 5 ms (65.41%) | Memory: 46.8 MB (27.90%) - LeetSync

---
+++ 1229-shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.java
@@ -0,0 +1,57 @@
+class Solution {
+    public int[] shortestAlternatingPaths(int n, int[][] redEdges, int[][] blueEdges) {
+        List<List<int[]>> adj = new ArrayList<>();
+
+        for(int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for(int[] e: redEdges) {
+            adj.get(e[0]).add(new int[]{e[1], 1});
+        }
+
+        for(int[] e: blueEdges) {
+            adj.get(e[0]).add(new int[]{e[1], 2});
+        }
+
+        Queue<int[]> q = new LinkedList<>();
+        int[] ans = new int[n];
+        boolean[][] visited = new boolean[n][3];
+        Arrays.fill(ans, -1);
+        
+        q.add(new int[]{0, 0});
+        int len = 0;
+        visited[0][0] = true;
+
+        while(!q.isEmpty()) {
+            int size = q.size();
+            while(size-- > 0) {
+                int[] rv = q.poll();
+                if(ans[rv[0]] == -1) {
+                    ans[rv[0]] = len;
+                }
+                for(int[] nbrs: adj.get(rv[0])) {
+                    if(nbrs[1] == 1) {
+                        if(rv[1] == 0 || rv[1] == 2) {
+                            if(!visited[nbrs[0]][1]) {
+                                visited[nbrs[0]][1] = true;
+                                q.add(nbrs);
+                            }
+                        }
+                    }
+                    else {
+                        if(rv[1] == 0 || rv[1] == 1) {
+                            if(!visited[nbrs[0]][2]) {
+                                visited[nbrs[0]][2] = true;
+                                q.add(nbrs);
+                            }
+                        }
+                    }
+                }
+            }
+            len++;
+        }
+
+        return ans;
+    }
+}
