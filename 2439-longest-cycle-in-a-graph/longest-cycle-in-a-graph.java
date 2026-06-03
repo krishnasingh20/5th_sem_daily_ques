56f0363c55bf2147908a6ff0978430d630afe2e3
Krishna Singh <krishnasinghks1225@gmail.com>
2026-06-03 13:27:53 +0530

    Time: 92 ms (20.82%) | Memory: 176.9 MB (12.95%) - LeetSync

--- 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.java
+++ 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.java
@@ -1,40 +1,14 @@
 class Solution {
-    int n;
-    boolean[] visited;
-    int ans = 0;
     public int longestCycle(int[] edges) {
-        n = edges.length;
-        visited = new boolean[n];
-        boolean hasCycle = topoSort(edges);
-        if(hasCycle) {
-            return -1;
-        }
-        topoSortdfs(edges);
-        return ans;
-    }
-    public void topoSortdfs(int[] edges) {
-        for(int i = 0; i < n; i++) {
-            if(!visited[i]) {
-                dfs(i, 0, edges);
-            }
-        }
-    }
-    public void dfs(int src, int len, int[] edges) {
-        visited[src] = true;
-        if(edges[src] != -1) {
-            if(visited[edges[src]]) {
-                ans = Math.max(ans, len+1);
-                visited[edges[src]] = true;
-            }
-            else {
-                dfs(edges[src], len+1, edges);
-            }
-        }
-    }
-    public boolean topoSort(int[] edges) {
+        int n = edges.length;
+        List<List<Integer>> adj = new ArrayList<>();
         int[] inDegree = new int[n];
         for(int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for(int i = 0; i < n; i++) {
             if(edges[i] != -1) {
+                adj.get(i).add(edges[i]);
                 inDegree[edges[i]]++;
             }
         }
@@ -44,18 +18,45 @@ class Solution {
                 q.add(i);
             }
         }
+        boolean[] visited = new boolean[n];
         int count = 0;
         while(!q.isEmpty()) {
             int rv = q.poll();
             visited[rv] = true;
             count++;
-            if(edges[rv] != -1) {
-                inDegree[edges[rv]]--;
-                if(inDegree[edges[rv]] == 0) {
-                    q.add(edges[rv]);
+            for(int nbrs: adj.get(rv)) {
+                inDegree[nbrs]--;
+                if(inDegree[nbrs] == 0) {
+                    q.add(nbrs);
                 }
             }
         }
-        return count == n;
+        if(count == n) {
+            return -1;
+        }
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            if(visited[i]) {
+                continue;
+            }
+            int cnt = 0;
+            q.add(i);
+            while(!q.isEmpty()) {
+                int rv = q.poll();
+                if(visited[rv]) {
+                    continue;
+                }
+                visited[rv] = true;
+                cnt++;
+                for(int nbrs: adj.get(rv)) {
+                    if(!visited[nbrs]) {
+                        q.add(nbrs);
+                    }
+                }
+            }
+            ans = Math.max(ans, cnt);
+        }
+
+        return ans;
     }
 }
