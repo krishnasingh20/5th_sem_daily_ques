b6f6dc44f09d78547dacf36ab2ad713799bb94df
Krishna Singh <krishnasinghks1225@gmail.com>
2026-06-11 00:05:57 +0530

    Time: 63 ms (81.70%) | Memory: 79.4 MB (83.50%) - LeetSync

---
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.java
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        HashMap<Integer, TreeNode> map = new HashMap<>();
+        HashSet<Integer> parent = new HashSet<>();
+
+        for(int[] d: descriptions) {
+            int p = d[0];
+            int c = d[1];
+            TreeNode p1;
+            TreeNode c1;
+            if(!map.containsKey(p)) {
+                p1 = new TreeNode(p);
+                map.put(p, p1);
+                parent.add(p);
+            }
+            else {
+                p1 = map.get(p);
+            }
+
+            if(!map.containsKey(c)) {
+                c1 = new TreeNode(c);
+                map.put(c, c1);
+            }
+            else {
+                c1 = map.get(c);
+                if(parent.contains(c)) {
+                    parent.remove(c);
+                }
+            }
+
+            if(d[2] == 1) {
+                p1.left = c1;
+            }
+            else {
+                p1.right = c1;
+            }
+        }
+
+        int p = 0;
+
+        for(int p1: parent) {
+            p = p1;
+        }
+
+        return map.get(p);
+    }
+}
