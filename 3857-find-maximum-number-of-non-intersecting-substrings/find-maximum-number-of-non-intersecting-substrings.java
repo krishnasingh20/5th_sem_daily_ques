f8818aa0d3333a471b1fd9a3efb94fc2d8cb3d9d
Krishna Singh <krishnasinghks1225@gmail.com>
2026-06-16 19:49:11 +0530

    Time: 131 ms (27.27%) | Memory: 273.5 MB (5.68%) - LeetSync

--- 3857-find-maximum-number-of-non-intersecting-substrings/find-maximum-number-of-non-intersecting-substrings.java
+++ 3857-find-maximum-number-of-non-intersecting-substrings/find-maximum-number-of-non-intersecting-substrings.java
@@ -6,43 +6,35 @@ class Solution {
             return 0;
         }
 
-        int[] dp = new int[n];
-        HashMap<Integer, TreeSet<Integer>> map = new HashMap<>();
-        
-        for(int i = 0; i < n; i++) {
+        int[][] dp = new int[n][27];
+
+        for(int i = 0; i < 3; i++) {
             int x = s.charAt(i)-'a';
-
-            if(!map.containsKey(x)) {
-                TreeSet<Integer> set = new TreeSet<>();
-                set.add(i);
-                map.put(x, set);
-
-                if(i > 0) {
-                    dp[i] = dp[i-1];
-                }
-
-                continue;
+            for(int j = 0; j < 26; j++) {
+                dp[i][j] = (i-1 >= 0) ? dp[i-1][j] : -1;
             }
-
-            TreeSet<Integer> set = map.get(x);
-            Integer idx = set.floor(i-3);
-
-            if(idx == null) {
-                set.add(i);
-
-                if(i > 0) {
-                    dp[i] = dp[i-1];
-                }
-                
-                continue;
-            }
-
-            int curr = (idx > 0 ? dp[idx - 1] : 0) + 1;
-            dp[i] = Math.max(dp[i-1], curr);
-
-            set.add(i);
+            dp[i][x] = i;
         }
 
-        return dp[n-1];
+        for(int i = 3; i < n; i++) {
+            int x = s.charAt(i)-'a';
+
+            if(dp[i-3][x] != -1) {
+                int idx = dp[i-3][x];
+                int curr = (idx - 1 >= 0 ? dp[idx - 1][26] : 0) + 1;
+                dp[i][26] = Math.max(dp[i-1][26], curr);
+            }
+            else {
+                dp[i][26] = dp[i-1][26];
+            }
+
+            for(int j = 0; j < 26; j++) {
+                dp[i][j] = dp[i-1][j];
+            }
+
+            dp[i][x] = i;
+        }
+
+        return dp[n-1][26];
     }
 }
