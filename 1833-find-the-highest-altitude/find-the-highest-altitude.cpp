e5cd78ba17690289ed5800f3ed05312922f29f5c
Krishna Singh <krishnasinghks1225@gmail.com>
2026-06-20 14:34:50 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (17.16%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
     int largestAltitude(vector<int>& gain) {
         int ans = max(0, gain[0]);
         for(int i = 1; i < gain.size(); i++) {
