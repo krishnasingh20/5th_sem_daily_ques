e1941f7058a1b0c65dbeaa5066c6f62ea3e4f374
Krishna Singh <krishnasinghks1225@gmail.com>
2026-06-21 16:23:33 +0530

    Time: 5 ms (11.62%) | Memory: 46.6 MB (9.85%) - LeetSync

--- 685-redundant-connection-ii/redundant-connection-ii.java
+++ 685-redundant-connection-ii/redundant-connection-ii.java
@@ -1,61 +1,66 @@
 class Solution {
+    Queue<Integer> q = new LinkedList<>();
+    HashSet<Integer>[] adj;
     public int[] findRedundantDirectedConnection(int[][] edges) {
         int n = edges.length;
-        HashSet<Integer>[] adj = new HashSet[n+1];
+        int[] inDegree = new int[n+1];
+        adj = new HashSet[n+1];
 
-        for(int i = 0; i <= n; i++) {
+        for(int i = 1; i <= n; i++) {
             adj[i] = new HashSet<>();
         }
 
         for(int[] edge: edges) {
             adj[edge[0]].add(edge[1]);
+            inDegree[edge[1]]++;
         }
 
-        Queue<Integer> q = new LinkedList<>();
-        int[] inDegree = new int[n+1];
+        int zero = -1;//root node
+        int two = -1;
+
+        for(int i = 1; i <= n; i++) {
+            if(inDegree[i] == 0) {
+                zero = i;
+            }
+            if(inDegree[i] == 2) {
+                two = i;
+            }
+        }
+
+        if(two != -1) {//node having two parent
+            for(int i = n - 1; i >= 0; i--) {
+                if(edges[i][1] == two) {
+                    adj[edges[i][0]].remove(edges[i][1]);
+                    if(validTree(zero)) {
+                        return edges[i];
+                    }
+                    adj[edges[i][0]].add(edges[i][1]);
+                }
+            }
+        }
 
         for(int i = n - 1; i >= 0; i--) {
-
-            for(int[] edge: edges) {
-                inDegree[edge[1]]++;
-            }
-
             adj[edges[i][0]].remove(edges[i][1]);
-            inDegree[edges[i][1]]--;
-
-            for(int j = 1; j <= n; j++) {
-                if(inDegree[j] == 0) {
-                    q.add(j);
-                }
-            }
-
-            if(q.size() > 1) {
-                Arrays.fill(inDegree, 0);
-                adj[edges[i][0]].add(edges[i][1]);
-                q.clear();
-                continue;
-            }
-
-            int count = 0;
-            while(!q.isEmpty()) {
-                int rv = q.poll();
-                count++;
-                for(int nbrs: adj[rv]) {
-                    if(--inDegree[nbrs] == 0) {
-                        q.add(nbrs);
-                    }
-                }
-            }
-
-            if(count == n) {
+            if(validTree(edges[i][1])) {
                 return edges[i];
             }
-
-            Arrays.fill(inDegree, 0);
             adj[edges[i][0]].add(edges[i][1]);
-
         }
 
         return new int[]{};
+    }
+
+    public boolean validTree(int src) {
+        q.add(src);
+        int count = 0;
+        while(!q.isEmpty()) {
+            int rv = q.poll();
+            count++;
+            for(int nbrs: adj[rv]) {
+                q.add(nbrs);
+            }
+        }
+
+        return count == adj.length - 1;
     }
 }
