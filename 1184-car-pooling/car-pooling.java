5d23f2213e286c53728f5c80d040d90a3ad8ad06
Krishna Singh <krishnasinghks1225@gmail.com>
2026-07-03 16:13:27 +0530

    Time: 1 ms (100.00%) | Memory: 46.1 MB (78.12%) - LeetSync

--- 1184-car-pooling/car-pooling.java
+++ 1184-car-pooling/car-pooling.java
@@ -1,25 +1,31 @@
 class Solution {
     public boolean carPooling(int[][] trips, int cap) {
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
-        Arrays.sort(trips, (a, b) -> {
-            if(a[1] == b[1]) {
-                return Integer.compare(a[2], b[2]);
-            }
-            return Integer.compare(a[1], b[1]);
-        });
+        int max = 0;
+        int n = trips.length;
 
-        int curr = 0;
+        for(int i = 0; i < n; i++) {
+            max = Math.max(max, trips[i][2]);
+        }
 
-        for(int i = 0; i < trips.length; i++) {
-            while(!pq.isEmpty() && trips[i][1] >= pq.peek()[0]) {
-                curr -= pq.poll()[1];
-            }
-            if(curr+trips[i][0] > cap) {
+        int[] diff = new int[max+1];
+
+        for(int i = 0; i < n; i++) {
+            int l = trips[i][1];
+            int r = trips[i][2];
+            int val = trips[i][0];
+            diff[l] += val;
+            diff[r] -= val;
+        }
+
+        for(int i = 1; i <= max; i++) {
+            diff[i] += diff
+            [i-1];
+        }
+
+        for(int i = 0; i <= max; i++) {
+            if(diff[i] > cap) {
                 return false;
             }
-            pq.add(new int[]{trips[i][2], trips[i][0]});
-            curr += trips[i][0];
         }
 
         return true;
