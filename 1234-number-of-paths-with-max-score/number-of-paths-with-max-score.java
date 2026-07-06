cffd9a16642e3d4be91009f27e618b6a1ebf84fa
Krishna Singh <krishnasinghks1225@gmail.com>
2026-07-07 00:25:56 +0530

    Time: 1086 ms (6.90%) | Memory: 276.5 MB (9.20%) - LeetSync

--- 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.java
+++ 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.java
@@ -1,4 +1,6 @@
 class Solution {
+    static int[] dx = {0, 1, 1};
+    static int[] dy = {1, 0, 1};
     public int[] pathsWithMaxScore(List<String> board) {
         int n = board.size();
         char[][] grid = new char[n][n];
@@ -9,38 +11,49 @@ class Solution {
             }
         }
         grid[0][0] = grid[n - 1][n - 1] = '0';
-        int[][] dp1 = new int[n][n];
-        for(int[] d: dp1) {
-            Arrays.fill(d, -1);
+        int[][] dist = new int[n][n];
+        for(int[] d: dist) {
+            Arrays.fill(d, Integer.MIN_VALUE);
         }
-        int max = maxPath(grid, n - 1, n - 1, dp1);
-        if(max < 0) {
+        dist[0][0] = 0;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0, 0, 0});
+        
+        while(!q.isEmpty()) {
+            int[] rv = q.poll();
+
+            if(dist[rv[0]][rv[1]] != rv[2]) {
+                continue;
+            }
+
+            for(int i = 0; i < 3; i++) {
+                int nX = dx[i] + rv[0];
+                int nY = dy[i] + rv[1];
+
+                if(nX >= n || nY >= n || grid[nX][nY] == 'X') {
+                    continue;
+                }
+
+                int newDist = dist[rv[0]][rv[1]] + (grid[nX][nY] - '0');
+                if(newDist > dist[nX][nY]) {
+                    dist[nX][nY] = newDist;
+                    q.add(new int[]{nX, nY, newDist});
+                }
+            }
+        }
+
+        if(dist[n-1][n-1] == Integer.MIN_VALUE) {
             return new int[]{0, 0};
         }
-        int[][][] dp2 = new int[n][n][max+1];
+
+        int[][][] dp2 = new int[n][n][dist[n-1][n-1]+1];
         for(int[][] d: dp2) {
             for(int[] a: d) {
                 Arrays.fill(a, -1);
             }
         }
-        int count = countPath(grid, n - 1, n - 1, max, dp2);
-        return new int[]{max, count};
-    }
-    public int maxPath(char[][] grid, int r, int c, int[][] dp) {
-        if(r == 0 && c == 0) {
-            return 0;
-        }
-        if(r < 0 || c < 0 || grid[r][c] == 'X') {
-            return Integer.MIN_VALUE;
-        }
-        if(dp[r][c] != -1) {
-            return dp[r][c];
-        }
-        int val = grid[r][c] - '0';
-        int up = maxPath(grid, r-1, c, dp);
-        int left = maxPath(grid, r, c-1, dp);
-        int up_left = maxPath(grid, r-1, c-1, dp);
-        return dp[r][c] = val + Math.max(up, Math.max(left, up_left));
+        int count = countPath(grid, n - 1, n - 1, dist[n-1][n-1], dp2);
+        return new int[]{dist[n-1][n-1], count};
     }
     public int countPath(char[][] grid, int r, int c, int max, int[][][] dp) {
         if(r == 0 && c == 0) {
