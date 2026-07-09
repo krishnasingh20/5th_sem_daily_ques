b40bc4d051ae40c3ebb47023e0fe22ad80e882e3
Krishna Singh <krishnasinghks1225@gmail.com>
2026-07-09 09:10:21 +0530

    Time: 9 ms (52.48%) | Memory: 166 MB (31.68%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.java
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.java
@@ -1,36 +1,64 @@
 class Solution {
+    class DSU {
+        int n;
+        int[] parent;
+        int[] size;
+
+        DSU(int n) {
+            this.n = n;
+            parent = new int[n];
+            size = new int[n];
+            for(int i = 0; i < n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
+        }
+
+        int find(int node) {
+            if(node == parent[node]) {
+                return node;
+            }
+            return parent[node] = find(parent[node]);
+        }
+
+        void unite(int u, int v) {
+            int p1 = find(u);
+            int p2 = find(v);
+
+            if(p1 == p2) {
+                return;
+            }
+
+            if(size[p1] > size[p2]) {
+                parent[p2] = p1;
+                size[p1] += size[p2];
+            }
+            else {
+                parent[p1] = p2;
+                size[p2] += size[p1];
+            }
+        }
+    }
     public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
-        // below array will tell where the connection is breaking
-        int[] connection = new int[n];
-        Arrays.fill(connection, -1);
+
+        DSU dsu = new DSU(n);
+
         for(int i = 1; i < n; i++) {
-            if(nums[i] - nums[i-1] > maxDiff) {
-                connection[i-1] = i-1;
+            if(nums[i]-nums[i-1] <= maxDiff) {
+                dsu.unite(i, i-1);
             }
         }
-        for(int i = n-2; i >= 0; i--) {
-            if(connection[i] != -1) {
-                continue;
-            }
-            if(connection[i+1] != -1) {
-                connection[i] = connection[i+1];
-            }
-        }
-        int m = queries.length;
-        boolean[] ans = new boolean[m];
-        for(int i = 0; i < m; i++) {
-            if(queries[i][0] == queries[i][1]) {
+
+        boolean[] ans = new boolean[queries.length];
+
+        for(int i = 0; i < queries.length; i++) {
+            int u = queries[i][0];
+            int v = queries[i][1];
+            if(dsu.find(u) == dsu.find(v)) {
                 ans[i] = true;
-                continue;
             }
-            //--> take path from u to v (smallest node to largest because if path form one end possible then it will also possible from another end)
-            int u = Math.min(queries[i][0], queries[i][1]);
-            int v = Math.max(queries[i][0], queries[i][1]);
-            if(connection[u] != -1 && connection[u] < v) {
-                continue;
-            }
-            ans[i] = true;
         }
+
         return ans;
     }
 }
