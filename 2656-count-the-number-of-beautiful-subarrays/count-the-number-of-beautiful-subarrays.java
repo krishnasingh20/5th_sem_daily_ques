32f006bb517b999aeafa62501c6607d15f9c4aef
Krishna Singh <krishnasinghks1225@gmail.com>
2026-07-10 17:40:17 +0530

    Time: 49 ms (84.68%) | Memory: 107.2 MB (85.89%) - LeetSync

--- 2656-count-the-number-of-beautiful-subarrays/count-the-number-of-beautiful-subarrays.java
+++ 2656-count-the-number-of-beautiful-subarrays/count-the-number-of-beautiful-subarrays.java
@@ -2,7 +2,7 @@ class Solution {
     public long beautifulSubarrays(int[] nums) {
 
         int n = nums.length;
-        
+
         HashMap<Integer, Integer> map = new HashMap<>();
         map.put(0, 1);
 
@@ -10,14 +10,7 @@ class Solution {
         long ans = 0;
 
         for(int i = 0; i < n; i++) {
-
-            for(int j = 0; j <= 20; j++) {
-
-                if((nums[i] & (1 << j)) != 0) {
-                    curr = curr ^ (1 << j);
-                }
-            }
-
+            curr = curr ^ nums[i];
             int val = map.getOrDefault(curr, 0);
             map.put(curr, val+1);
             ans += val;
