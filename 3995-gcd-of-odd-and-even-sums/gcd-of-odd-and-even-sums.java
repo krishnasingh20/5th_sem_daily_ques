fe13d3124b05d05a027b3377c3bd930e90ead0ec
Krishna Singh <krishnasinghks1225@gmail.com>
2026-07-16 00:26:21 +0530

    Time: 1 ms (83.11%) | Memory: 42.6 MB (57.16%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -2,7 +2,7 @@ class Solution {
     public int gcdOfOddEvenSums(int n) {
         int sumOdd = n * n;
         int sumEven = n * (n + 1);
-        return GCD(sumOdd, sumEven);
+        return n; //highest factor is n that is common in both the number
     }
     private int GCD(int a, int b) {
         return (b == 0 ? a : GCD(b, a % b));
