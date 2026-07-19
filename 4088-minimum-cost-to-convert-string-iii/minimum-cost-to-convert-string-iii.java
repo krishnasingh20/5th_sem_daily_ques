bcf39cb9dae8fe3622c7e430c00f922c56811a1c
Krishna Singh <krishnasinghks1225@gmail.com>
2026-07-20 01:32:36 +0530

    Time: 283 ms (0.00%) | Memory: 47.6 MB (50.00%) - LeetSync

--- 4088-minimum-cost-to-convert-string-iii/minimum-cost-to-convert-string-iii.java
+++ 4088-minimum-cost-to-convert-string-iii/minimum-cost-to-convert-string-iii.java
@@ -12,67 +12,61 @@ class Solution {
         this.src = source;
         this.des = target;
 
-        dp = new int[source.length()];
-        Arrays.fill(dp, -1);
-
-        int ans = minCost(0);
-
-        return ans == Integer.MAX_VALUE ? -1 : ans;
+        return bottomUp();
     }
 
-    private int minCost(int i) {
-        if (i == src.length()) {
-            return 0;
-        }
+    private int bottomUp() {
+        int n = src.length();
 
-        if(dp[i] != -1) {
-            return dp[i];
-        }
+        dp = new int[n+1];
 
-        int match = Integer.MAX_VALUE;
-        if (src.charAt(i) == des.charAt(i)) {
-            match = minCost(i + 1);
-        }
+        for(int i = n - 1; i >= 0; i--) {
 
-        //apply rules
-        int replace = Integer.MAX_VALUE;
+            int match = Integer.MAX_VALUE;
+            if(src.charAt(i) == des.charAt(i)) {
+                match = dp[i+1];
+            }
 
-        for (int j = 0; j < rules.size(); j++) {
+            // apply rules
+            int replace = Integer.MAX_VALUE;
 
-            String pattern = rules.get(j).get(0);
-            String replacement = rules.get(j).get(1);
+            for(int j = 0; j < rules.size(); j++) {
 
-            if (i + pattern.length() <= src.length()) {
+                String pattern = rules.get(j).get(0);
+                String replacement = rules.get(j).get(1);
 
-                int star = 0;
-                boolean flag = false;
+                if(i+pattern.length() <= n) {
+                    int star = 0;
+                    boolean flag = false;
 
-                for (int k = 0; k < pattern.length(); k++) {
-                    if (des.charAt(i + k) != replacement.charAt(k)) {
-                        flag = true;
-                        break;
+                    for(int k = 0; k < pattern.length(); k++) {
+                        if(replacement.charAt(k) != des.charAt(i+k)) {
+                            flag = true;
+                            break;
+                        }
+
+                        if(pattern.charAt(k) == '*') {
+                            star++;
+                        }
+                        else if(pattern.charAt(k) != src.charAt(i+k)) {
+                            flag = true;
+                            break;
+                        }
                     }
 
-                    if (pattern.charAt(k) == '*') {
-                        star++;
-                    } else if (pattern.charAt(k) != src.charAt(i + k)) {
-                        flag = true;
-                        break;
+                    if(!flag) {
+                        int curr = dp[i+pattern.length()];
+                        if(curr != Integer.MAX_VALUE) {
+                            curr += star + costs[j];
+                        }
+                        replace = Math.min(replace, curr);
                     }
-                }
-
-                if (!flag) {
-                    int curr = minCost(i + pattern.length());
-
-                    if (curr != Integer.MAX_VALUE) {
-                        curr += star + costs[j];
-                    }
-
-                    replace = Math.min(replace, curr);
                 }
             }
+
+            dp[i] = Math.min(match, replace);
         }
 
-        return dp[i] = Math.min(replace, match);
+        return dp[0] == Integer.MAX_VALUE ? -1 : dp[0];
     }
 }
