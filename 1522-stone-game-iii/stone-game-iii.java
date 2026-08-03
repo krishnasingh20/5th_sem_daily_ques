55e0d3841dc349ef16bf71460a917b9cf26b0a69
Krishna Singh <krishnasinghks1225@gmail.com>
2026-08-03 22:32:22 +0530

    Time: 12 ms (60.23%) | Memory: 85.2 MB (83.24%) - LeetSync

--- 1522-stone-game-iii/stone-game-iii.java
+++ 1522-stone-game-iii/stone-game-iii.java
@@ -2,10 +2,7 @@ class Solution {
     int[] dp;
     public String stoneGameIII(int[] stoneValue) {
 
-        dp = new int[stoneValue.length];
-        Arrays.fill(dp, -1);
-
-        int stoneA = maxStone(stoneValue, 0);
+        int stoneA = bottomUp(stoneValue);
 
         int totalSum = 0;
         for(int s: stoneValue) {
@@ -18,34 +15,28 @@ class Solution {
 
         return stoneA > (totalSum - stoneA) ? "Alice" : "Bob";
     }
-    public int maxStone(int[] arr, int i) {
-        if(i >= arr.length) {
-            return 0;
+
+    public int bottomUp(int[] stone) {
+        int n = stone.length;
+        dp = new int[n];
+
+        for(int i = n-1; i >= 0; i--) {
+            int a = stone[i] + Math.min((i+2 < n ? dp[i+2] : 0), Math.min((i+3 < n ? dp[i+3] : 0), (i+4 < n ? dp[i+4] : 0)));
+
+            int b = Integer.MIN_VALUE;
+            int c = Integer.MIN_VALUE;
+
+            if(i+1 < n) {
+                b = stone[i] + stone[i+1] + Math.min((i+3 < n ? dp[i+3] : 0), Math.min((i+4 < n ? dp[i+4] : 0), (i+5 < n ? dp[i+5] : 0)));
+            }
+
+            if(i+2 < n) {
+                c = stone[i] + stone[i+1] + stone[i+2] + Math.min((i+4 < n ? dp[i+4] : 0), Math.min((i+5 < n ? dp[i+5] : 0), (i+6 < n ? dp[i+6] : 0)));
+            }
+
+            dp[i] = Math.max(a, Math.max(b, c));
         }
 
-        if(dp[i] != -1) {
-            return dp[i];
-        }
-
-        int a = arr[i] + Math.min(maxStone(arr, i+2), Math.min(maxStone(arr, i+3), maxStone(arr, i+4)));
-        int b = Integer.MIN_VALUE;
-        int c = Integer.MIN_VALUE;
-
-        if(i+1 < arr.length) {
-            b = arr[i] + arr[i+1] + Math.min(maxStone(arr, i+3), Math.min(maxStone(arr, i+4), maxStone(arr, i+5)));
-        }
-
-        if(i+2 < arr.length) {
-            c = arr[i] + arr[i+1] + arr[i+2] + Math.min(maxStone(arr, i+4), Math.min(maxStone(arr, i+5), maxStone(arr, i+6)));
-        }
-
-        return dp[i] = Math.max(a, Math.max(b, c));
+        return dp[0];
     }
-
-    // public int bottomUp(int[] stone) {
-    //     int n = stone.length;
-    //     dp = new int[n];
-    //     dp[n-1] = stone[n];
-
-    // }
 }
