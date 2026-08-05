7e44643bf550159fb5729640f9ac5690b41d4ce7
Krishna Singh <krishnasinghks1225@gmail.com>
2026-08-05 23:00:23 +0530

    Time: 68 ms (64.42%) | Memory: 275 MB (90.38%) - LeetSync

--- 3561-remove-methods-from-project/remove-methods-from-project.java
+++ 3561-remove-methods-from-project/remove-methods-from-project.java
@@ -6,8 +6,8 @@ class Solution {
             adj.add(new ArrayList<>());
         }
 
-        for(int[] edge: invocations) {
-            adj.get(edge[0]).add(edge[1]);
+        for(int[] invoke: invocations) {
+            adj.get(invoke[0]).add(invoke[1]);
         }
 
         boolean[] visited = new boolean[n];
@@ -28,43 +28,17 @@ class Solution {
         }
 
         List<Integer> ans = new ArrayList<>();
-        boolean[] visited1 = new boolean[n];
         boolean flag = false;
-        
-        for(int i = 0; i < n; i++) {
-            if(visited1[i] || visited[i]) {
-                continue;
-            }
-            ans.add(i);
-            visited1[i] = true;
-            q.add(i);
-            while(!q.isEmpty()) {
-                int rv = q.poll();
-                for(int nbrs: adj.get(rv)) {
-                    if(visited[nbrs]) {
-                        flag = true;
-                        break;
-                    }
-                    if(!visited1[nbrs]) {
-                        q.add(nbrs);
-                        visited1[nbrs] = true;
-                        ans.add(nbrs);
-                    }
 
-                }
-            }
-
-            if(flag) {
+        for(int[] invoke: invocations) {
+            if(!visited[invoke[0]] && visited[invoke[1]]) {
+                flag = true;
                 break;
             }
         }
 
-        if(!flag) {
-            return ans;
-        }
-
         for(int i = 0; i < n; i++) {
-            if(visited[i] || (!visited[i] && !visited1[i])) {
+            if(!visited[i] || flag) {
                 ans.add(i);
             }
         }
