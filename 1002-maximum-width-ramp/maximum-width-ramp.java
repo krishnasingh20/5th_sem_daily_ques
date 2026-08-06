8b6a1959e5dfdcf44398d885f60dddac88384797
Krishna Singh <krishnasinghks1225@gmail.com>
2026-08-06 23:53:23 +0530

    Time: 5 ms (79.00%) | Memory: 59.7 MB (61.60%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.java
+++ 1002-maximum-width-ramp/maximum-width-ramp.java
@@ -1,20 +1,23 @@
 class Solution {
     public int maxWidthRamp(int[] nums) {
-        TreeMap<Integer, Integer> map = new TreeMap<>();
+        int max  = 0;
+        for(int num: nums) {
+            max = Math.max(max, num);
+        }
+
+        int[] ramp = new int[max+1];
+        Arrays.fill(ramp, -1);
         int ans = 0;
 
         for(int i = 0; i < nums.length; i++) {
             int num = nums[i];
-            Integer key = map.floorKey(num);
-
-            // System.out.println(key);
-
-            if(key != null) {
-                ans = Math.max(ans, i - map.get(key));
+            if(ramp[num] == -1) {
+                while(num <= max && ramp[num] == -1) {
+                    ramp[num] = i;
+                    num++;
+                }
             }
-            else {
-                map.put(num, i);
-            }
+            ans = Math.max(ans, i - ramp[nums[i]]);
         }
 
         return ans;
