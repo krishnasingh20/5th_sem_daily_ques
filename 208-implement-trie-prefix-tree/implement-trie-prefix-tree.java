6d2e0d8bb5c503b4c32f0e8703c23182fe1b6582
Krishna Singh <krishnasinghks1225@gmail.com>
2026-08-07 23:37:56 +0530

    Time: 33 ms (62.52%) | Memory: 62.1 MB (39.58%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.java
@@ -1,57 +1,55 @@
 class Trie {
 
     class Node {
-        char ch;
         boolean isTerminal;
         Node[] child;
-        public Node(char ch) {
-            this.ch = ch;
+        Node() {
             child = new Node[26];
         }
     }
 
-    private Node root;
+    Node root;
 
     public Trie() {
-        root = new Node('*');
+        root = new Node();
     }
-
+    
     public void insert(String word) {
         Node curr = root;
+        
         for(int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
-            if(curr.child[ch-'a'] != null) {
-                curr = curr.child[ch-'a'];
+            int x = word.charAt(i)-'a';
+            if(curr.child[x] == null) {
+                curr.child[x] = new Node();
             }
-            else {
-                Node nn = new Node(ch);
-                curr.child[ch-'a'] = nn;
-                curr = nn;
-            }
+            curr = curr.child[x];
         }
+
         curr.isTerminal = true;
     }
-
+    
     public boolean search(String word) {
         Node curr = root;
+
         for(int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
-            if(curr.child[ch-'a'] == null) {
+            int x = word.charAt(i)-'a';
+            if(curr.child[x] == null) {
                 return false;
             }
-            curr = curr.child[ch-'a'];
+            curr = curr.child[x];
         }
+
         return curr.isTerminal;
     }
-
+    
     public boolean startsWith(String prefix) {
         Node curr = root;
         for(int i = 0; i < prefix.length(); i++) {
-            char ch = prefix.charAt(i);
-            if(curr.child[ch-'a'] == null) {
+            int x = prefix.charAt(i)-'a';
+            if(curr.child[x] == null) {
                 return false;
             }
-            curr = curr.child[ch-'a'];
+            curr = curr.child[x];
         }
         return true;
     }
