e5c9b80a34418d8a2c7804f369a137c46a5bffb7
Krishna Singh <krishnasinghks1225@gmail.com>
2026-08-12 23:07:06 +0530

    Time: 6 ms (58.46%) | Memory: 44.2 MB (38.71%) - LeetSync

---
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.java
@@ -0,0 +1,67 @@
+class Solution {
+    public int longestSubstring(String s, int k) {
+        int n = s.length();
+
+        int[][] freq = new int[n][26];
+        int[] index = new int[26];
+        Queue<Integer> q = new LinkedList<>();
+        int ans = 0;
+        
+        for(int i = 0; i < n; i++) {
+            int x = s.charAt(i)-'a';
+            freq[i][x]++;
+            index[x] = i;
+
+            for(int j = 0; j < 26; j++) {
+                freq[i][j] += (i-1 >= 0 ? freq[i-1][j] : 0);
+            }
+
+            int idx = -1;
+            int cnt = 0;
+            for(int j = 0; j < 26; j++) {
+                if(freq[i][j] > 0 && freq[i][j] < k) {
+                    idx = Math.max(idx, index[j]);
+                }
+                else if(freq[i][j] >= k) {
+                    cnt += freq[i][j];
+                }
+            }
+
+            if(cnt == (i+1)) {
+                ans = Math.max(ans, cnt);
+                continue;
+            }
+
+            if(idx != -1 && idx != i) {
+                q.add(idx);
+            }
+
+            int last = -1;
+
+            while(!q.isEmpty()) {
+                int rv = q.poll();
+                int max = -1;
+                last = Math.max(last, rv);
+                for(int j = 0; j < 26; j++) {
+                    if(freq[i][j] - freq[rv][j] > 0 && freq[i][j] - freq[rv][j] < k) {
+                        max = Math.max(max, index[j]);
+                    }
+                }
+
+                if(max != -1) {
+                    if(max == i) {
+                        last = -1;
+                        break;
+                    }
+                    q.add(max);
+                }
+            }
+
+            if(last != -1) {
+                ans = Math.max(ans, i - last);
+            }
+        }
+
+        return ans;
+    }
+}
