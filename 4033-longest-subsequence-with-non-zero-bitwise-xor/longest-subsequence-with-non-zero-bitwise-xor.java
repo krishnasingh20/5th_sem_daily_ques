6a147ccce97e0b577f9d4ee47a2b23ccd026f482
Krishna Singh <krishnasinghks1225@gmail.com>
2026-08-15 14:53:46 +0530

    Time: 2 ms (68.38%) | Memory: 133.8 MB (7.35%) - LeetSync

--- 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.java
+++ 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.java
@@ -1,25 +1,18 @@
 class Solution {
     public int longestSubsequence(int[] nums) {
-        int ans = 0;
         int n = nums.length;
-        int[] count = new int[31];
-
+        int zero = 0;
+        int xor = 0;
+        
         for(int num: nums) {
-            for(int bit = 0; bit < 31; bit++) {
-                if((num & (1L << bit)) != 0) {
-                    count[bit]++;
-                }
-            }
+            zero += (num == 0 ? 1 : 0);
+            xor ^= num;
         }
 
-        for(int i = 0; i < 31; i++) {
-            if(count[i] == 0) {
-                continue;
-            }
-            int curr = ((count[i] - 1)/2)*2 + (n - count[i]) + 1;
-            ans = Math.max(ans, curr);
+        if(xor != 0) {
+            return n;
         }
-
-        return ans;
+        
+        return zero == n ? 0 : n-1;
     }
 }
